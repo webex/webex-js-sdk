151625340ab8da4728ea9703141fa0d138e2bf96
Ian W. Remmel <design@ianwremmel.com>
2016-07-26 12:45:49 -0700

    chore(build): update release script and bump repo version to 0.7.0 to release canary builds that hopefully will not confuse people

--- tooling/circle-scripts/publish.sh
+++ tooling/circle-scripts/publish.sh
@@ -15,14 +15,21 @@ NODE_ENV=production npm run build
 npm run grunt:circle -- publish-docs
 
 # npm run lerna -- publish --repo-version=${NEXT_VERSION}
-npm run lerna -- publish --canary
-
+npm run lerna -- publish --skip-git --canary
+# TODO parse top commit message for something like !release:X.Y.Z to decide
+# whether to do a canary release
+NEW_VERSION=`cat lerna.json | grep version | awk '{print $2}'`
+git add .
+git commit -m "Release ${NEW_VERSION}"
+git tag -a -m "Release ${NEW_VERSION}" "${NEW_VERSION}"
+git commit --allow-empty -m '[skip ci]'
 git push ghc HEAD:/refs/heads/master
+git push origin "${NEW_VERSION}"
 
 # Trick npmjs.com into updating the readme
 # See https://github.com/npm/newww/issues/389#issuecomment-188428605 and
 # https://github.com/lerna/lerna/issues/64 for details
 cd packages/ciscospark
-npm version --no-git-tag-version "${NEXT_VERSION}-readmehack"
+npm version --no-git-tag-version "${NEW_VERSION}-readmehack"
 npm publish
-npm unpublish ciscospark@"${NEXT_VERSION}-readmehack"
+npm unpublish ciscospark@"${NEW_VERSION}-readmehack"
