1d76f63f518eda121ab998c442b7813a838fb4eb
Ian W. Remmel <design@ianwremmel.com>
2016-07-27 14:36:40 -0700

    chore(build): use bash correctly

--- tooling/jenkins-scripts/check-for-circle-outage.sh
+++ tooling/jenkins-scripts/check-for-circle-outage.sh
@@ -4,7 +4,7 @@ echo "Begin check-for-circle-outage.sh"
 
 if [ -e 503 ]; then
   FALLBACK_TO_INTERNAL=`cat 503`
-  if [ ${FALLBACK_TO_INTERNAL} = "yes" ]; then
+  if [ "${FALLBACK_TO_INTERNAL}" -eq "yes" ]; then
     echo "Circle appears to be having an outage, falling back to internal executors"
     echo "Complete check-for-circle-outage.sh"
     exit 0
