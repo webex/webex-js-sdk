e1214db13e475ef4598d179eb3fafd2129a292c2
Ian W. Remmel <design@ianwremmel.com>
2016-07-28 12:55:10 -0700

    fix(tooling): adjust format of suite.xml

--- tooling/circle-cli/lib/status-to-xunit.js
+++ tooling/circle-cli/lib/status-to-xunit.js
@@ -8,21 +8,25 @@ const path = require(`path`);
 module.exports = _.curry((argv, ci, result) => {
   const filename = `reports/junit/suite.xml`;
   return new Promise((resolve, reject) => {
-    let errorCount, failureCount;
+    let errorCount, failureCount, testCase;
     switch (result.status) {
     case `success`:
       failureCount = errorCount = 0;
+      testCase = `<testcase name="suite" classname=suite" time="0.1"/>`;
       break;
     case `timedout`:
       failureCount = 0;
       errorCount = 1;
+      testCase = `<testcase name="suite" classname=suite" time="0.1"><failure type="">Timed out</failure><testcase>`;
       break;
     case `failed`:
       failureCount = 1;
       errorCount = 0;
+      testCase = `<testcase name="suite" classname=suite" time="0.1"><failure type="">failed</failure><testcase>`;
       break;
     default:
       failureCount = errorCount = 1;
+      testCase = `<testcase name="suite" classname=suite" time="0.1"><failure type="">${result.status}</failure><testcase>`;
     }
     const out = `<?xml version="1.0"?>
     <testsuite
@@ -32,7 +36,9 @@ module.exports = _.curry((argv, ci, result) => {
       tests="1"
       errors="${errorCount}"
       failures="${failureCount}"
-    />`;
+    >
+    ${testCase}
+    </testsuite>`;
     console.log(`writing xunit summary to ${filename}`);
     console.log(out.toString());
     mkdirp.sync(path.dirname(filename));
