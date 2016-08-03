b68c79d89c930cdf93222fdfbfed74100d71a567
Ian W. Remmel <design@ianwremmel.com>
2016-08-03 10:10:27 -0700

    feat(bundle):  create package for generating a bundle automatically

---
+++ packages/bundle/Gruntfile.js
@@ -0,0 +1,12 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ */
+
+'use strict';
+
+module.exports = function configGrunt(grunt) {
+  grunt.registerTask('build', []);
+
+  grunt.registerTask('test', []);
+};
