b609c6458d1d405c5dcc4ff2320bd3322ad4b312
Ian W. Remmel <design@ianwremmel.com>
2016-08-03 13:35:16 -0700

    feat(common): add cappedDebounce()

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -4,6 +4,7 @@
  */
 
 export {default as base64} from './base64';
+export {default as cappedDebounce} from './capped-debounce';
 export {default as checkRequired} from './check-required';
 export {default as deprecate} from './deprecate';
 export {default as oneFlight} from './one-flight';
