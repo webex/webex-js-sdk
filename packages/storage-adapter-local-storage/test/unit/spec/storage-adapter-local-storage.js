f02ccef016bf26964dc90d00443d464ad0eed823
Ian W. Remmel <design@ianwremmel.com>
2016-08-03 17:42:26 -0700

    feat(storage-adapter-local-storage): implement local storage adapter

---
+++ packages/storage-adapter-local-storage/test/unit/spec/storage-adapter-local-storage.js
@@ -0,0 +1,13 @@
+/**!
+ *
+ * Copyright (c) 2015 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import runAbstractStorageAdapterSpec from '@ciscospark/storage-adapter-spec';
+import {skipInNode} from '@ciscospark/test-helper-mocha';
+import StorageAdapterLocalStorage from '../..';
+
+skipInNode(describe)(`StorageAdapterLocalStorage`, () => {
+  runAbstractStorageAdapterSpec(new StorageAdapterLocalStorage(`test`));
+});
