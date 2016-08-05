7cb2f0ddc8ca9356c5bf505076d11d77cbaa2f39
Ian W. Remmel <design@ianwremmel.com>
2016-08-05 14:41:08 -0700

    test(spark-core): refactor @persist test, start implementing waitForValue test

--- packages/spark-core/src/spark-core.js
+++ packages/spark-core/src/spark-core.js
@@ -223,13 +223,3 @@ export function registerPlugin(name, constructor, options) {
     makeSparkConstructor();
   }
 }
-
-export {default as SparkHttpError} from './lib/spark-http-error';
-export {default as SparkPlugin} from './lib/spark-plugin';
-export {default as AuthInterceptor} from './plugins/credentials/auth-interceptor';
-export {default as NetworkTimingInterceptor} from './interceptors/network-timing';
-export {default as RedirectInterceptor} from './interceptors/redirect';
-export {default as RequestLoggerInterceptor} from './interceptors/request-logger';
-export {default as ResponseLoggerInterceptor} from './interceptors/response-logger';
-export {default as RequestTimingInterceptor} from './interceptors/request-timing';
-export {default as SparkTrackingIdInterceptor} from './interceptors/spark-tracking-id';
--- packages/spark-core/test/unit/spec/storage/decorators.js
+++ packages/spark-core/test/unit/spec/storage/decorators.js
@@ -3,14 +3,17 @@
  * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
  */
 
+/* eslint camelcase: [0] */
+
 import {assert} from '@ciscospark/test-helper-chai';
 import sinon from '@ciscospark/test-helper-sinon';
-import {default as Spark} from '../../..';
+import Spark from '../../..';
 
 describe(`spark-core`, () => {
-  describe(`Decorators`, () => {
-    it(`works`, () => {
+  describe(`@persist`, () => {
+    it(`writes the identified value into the store when the value changes`, () => {
       const spark = new Spark();
+
       sinon.stub(spark, `request`).returns(Promise.resolve({
         body: {
           access_token: `fake token`,
@@ -25,6 +28,39 @@ describe(`spark-core`, () => {
             access_token: `fake token`,
             token_type: `Bearer`
           });
+        });
+    });
+  });
+
+  describe(`@waitForValue`, () => {
+    // TODO this should be tests with MockSpark
+    it.skip(`prevents the method from executing until the specified value changes`, () => {
+      const spark = new Spark();
+      spark.credentials.set({
+        authorization: {
+          refresh_token_expires: Date.now() - 10000,
+          refresh_token: `refresh_token`
+        }
+      });
+
+      sinon.stub(spark, `request`).returns(Promise.resolve({
+        body: {
+          access_token: `fake token @waitForValue`,
+          token_type: `Bearer`
+        }
+      }));
+
+      let resolve;
+      sinon.stub(spark.boundedStorage, `get`).returns(new Promise((r) => {resolve = r;}));
+
+
+      const promise = spark.credentials.getAuthorization();
+      assert.notCalled(spark.request);
+      resolve();
+      return (promise)
+        .then((auth) => {
+          assert.called(spark.request);
+          assert.equal(auth, `Bearer fake token @waitForValue`);
         });
     });
   });
