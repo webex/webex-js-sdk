905b476027aaf9bdc4d4ce4b32f58b58d5d5cdd2
Ian W. Remmel <design@ianwremmel.com>
2016-08-05 14:41:08 -0700

    test(spark-core): refactor @persist test, start implementing waitForValue test

--- packages/spark-core/src/index.js
+++ packages/spark-core/src/index.js
@@ -9,17 +9,24 @@ import './plugins/credentials';
 
 export {
   children,
+  makeSparkStore,
+  makeSparkPluginStore,
+  MemoryStoreAdapter
+} from './lib/storage';
+
+export {default as SparkHttpError} from './lib/spark-http-error';
+export {default as SparkPlugin} from './lib/spark-plugin';
+export {default as AuthInterceptor} from './plugins/credentials/auth-interceptor';
+export {default as NetworkTimingInterceptor} from './interceptors/network-timing';
+export {default as RedirectInterceptor} from './interceptors/redirect';
+export {default as RequestLoggerInterceptor} from './interceptors/request-logger';
+export {default as ResponseLoggerInterceptor} from './interceptors/response-logger';
+export {default as RequestTimingInterceptor} from './interceptors/request-timing';
+export {default as SparkTrackingIdInterceptor} from './interceptors/spark-tracking-id';
+
+export {
   default as default,
-  SparkHttpError,
-  SparkPlugin,
-  AuthInterceptor,
-  NetworkTimingInterceptor,
-  registerPlugin,
-  RedirectInterceptor,
-  ResponseLoggerInterceptor,
-  RequestLoggerInterceptor,
-  RequestTimingInterceptor,
-  SparkTrackingIdInterceptor
+  registerPlugin
 } from './spark-core';
 
 export {
