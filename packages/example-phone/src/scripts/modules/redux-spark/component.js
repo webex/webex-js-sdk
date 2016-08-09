a80f368622134cc1f781f1eeac73b24d3bbbfae1
Ian W. Remmel <design@ianwremmel.com>
2016-08-08 17:14:42 -0700

    chore(example-phone): remove app-layer caching from example-phone

--- packages/example-phone/src/scripts/modules/redux-spark/component.js
+++ packages/example-phone/src/scripts/modules/redux-spark/component.js
@@ -6,39 +6,21 @@ import injectSpark from './inject';
 
 import {
   connectToMercury,
-  receiveCredentials,
-  receiveDevice,
   updateState
 } from './actions';
 
 class SparkComponent extends Component {
   static propTypes = {
     connectToMercury: React.PropTypes.func.isRequired,
-    credentials: React.PropTypes.object,
-    device: React.PropTypes.object,
-    receiveCredentials: React.PropTypes.func.isRequired,
-    receiveDevice: React.PropTypes.func.isRequired,
     spark: React.PropTypes.object.isRequired,
     updateState: React.PropTypes.func.isRequired
   };
 
   componentDidMount() {
     const {
-      credentials,
-      device,
       updateState,
-      receiveDevice,
-      receiveCredentials,
       spark
     } = this.props;
-
-    if (credentials) {
-      spark.credentials.set(credentials);
-    }
-
-    if (device) {
-      spark.device.set(device);
-    }
 
     spark.listenToAndRun(spark, `change:isAuthenticated`, () => {
       updateState({authenticated: spark.isAuthenticated});
@@ -54,18 +36,6 @@ class SparkComponent extends Component {
 
     spark.mercury.listenToAndRun(spark.mercury, `change:connecting`, () => {
       updateState({connecting: spark.mercury.connecting});
-    });
-
-    spark.credentials.on(`change:authorization`, () => {
-      receiveCredentials(spark);
-    });
-
-    spark.device.on(`change:url`, () => {
-      receiveDevice(spark);
-    });
-
-    spark.device.on(`change:webSocketUrl`, () => {
-      receiveDevice(spark);
     });
   }
 
@@ -91,8 +61,6 @@ export default connect(
   (state) => state.spark,
   (dispatch) => bindActionCreators({
     connectToMercury,
-    receiveCredentials,
-    receiveDevice,
     updateState
   }, dispatch)
 )(injectSpark(SparkComponent));
