7eba61d81afbc370d52ebe951aa82fd22ea00c26
Ian W. Remmel <design@ianwremmel.com>
2016-08-11 22:51:01 -0700

    feat(state): being implement static extend method

--- packages/state/src/state.js
+++ packages/state/src/state.js
@@ -10,15 +10,23 @@
 
 import {
   deprecated,
+  nonenumerable,
   nonconfigurable,
   readonly
 } from 'core-decorators';
 
-import {escape} from 'lodash';
+import {escape, isString} from 'lodash';
 
 import {
+  child,
   computed,
-  evented
+  evented,
+  type,
+  required,
+  noNull,
+  setOnce,
+  values,
+  test
 } from './decorators';
 
 @evented
@@ -34,23 +42,23 @@ export default class State {
     // TODO set collection
     // TODO sett attrs
   }
-  //
-  // // config attributes
-  // @nonconfigurable
-  // @readonly
-  // extraProperties = `ignore`
-  //
-  // @nonconfigurable
-  // @readonly
-  // idAttribute = `id`
-  //
-  // @nonconfigurable
-  // @readonly
-  // namespaceAttribute = `namespace`
-  //
-  // @nonconfigurable
-  // @readonly
-  // typeAttribute = `modelType`
+
+  // config attributes
+  @nonconfigurable
+  @readonly
+  extraProperties = `ignore`
+
+  @nonconfigurable
+  @readonly
+  idAttribute = `id`
+
+  @nonconfigurable
+  @readonly
+  namespaceAttribute = `namespace`
+
+  @nonconfigurable
+  @readonly
+  typeAttribute = `modelType`
 
   initialize() {
 
@@ -68,8 +76,8 @@ export default class State {
     return this[this.typeAttribute];
   }
 
-  // @computed(function isNew() {return this.getId() === null;})
-  // isNew
+  @computed(function isNew() {return this.getId() === null;})
+  isNew
 
   @deprecated(`Consider investing on a more complete escaping function than lodash.escape`)
   escape(attr) {
@@ -151,7 +159,89 @@ export default class State {
     return true;
   }
 
-  static extend() {
+  static extend(def) {
+    // breaking: constructor can't be overridden
+    class Child extends State {}
+    const proto = Object.keys(def).reduce((p, key) => {
+      if (isFunction(def[key])) {
+        p[key] = def[key];
+      }
 
+      // TODO idAttribute
+      // TODO namespaceAttribute
+      // TODO typeAttribute
+      // TODO extraProperties
+      // TODO collection
+      // TODO cid
+
+      return p;
+    }, {});
+    Object.assign(Child.prototype, proto);
+    let props = {};
+
+    function prepare(propDef, prop) {
+      /* eslint complexity: [0] */
+      const desc = {};
+      if (isString(propDef)) {
+        type(propDef)(Child, prop, desc);
+      }
+
+      // Reminder: default must come first
+      if (propDef.default) {
+        propDef.initializer = propDef.default;
+      }
+      if (propDef.type) {
+        type(propDef.type)(Child, prop, desc);
+      }
+      if (propDef.required) {
+        required(Child, prop, desc);
+      }
+      if (propDef.allowNull) {
+        noNull(Child, prop, desc);
+      }
+      if (propDef.setOnce) {
+        setOnce(Child, prop, desc);
+      }
+      if (propDef.values) {
+        values(propDef.values)(Child, prop, desc);
+      }
+      if (propDef.test) {
+        test(propDef.test)(Child, prop, desc);
+      }
+
+      return desc;
+    }
+    if (def.session) {
+      props = Object.keys(def.session).reduce((p, prop) => {
+        const propDef = def.session[prop];
+        p[prop] = prepare(propDef, prop);
+        nonenumerable(Child, prop, p[prop]);
+        return p;
+      }, props);
+    }
+    if (def.props) {
+      props = Object.keys(def.props).reduce((p, prop) => {
+        const propDef = def.props[prop];
+        p[prop] = prepare(propDef, prop);
+        return p;
+      }, props);
+    }
+    if (def.derived) {
+      props = Object.keys(def.derived).reduce((p, prop) => {
+        const propDef = def.derived[prop];
+        p[prop] = computed(propDef);
+        return p;
+      }, props);
+    }
+    if (def.children) {
+      props = Object.keys(def.children).reduce((p, prop) => {
+        const propDef = def.children[prop];
+        p[prop] = child(propDef);
+        return p;
+      }, props);
+    }
+
+    // TODO dataTypes
+    // TODO collections
   }
 }
