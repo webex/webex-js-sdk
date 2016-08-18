a298b440c180831bd196a56c30f2280c89128118
Ian W. Remmel <design@ianwremmel.com>
2016-08-17 19:42:00 -0700

    chore(tooling): retry module tests suites

---
+++ docker/builder/retry.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set +e
+
+/work/test.sh
+EXIT_CODE=$?
+
+[ "${EXIT_CODE}" -eq "0" ]; then
+  exit ${EXIT_CODE}
+fi
+
+echo "Retrying ${PACKAGE} for the first time"
+/work/test.sh
+EXIT_CODE=$?
+
+[ "${EXIT_CODE}" -eq "0" ]; then
+  exit ${EXIT_CODE}
+fi
+
+echo "Retrying ${PACKAGE} for the second time"
+/work/test.sh
+EXIT_CODE=$?
+
+[ "${EXIT_CODE}" -eq "0" ]; then
+  exit ${EXIT_CODE}
+fi
