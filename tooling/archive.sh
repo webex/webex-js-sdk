32b2ca3e34e83d2c4a65c1f523a77c4abcb57441
Ian W. Remmel <design@ianwremmel.com>
2016-08-19 10:56:56 -0700

    refactor(tooling): don't generate artifacts.csv

--- tooling/archive.sh
+++ tooling/archive.sh
@@ -4,6 +4,4 @@ set -e
 
 cd "${SDK_ROOT_DIR}"
 
-find ./reports -type f | xargs ls -lh | awk '{print $5 "," $6 " " $7 " " $8 "," $9}' > artifacts.csv
 gzip -r ./reports
-mv artifacts.csv ./reports/artifacts.csv
