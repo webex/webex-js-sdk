9d603be31df78f89712f1f23509e8cf9a47205fb
Adam Weeks <adweeks@cisco.com>
2016-09-16 16:22:37 -0400

    test(widget-chat): basic test of ChatWidget

--- packages/widget-chat/Gruntfile.js
+++ packages/widget-chat/Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function configGrunt(grunt) {
         }
       }
     });
-  grunt.confg('jest',
+  grunt.config('jest',
     {
       options: {
         testPathPattern: /.*.test.js/
