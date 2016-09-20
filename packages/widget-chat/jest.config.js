96eb21633ec82e153309ae1592337a8c9870298e
Bernie Zang <nzang@cisco.com>
2016-09-20 18:46:32 -0400

    feat(widget-chat): add css-modules and tests to package
    Add PostCSS, CSSNext, CSSNano to support more modern and module styles
    build stack and CSS Module in new widgets. Mock non-js files to prevent
    non-js files from failing Jest tests.

--- packages/widget-chat/jest.config.js
+++ packages/widget-chat/jest.config.js
@@ -2,12 +2,16 @@
 var path = require('path');
 
 module.exports = {
-  testPathPattern: /.*.test.js/,
+  testPathPattern: `.*.test.js`,
   coverage: process.env.COVERAGE,
   config: {
     coverageDirectory: path.join(__dirname, '.coverage'),
     coveragePathIgnorePatterns: ['setup-jasmine-env.js'],
-    setupTestFrameworkScriptFile: path.join(__dirname, 'setup-jasmine-env.js')
+    setupTestFrameworkScriptFile: path.join(__dirname, 'setup-jasmine-env.js'),
+    moduleNameMapper: {
+      "^.+\\.(css|less)$": `<rootDir>/test/styleMock.js`,
+      "^.+\\.(gif|ttf|eot|svg)$": `<rootDir>/test/fileMock.js`
+    }
   },
   verbose: true
 };
