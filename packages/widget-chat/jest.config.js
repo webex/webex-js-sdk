6ff67a414b1583d23f695c5bcfe1873357427937
Adam Weeks <adweeks@cisco.com>
2016-09-20 14:46:31 -0400

    chore(widget-chat): JUnit coverage reporting

--- packages/widget-chat/jest.config.js
+++ packages/widget-chat/jest.config.js
@@ -1,10 +1,13 @@
+/* eslint-disable */
 var path = require('path');
 
 module.exports = {
   testPathPattern: /.*.test.js/,
   coverage: process.env.COVERAGE,
   config: {
-    coverageDirectory: path.join(__dirname, '.coverage')
+    coverageDirectory: path.join(__dirname, '.coverage'),
+    coveragePathIgnorePatterns: ['setup-jasmine-env.js'],
+    setupTestFrameworkScriptFile: path.join(__dirname, 'setup-jasmine-env.js')
   },
   verbose: true
 };
