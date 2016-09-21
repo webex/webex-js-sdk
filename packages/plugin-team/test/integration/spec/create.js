e1a4fa73d871480813acc2c243307e78dd2a8555
Josh Dykstra <joshuajdykstra@gmail.com>
2016-09-21 14:34:24 -0700

    test(plugin-team): Add integration test skeleton for plugin-team

---
+++ packages/plugin-team/test/integration/spec/create.js
@@ -0,0 +1,41 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import '../..';
+
+describe(`plugin-team`, function() {
+
+  describe(`#create()`, () => {
+
+    it(`creates a team with a name and summary`, () => {
+
+    });
+
+    it(`creates a team with a name but without a summary`, () => {
+
+    });
+  });
+
+  describe(`#createConversation()`, () => {
+
+    it(`creates a team conversation with a single participant`, () => {
+
+    });
+
+    it(`creates a team conversation with multiple participants`, () => {
+
+    });
+
+    it(`creates a team using the 'includeAllTeamMembers' parameter`, () => {
+
+    });
+
+    it(`decrypts the 'add' activity appended to the general conversation after team room is created`, () => {
+
+    });
+  });
+
+});
