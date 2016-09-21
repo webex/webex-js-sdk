b57e7b68c80aad03a21b80021a05126e3f27cb47
Josh Dykstra <joshuajdykstra@gmail.com>
2016-09-21 14:34:24 -0700

    test(plugin-team): Add integration test skeleton for plugin-team

---
+++ packages/plugin-team/test/integration/spec/actions.js
@@ -0,0 +1,48 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import '../..';
+
+describe(`plugin-team`, function() {
+  describe(`#addConversation()`, () => {
+    it(`adds an existing group conversation to a team`, () => {});
+  });
+
+  describe(`#addMember()`, () => {
+    it(`adds a team member to a team`, () => {});
+  });
+
+  describe(`#assignModerator()`, () => {
+    it(`assigns a team moderator`, () => {});
+  });
+
+  describe(`#archive()`, () => {
+    it(`archives a team`, () => {});
+    it(`archives a team conversation`, () => {});
+  });
+
+  describe(`#joinConversation()`, () => {
+    it(`adds the user to an open team conversation`, () => {});
+  });
+
+  describe(`#removeConversation()`, () => {
+    it(`removes a team conversation from a team`, () => {});
+  });
+
+  describe(`#removeMember()`, () => {
+    it(`removes a team member from a team`, () => {});
+  });
+
+  describe(`#unassignModerator()`, () => {
+    it(`unassigns a teamMember as a moderator`, () => {});
+  });
+
+  describe(`#update()`, () => {
+    it(`updates a team displayName`, () => {});
+    it(`updates a team summary`, () => {});
+    xit(`updates a team color`, () => {});
+  });
+});
---
+++ packages/plugin-team/test/integration/spec/get.js
@@ -0,0 +1,43 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import '../..';
+
+describe('plugin-team', function() {
+  describe(`#get()`, () => {
+    it(`retrieves a team`, () => {
+
+    });
+
+    it(`retrieves a team with teamMembers`, () => {
+
+    });
+
+    it(`retrieves a team with conversations`, () => {
+
+    });
+  });
+
+  describe(`#getConversations()`, () => {
+    it(`retrieves conversations for a team`, () => {
+
+    });
+  });
+
+  describe(`#list()`, () => {
+    it(`retrieves a list of teams`, () => {
+
+    });
+
+    it(`retrieves a list of teams with teamMembers`, () => {
+
+    });
+
+    it(`retrieves a list of teams with conversations`, () => {
+
+    });
+  });
+});
