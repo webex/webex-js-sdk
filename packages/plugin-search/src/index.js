4ae2b94767176ea10b12d844bc770be713868c06
Michael Wegman <mwegman@cisco.com>
2016-09-23 17:41:52 -0600

    feat(search): start implementing search package

---
+++ packages/plugin-search/src/index.js
@@ -0,0 +1,17 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import {registerPlugin} from '@ciscospark/spark-core';
+import Search from './search';
+import config from './config';
+
+import '@ciscospark/plugin-encryption';
+
+registerPlugin(`search`, Search, {
+  config
+});
+
+export {default as default} from './search';
