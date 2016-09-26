0c092f5006d861e948cebabceed9bf63c13313bd
Bernie Zang <nzang@cisco.com>
2016-09-26 16:15:23 -0400

    WIP

--- packages/widget-chat/src/containers/chat-widget/index.js
+++ packages/widget-chat/src/containers/chat-widget/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
 import classNames from 'classnames';
 
-import ActivityTitle from '../../components/activity-title';
 
+import AuthStatus from '../../components/auth-status';
+import ConnectionStatus from '../../components/connection-status';
+import ActivityTitle from '../../components/activity-title';
 import styles from './styles.css';
+
+import injectSpark from '../../modules/redux-spark/inject-spark';
 
 /**
  * ChatWidget Component
  */
-export default class ChatWidget extends React.Component {
-  /**
-   * Never update since we're not using any props yet
-   *
-   * @returns {Boolean}
-   */
+class ChatWidget extends Component {
+
   shouldComponentUpdate() {
     return false;
   }
@@ -24,10 +25,47 @@ export default class ChatWidget extends React.Component {
    * @returns {Object}
    */
   render() {
+    const {userId} = this.props;
+    const props = this.props;
+
+    console.log(props);
+
     return (
       <div className={classNames(`widget-chat`, styles.widgetChat)}>
-        <ActivityTitle heading="Chat Widget!" />
+        <ActivityTitle heading={userId} />
+        <ConnectionStatus id="connection-status" {...props} />
+        <AuthStatus id="auth-status" {...props} />
       </div>
     );
   }
 }
+
+ChatWidget.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  spark: React.PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  userId: PropTypes.string.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  const {user} = state;
+  const {
+    isFetching,
+    item
+  } = user || {
+    isFetching: true,
+    item: []
+  };
+
+  return {
+    user,
+    isFetching,
+    item,
+    userId: ownProps.userId
+  };
+}
+
+export default connect(
+  mapStateToProps
+)(injectSpark(ChatWidget));
---
+++ packages/widget-chat/src/store.js
@@ -0,0 +1,29 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+import user from './reducers';
+import sparkReducer from './modules/redux-spark/reducers';
+
+const devtools = window.devToolsExtension || (() => (noop) => noop);
+
+const enhancers = [
+  applyMiddleware(
+    thunk,
+    createLogger({
+      level: `info`,
+      duration: true,
+      collapsed: false,
+      logErrors: process.env.NODE_ENV === `production`
+    })
+  ),
+  devtools()
+];
+
+export default createStore(
+  combineReducers({
+    user,
+    spark: sparkReducer
+  }),
+  compose(...enhancers)
+);
