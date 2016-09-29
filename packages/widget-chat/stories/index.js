610e9ff9ba28987dc9c678c12049619f9085f181
Adam Weeks <adweeks@cisco.com>
2016-09-29 10:49:06 -0400

    chore(widget-chat): basic storybook implementation for components

--- packages/widget-chat/stories/index.js
+++ packages/widget-chat/stories/index.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import {storiesOf} from '@kadira/storybook';
-import ChatWidget from '../src/containers/chat-widget';
+
+import ActivityItem from '../src/components/activity-item';
+import ActivityList from '../src/components/activity-list';
+import ActivityReadReceipt from '../src/components/activity-read-receipt';
 import ActivityTitle from '../src/components/activity-title';
 import ActivityTitleBar from '../src/components/activity-title-bar';
 import Avatar from '../src/components/avatar';
+import MessageComposer from '../src/components/message-composer';
 
 const testUser = {
   userId: `bernie@gmail.net`
 };
 
-storiesOf(`ChatWidget`, module)
-  .add(`basic`, () =>
-    <ChatWidget />
-  );
+storiesOf(`ActivityItem`, module)
+  .add(`message`, () => {
+    const activity = {
+      id: `abc124`,
+      user: {
+        userId: `bernie@aol.net`,
+        isSelf: false
+      },
+      activity: {
+        message: `hey, what's up?!`
+      }
+    };
+    return <ActivityItem activity={activity} />;
+  });
 
+storiesOf(`ActivityList`, module)
+  .add(`messages`, () => {
+    const activities = [
+      {
+        id: `abc124`,
+        user: {
+          userId: `bernie@aol.net`,
+          isSelf: false
+        },
+        activity: {
+          message: `hey, what's up?!`
+        }
+      },
+      {
+        id: `abc125`,
+        user: {
+          userId: `earl@aol.net`,
+          isSelf: false
+        },
+        activity: {
+          message: `hello, how's it going?`
+        }
+      }
+    ];
+    return <ActivityList activities={activities} />;
+  });
+
+storiesOf(`ActivityReadReceipt`, module)
+  .add(`Basic`, () => {
+    const mockReadUsers = [{userId: `bernie`}, {userId: `adam`}];
+    return <ActivityReadReceipt actors={mockReadUsers}/>;
+  });
 
 storiesOf(`ActivityTitle`, module)
   .add(`Welcome Text`, () =>
@@ -37,3 +83,8 @@ storiesOf(`Avatar`, module)
       <Avatar user={dUser} />
     );
   });
+
+storiesOf(`MessageComposer`, module)
+  .add(`Basic`, () =>
+    <MessageComposer />
+  );
