c06a1f9eb206531ec153ff0f88a570f03f8bcdb0
Ian W. Remmel <design@ianwremmel.com>
2016-09-29 16:46:21 -0700

    feat(web-client-internal): add single package for web client to consume

---
+++ packages/web-client-internal/src/index.js
@@ -0,0 +1,24 @@
+/**!
+ *
+ * Copyright (c) 2015-2016 Cisco Systems, Inc. See LICENSE file.
+ * @private
+ */
+
+import '@ciscospark/plugin-avatar';
+import '@ciscospark/plugin-board';
+import '@ciscospark/plugin-conversation';
+import '@ciscospark/plugin-credentials';
+import '@ciscospark/plugin-encryption';
+import '@ciscospark/plugin-feature';
+import '@ciscospark/plugin-flag';
+import '@ciscospark/plugin-logger';
+import '@ciscospark/plugin-mashups';
+import '@ciscospark/plugin-mercury';
+import '@ciscospark/plugin-metrics';
+import '@ciscospark/plugin-search';
+import '@ciscospark/plugin-support';
+import '@ciscospark/plugin-team';
+import '@ciscospark/plugin-user';
+import '@ciscospark/plugin-wdm';
+
+export {default as CiscoSpark} from '@ciscospark/spark-core';
