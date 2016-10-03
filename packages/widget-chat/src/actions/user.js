3a20f067b211444af96be204b5765fe6a9946ef8
Adam Weeks <adweeks@cisco.com>
2016-10-03 12:14:59 -0400

    fix(widget-chat): remove unused user actions

--- packages/widget-chat/src/actions/user.js
+++ packages/widget-chat/src/actions/user.js
@@ -1,20 +1,3 @@
-export const REQUEST_USER = `REQUEST_USER`;
-export function requestUser(userId) {
-  return {
-    type: REQUEST_USER,
-    userId
-  };
-}
-
-export const RECEIVE_USER = `RECEIVE_USER`;
-export function receiveUser(userId, user) {
-  return {
-    type: RECEIVE_USER,
-    userId,
-    user
-  };
-}
-
 export const RECEIVE_CURRENT_USER = `RECEIVE_CURRENT_USER`;
 export function receiveCurrentUser(user) {
   return {
@@ -30,14 +13,5 @@ export function fetchCurrentUser(spark) {
       .then((user) => {
         dispatch(receiveCurrentUser(user));
       });
-  };
-}
-
-
-export function fetchUser(userEmail, spark) {
-  return (dispatch) => {
-    dispatch(requestUser(userEmail));
-    spark.user.getUUID(userEmail)
-      .then((user) => dispatch(receiveUser(userEmail, user)));
   };
 }
