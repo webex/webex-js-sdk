946a3c2e2b6692dc561822493b9df584e26d8f44
Adam Weeks <adweeks@cisco.com>
2016-10-04 15:33:18 -0400

    refactor(widget-chat): change title bar to stateless

--- packages/widget-chat/src/components/title-bar/index.js
+++ packages/widget-chat/src/components/title-bar/index.js
@@ -1,30 +1,22 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import classNames from 'classnames';
 
 import styles from './styles.css';
 
 import Avatar from '../avatar';
 
-class TitleBar extends Component {
-  shouldComponentUpdate(nextProps) {
-    return this.props.displayName !== nextProps.displayName;
-  }
-
-  render() {
-    const {displayName} = this.props;
-    return (
-      <div className={classNames(`title-bar`, styles.titleBar)}>
-        <div className={classNames(`avatar-container`, styles.avatarContainer)}>
-          <Avatar displayName={displayName} />
-        </div>
-        <h1 className={classNames(`title`, styles.title)}>{displayName}</h1>
+export default function TitleBar(props) {
+  const {displayName} = props;
+  return (
+    <div className={classNames(`title-bar`, styles.titleBar)}>
+      <div className={classNames(`avatar-container`, styles.avatarContainer)}>
+        <Avatar displayName={displayName} />
       </div>
-    );
-  }
+      <h1 className={classNames(`title`, styles.title)}>{displayName}</h1>
+    </div>
+  );
 }
 
 TitleBar.propTypes = {
   displayName: PropTypes.string.isRequired
 };
-
-export default TitleBar;
