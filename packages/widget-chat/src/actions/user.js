db59b1d3c83a53fe050842074ff1eef716fd9ee8
Bernie Zang <nzang@cisco.com>
2016-10-06 12:38:02 -0400

    fix(widget-chat): Fix multiple retrevals of current user

--- packages/widget-chat/src/actions/user.js
+++ packages/widget-chat/src/actions/user.js
@@ -2,12 +2,25 @@ export const RECEIVE_CURRENT_USER = `RECEIVE_CURRENT_USER`;
 export function receiveCurrentUser(user) {
   return {
     type: RECEIVE_CURRENT_USER,
+    isFetching: false,
     user
   };
 }
 
+export const UPDATE_CURRENT_USER_STATE = `UPDATE_CURRENT_USER_STATE`;
+export function updateCurrentUserState(state) {
+  return {
+    type: UPDATE_CURRENT_USER_STATE,
+    state
+  };
+}
+
+
 export function fetchCurrentUser(spark) {
   return (dispatch) => {
+    dispatch(updateCurrentUserState({
+      isFetching: true
+    }));
     spark.user.get()
       .then((user) => {
         dispatch(receiveCurrentUser(user));
