43b841b9379aee3099ff11ed10595f7ad28e6309
Ian W. Remmel <design@ianwremmel.com>
2016-10-13 11:01:05 -0700

    refactor(plugin-encryption): replace encryption interceptor with payload transformation

--- packages/plugin-encryption/src/index.js
+++ packages/plugin-encryption/src/index.js
@@ -10,18 +10,81 @@
 // by using Promise.race to resolve replays (as more requests get enqueue for a
 // specific action, accept whichever one completes first).
 
-import {registerPlugin} from '@ciscospark/spark-core';
+import {registerPlugin, SparkHttpError} from '@ciscospark/spark-core';
 import Encryption from './encryption';
 import config from './config';
-import EncryptionInterceptor from './interceptor';
+import {has} from 'lodash';
 
 import '@ciscospark/plugin-wdm';
 import '@ciscospark/plugin-mercury';
 
 registerPlugin(`encryption`, Encryption, {
-  interceptors: {
-    EncryptionInterceptor: EncryptionInterceptor.create
+  payloadTransformer: {
+    predicates: [{
+      name: `encryptKmsMessage`,
+      direction: `outbound`,
+      test(options) {
+        return Promise.resolve(has(options, `body.kmsMessage`));
+      },
+      extract(options) {
+        return Promise.resolve(options.body);
+      }
+    }, {
+      name: `decryptKmsMessage`,
+      direction: `inbound`,
+      test(response) {
+        return Promise.resolve(has(response, `body.kmsMessage`));
+      },
+      extract(response) {
+        return Promise.resolve(response.body);
+      }
+    }, {
+      name: `decryptErrorResponse`,
+      direction: `inbound`,
+      test(reason) {
+        return Promise.resolve(Boolean(reason.body && reason.body.errorCode === 1900000));
+      }
+    }],
+    transforms: [{
+      name: `encryptKmsMessage`,
+      fn(ctx, object) {
+        return ctx.spark.encryption.kms.prepareRequest(object.kmsMessage)
+          .then((req) => {
+            object.kmsMessage = req.wrapped;
+            return object;
+          });
+      }
+    }, {
+      name: `decryptKmsMessage`,
+      fn(ctx, object) {
+        return ctx.spark.encryption.kms.decryptKmsMessage(object.kmsMessage)
+          .then((kmsMessage) => {
+            object.kmsMessage = kmsMessage;
+            return object;
+          });
+      }
+    }, {
+      name: `decryptErrorResponse`,
+      fn(ctx, reason) {
+        const promises = reason.body.errors.map((error) => ctx.spark.encryption.kms.decryptKmsMessage(error.description)
+          .then((desc) => {
+            error.description = desc;
+          }));
+
+        promises.push(ctx.spark.encryption.kms.decryptKmsMessage(reason.body.message)
+          .then((kmsMessage) => {
+            reason.body.message = kmsMessage;
+          }));
+
+        return Promise.all(promises)
+          .then(() => {
+            const E = SparkHttpError.select(reason.statusCode);
+            return Promise.reject(new E(reason));
+          });
+      }
+    }]
   },
+
   config
 });
 
