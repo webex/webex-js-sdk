16ef90cff93b3c64e9ae2dd22431d5d4ca65cdb7
Adam Weeks <adweeks@cisco.com>
2016-10-18 11:59:16 -0400

    test(widget-chat): update existing snapshots

--- packages/widget-chat/src/components/activity-list/index.test.js
+++ packages/widget-chat/src/components/activity-list/index.test.js
@@ -6,22 +6,36 @@ import ActivityList from '.';
 describe(`ActivityList component`, () => {
   const activities = [{
     id: `test-123-123-123-123`,
-    content: `Test Activity Content 1`,
-    name: `Test User 1`,
-    timestamp: `2016-09-20T19:52:57.186Z`,
+    actor: {
+      id: 1,
+      displayName: `Test User 1`
+    },
+    object: {
+      displayName: `Test Activity Content 1`
+    },
+    published: `2016-09-20T19:52:57.186Z`,
     verb: `post`
   }, {
-    id: `test-456-456-456-456`,
-    content: `Test Activity Content 2`,
-    name: `Test User 2`,
-    timestamp: `2016-09-21T19:53:57.186Z`,
+    id: `test-456-123-456-123`,
+    actor: {
+      id: 2,
+      displayName: `Test User 2`
+    },
+    object: {
+      displayName: `Test Activity Content 2`
+    },
+    published: `2016-09-20T19:53:57.186Z`,
     verb: `post`
   }, {
-    id: `test-789-789-789-789`,
-    content: `Test Activity Content 3`,
-    isAdditional: true,
-    name: `Test User 2`,
-    timestamp: `2016-09-21T19:55:57.186Z`,
+    id: `test-789-123-789-123`,
+    actor: {
+      id: 3,
+      displayName: `Test User 3`
+    },
+    object: {
+      displayName: `Test Activity Content 3`
+    },
+    published: `2016-09-20T19:54:57.186Z`,
     verb: `post`
   }];
 
