d463bfff7b0924fe872cef0ef6f751a887ba0f3e
Adam Weeks <adweeks@cisco.com>
2016-10-19 12:46:45 -0400

    test(widget-chat): icons and is self parameters

--- packages/widget-chat/src/components/activity-item-post-actions/index.test.js
+++ packages/widget-chat/src/components/activity-item-post-actions/index.test.js
@@ -1,14 +1,29 @@
+/* eslint-disable max-nested-callbacks */
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import ActivityItemPostActions from '.';
 
 describe(`ActivityItemPostActions component`, () => {
-  const component = renderer.create(
-    <ActivityItemPostActions id="abc-123" />
+  const onDelete = jest.fn();
+  describe(`is self param`, () => {
+    const component = renderer.create(
+      <ActivityItemPostActions id="abc-123" isSelf="true" onDelete={onDelete} />
   );
 
-  it(`renders properly`, () => {
-    expect(component).toMatchSnapshot();
+    it(`renders properly`, () => {
+      expect(component).toMatchSnapshot();
+    });
   });
+
+  describe(`without is self param`, () => {
+    const component = renderer.create(
+      <ActivityItemPostActions id="abc-123" onDelete={onDelete} />
+  );
+
+    it(`renders properly`, () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
 });
--- packages/widget-chat/src/components/activity-item/index.test.js
+++ packages/widget-chat/src/components/activity-item/index.test.js
@@ -3,7 +3,26 @@ import renderer from 'react-test-renderer';
 
 import ActivityItem from '.';
 
-describe(`ActivityItem post component`, () => {
+describe(`ActivityItem post component is self`, () => {
+  const activity = {
+    id: `test-123-123-123-123`,
+    isSelf: `true`,
+    content: `Test Activity Content`,
+    name: `Test User`,
+    timestamp: `2016-09-20T19:52:57.186Z`,
+    verb: `post`
+  };
+
+  const component = renderer.create(
+    <ActivityItem {...activity} />
+  );
+
+  it(`renders properly`, () => {
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe(`ActivityItem post component not self`, () => {
   const activity = {
     id: `test-123-123-123-123`,
     content: `Test Activity Content`,
@@ -20,6 +39,7 @@ describe(`ActivityItem post component`, () => {
     expect(component).toMatchSnapshot();
   });
 });
+
 
 describe(`ActivityItem tombstone component`, () => {
   const activity = {
