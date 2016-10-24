9a55e06716c7e72fe44f2a3c686661f292d58c4d
Ian W. Remmel <design@ianwremmel.com>
2016-10-24 13:35:42 -0700

    refactor(plugin-flag): migrate flag plugin to use payload transformations

--- packages/plugin-flag/src/index.js
+++ packages/plugin-flag/src/index.js
@@ -8,9 +8,25 @@ import {registerPlugin} from '@ciscospark/spark-core';
 import Flag from './flag';
 import config from './config';
 import '@ciscospark/plugin-wdm';
+import {has} from 'lodash';
 
 registerPlugin(`flag`, Flag, {
-  config
+  config,
+  payloadTransformer: {
+    predicates: [
+      {
+        name: `transformObjectArray`,
+        direction: `inbound`,
+        test(ctx, response) {
+          return Promise.resolve(has(response, `body.multistatus`));
+        },
+        extract(response) {
+          return Promise.resolve(response.body.multistatus.map((item) => item.data.activity));
+        }
+      }
+    ],
+    transforms: []
+  }
 });
 
 export {default as default} from './flag';
