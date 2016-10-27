47669a4d5cdb132b7c14fd309e2eecce2b8003f8
Josh Dykstra <joshuajdykstra@gmail.com>
2016-10-27 14:39:40 -0700

    feature(plugin-presence): Presence store for caching user presence

--- packages/plugin-presence/src/presence-store.js
+++ packages/plugin-presence/src/presence-store.js
@@ -4,6 +4,8 @@
  * @private
  */
 
+import {patterns} from '@ciscospark/common';
+
 /**
  * A Presence object returned from the service.
  *
@@ -16,7 +18,7 @@
  *     {string} [expires] ISO-8601 UTC DT when this status state will automatically change
  *                        optional, currently returned for current user only
  *                        changing name and functionality shortly
- *  }
+ * }
  */
 
  /**
@@ -37,30 +39,86 @@ export default class PresenceStore {
    * Adds a user's presence object to the store.
    *
    * @param {Object} presence
-   * @param {string} user.id
+   * @param {string} presence.subject id of user
    * @returns {Promise} resolves with the newly added user presence.
    */
   add(presence) {
+    if (!presence) {
+      return Promise.reject(new Error(`\`presence\` is a required parameter`));
+    }
+    if (!presence.subject) {
+      return Promise.reject(new Error(`\`presence.subject\` is a required parameter`));
+    }
+    if (!patterns.uuid.test(presence.subject)) {
+      return Promise.reject(new Error(`\`presence.subject\` does not appear to be a uuid`));
+    }
+    if (!presence.status) {
+      return Promise.reject(new Error(`\`presence.status\` is a required parameter`));
+    }
+    if (!presence.statusTime) {
+      return Promise.reject(new Error(`\`presence.statusTime\` is a required parameter`));
+    }
+    if (!presence.lastActive) {
+      return Promise.reject(new Error(`\`presence.lastActive\` is a required parameter`));
+    }
 
+    const ttl = presence.expires || this.config.cacheExpiration;
+    setTimeout(this.remove.bind(this, presence.subject), ttl);
+    return Promise.resolve(presence);
   },
 
   /**
    * Retrieves a user's presence from the store.
    *
-   * @param {Object} user
+   * @param {Object|string} user
+   * @param {string} user.id
    * @returns {Promise} resolves with the user's presence.
    */
   get(user) {
+    if (!user) {
+      return Promise.reject(new Error(`\`user\` is a required parameter`));
+    }
 
+    const id = user.id || user;
+
+    if (!id) {
+      return Promise.reject(new Error(`\`user.id\` is a required parameter`));
+    }
+
+    if (!patterns.uuid.test(id)) {
+      return Promise.reject(new Error(`\`user.id\` does not appear to be a uuid`));
+    }
+
+    const cachedPresence = presenceById.get(this).get(id);
+    if (cachedPresence) {
+      return Promise.resolve(cachedPresence);
+    }
+    return Promise.reject(new Error(`No Presence found for specified user`));
   },
 
   /**
    * Removes a user's presence from the store.
    *
-   * @param {object} user
+   * @param {Object|string} user
+   * @param {string} user.id
    * @returns {Promise} resolves with true
    */
-  remove() {
+  remove(user) {
+    if (!user) {
+      return Promise.reject(new Error(`\`user\` is a required parameter`));
+    }
+
+    const id = user.id || user;
+
+    if (!id) {
+      return Promise.reject(new Error(`\`user.id\` is a required parameter`));
+    }
+
+    if (!patterns.uuid.test(id)) {
+      return Promise.reject(new Error(`\`user.id\` does not appear to be a uuid`));
+    }
+
+    presenceById.get(this).delete(id);
     return Promise.resolve(true);
   }
 
