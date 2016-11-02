017bafd3e3edfaa46b9ae46ab24005ea469bab64
Adam Weeks <adweeks@cisco.com>
2016-11-02 12:07:54 -0400

    refactor(widget-chat): change indicator actions to FSA

--- packages/widget-chat/src/actions/indicators.js
+++ packages/widget-chat/src/actions/indicators.js
@@ -2,7 +2,9 @@ export const ADD_TYPING_INDICATOR = `ADD_TYPING_INDICATOR`;
 export function addTyping(userId) {
   return {
     type: ADD_TYPING_INDICATOR,
-    userId
+    payload: {
+      userId
+    }
   };
 }
 
@@ -10,7 +12,9 @@ export const DELETE_TYPING_INDICATOR = `DELETE_TYPING_INDICATOR`;
 export function deleteTyping(userId) {
   return {
     type: DELETE_TYPING_INDICATOR,
-    userId
+    payload: {
+      userId
+    }
   };
 }
 
