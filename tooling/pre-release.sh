b3a6d44415207a387bf3faa1002be02fd49e7f74
Ian W. Remmel <design@ianwremmel.com>
2016-11-01 18:13:54 -0700

    chore(tooling): fix path reference for lerna.json

--- tooling/pre-release.sh
+++ tooling/pre-release.sh
@@ -6,7 +6,7 @@ set -e
 
 VERSION=$(node <<EOF
 var fs = require('fs');
-var lerna = JSON.parse(fs.readFileSync('./lerna.json'));
+var lerna = JSON.parse(fs.readFileSync('../lerna.json'));
 var version = lerna.version;
 version = version.split('.');
 console.log(version[0] + '.' + version[1] + '.' + (parseInt(version[2], 10) + 1));
