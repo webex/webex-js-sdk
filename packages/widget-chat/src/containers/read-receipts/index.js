44010195638e59c36877b5985409ff89ff8e1c6f
Adam Weeks <adweeks@cisco.com>
2016-11-08 16:33:00 -0500

    feat(widget-chat): implement TypingAvatar in read receipts

--- packages/widget-chat/src/containers/read-receipts/index.js
+++ packages/widget-chat/src/containers/read-receipts/index.js
@@ -3,12 +3,12 @@ import {connect} from 'react-redux';
 
 import {getMostRecentReadReceipts} from '../../selectors/conversation';
 
-import Avatar from '../../components/avatar';
+import TypingAvatar from '../typing-avatar';
 
 class ReadReceipts extends Component {
   shouldComponentUpdate(nextProps) {
     const {props} = this;
-    return nextProps.readUsers !== props.readUsers || nextProps.user !== props.user;
+    return nextProps.readUsers !== props.readUsers || nextProps.user !== props.user || nextProps.typing !== props.typing;
   }
 
   componentDidUpdate() {
@@ -18,12 +18,25 @@ class ReadReceipts extends Component {
   render() {
     const {props} = this;
     const {avatars, currentUser} = props.user;
+    const typing = props.typing;
     const users = props.readUsers
       .filter((user) => user.id !== currentUser.id)
       .map((user) => {
         const {displayName} = user;
         const image = avatars[user.id];
-        return <Avatar image={image} key={user.id} name={displayName} />;
+        // Typing events don't give us user IDs, only emails.
+        const isTyping = typing.includes(user.emailAddress);
+        /* eslint-disable-reason React.*/
+        /* eslint-disable no-extra-parens */
+        return (
+          <TypingAvatar
+            image={image}
+            isTyping={isTyping}
+            key={user.id}
+            name={displayName}
+          />
+        );
+        /* eslint-enable no-extra-parens */
       });
 
     return (
@@ -36,13 +49,14 @@ class ReadReceipts extends Component {
 
 function mapStateToProps(state) {
   return {
+    typing: state.indicators.typing,
     user: state.user,
     readUsers: getMostRecentReadReceipts(state)
   };
 }
 
 ReadReceipts.propTypes = {
-  readUsers: PropTypes.object,
+  readUsers: PropTypes.array,
   user: PropTypes.object
 };
 
