3d1c4625350990e63b65c08696ec78f7b604c4b0
Adam Weeks <adweeks@cisco.com>
2016-11-10 13:49:07 -0500

    feat(widget-chat): add display of markdown content

--- packages/widget-chat/src/components/activity-item-post/index.js
+++ packages/widget-chat/src/components/activity-item-post/index.js
@@ -10,13 +10,18 @@ export default function ActivityItemPost(props) {
     content
   } = props;
 
+  /* eslint-disable-reason content is considered safe from server */
+  /* eslint-disable react/no-danger */
+  const htmlContent = {__html: content};
   return (
     <ActivityItemBase {...props}>
-      <div className={classNames(`activity-text`, styles.activityText)}>
-        {content}
-      </div>
+      <div
+        className={classNames(`activity-text`, styles.activityText)}
+        dangerouslySetInnerHTML={htmlContent}
+      />
     </ActivityItemBase>
   );
+  /* eslint-enable react/no-danger */
 }
 
 ActivityItemPost.propTypes = {
