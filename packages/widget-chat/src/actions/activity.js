0fb6a4ba871b0682deff78bd840c90529749811e
Bernie Zang <nzang@cisco.com>
2016-11-23 10:14:47 -0500

    fix(widget-chat): Fix missed merge conflict

--- packages/widget-chat/src/actions/activity.js
+++ packages/widget-chat/src/actions/activity.js
@@ -1,7 +1,6 @@
 import marked from 'marked';
 import {filterSync} from '@ciscospark/helper-html';
 
-<<<<<<< HEAD
 import {
   constructImage,
   isImage,
@@ -24,13 +23,6 @@ export function updateActivityStatus(status) {
   return {
     type: UPDATE_ACTIVITY_STATUS,
     payload: {
-=======
-export const UPDATE_ACTIVITY_STATUS = `UPDATE_ACTIVITY_STATUS`;
-export function updateActivityStatus(status) {
-  return {
-    type: UPDATE_ACTIVITY_STATUS,
-    payload: {
->>>>>>> widget-dev
       status
     }
   };
