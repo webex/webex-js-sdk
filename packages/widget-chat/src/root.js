a16261c5984f66c6127efcfa77975db728390112
Adam Weeks <adweeks@cisco.com>
2016-11-28 15:58:12 -0500

    feat(widget-chat): add messages for default en locale

--- packages/widget-chat/src/root.js
+++ packages/widget-chat/src/root.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import {addLocaleData, IntlProvider} from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
-
+import messages from './locales/en';
 import ChatWidget from './containers/chat-widget';
 import store from './store';
 
@@ -11,7 +11,7 @@ export default function Root({accessToken, userId}) {
 
   return (
     <Provider store={store}>
-      <IntlProvider locale={`en`}>
+      <IntlProvider locale={`en`} messages={messages}>
         <ChatWidget accessToken={accessToken} userId={userId} />
       </IntlProvider>
     </Provider>
