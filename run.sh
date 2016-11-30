f17e3ae2558d0926ac91b456d6a00d648ed58cfd
Ian W. Remmel <iremmel@cisco.com>
2016-11-30 14:18:48 -0800

    chore(tooling): simplify passing env vars to aux containers, clean bashlint in run.sh

--- run.sh
+++ run.sh
@@ -67,51 +67,32 @@ echo "##########################################################################
 # PREPARE DOCKER ENVIRONMENT
 #
 
-cd $(dirname $0)
-export SDK_ROOT_DIR="$(pwd)"
-export WORKDIR="${SDK_ROOT_DIR}"
+cd "$(dirname $0)"
+SDK_ROOT_DIR="$(pwd)"
+export SDK_ROOT_DIR
+WORKDIR="${SDK_ROOT_DIR}"
+export WORKDIR
 
 # Pass environment variables to container at runtime
-DOCKER_RUN_ENV=""
-if [ -n "${CONVERSATION_SERVICE}" ]; then
-  DOCKER_RUN_ENV+=" -e CONVERSATION_SERVICE=${CONVERSATION_SERVICE} "
-fi
-if [ -n "${DEVICE_REGISTRATION_URL}" ]; then
-  DOCKER_RUN_ENV+=" -e DEVICE_REGISTRATION_URL=${DEVICE_REGISTRATION_URL} "
-fi
-if [ -n "${ATLAS_SERVICE_URL}" ]; then
-  DOCKER_RUN_ENV+=" -e ATLAS_SERVICE_URL=${ATLAS_SERVICE_URL} "
-fi
-if [ -n "${HYDRA_SERVICE_URL}" ]; then
-  DOCKER_RUN_ENV+=" -e HYDRA_SERVICE_URL=${HYDRA_SERVICE_URL} "
-fi
-if [ -n "${WDM_SERVICE_URL}" ]; then
-  DOCKER_RUN_ENV+=" -e WDM_SERVICE_URL=${WDM_SERVICE_URL} "
-fi
-if [ -n "${ENABLE_NETWORK_LOGGING}" ]; then
-  DOCKER_RUN_ENV+=" -e ENABLE_NETWORK_LOGGING=${ENABLE_NETWORK_LOGGING} "
-fi
-if [ -n "${ENABLE_VERBOSE_NETWORK_LOGGING}" ]; then
-  DOCKER_RUN_ENV+=" -e ENABLE_VERBOSE_NETWORK_LOGGING=${ENABLE_VERBOSE_NETWORK_LOGGING} "
-fi
-if [ -n "${BUILD_NUMBER}" ]; then
-  DOCKER_RUN_ENV+=" -e BUILD_NUMBER=${BUILD_NUMBER} "
-fi
-if [ -n "${PIPELINE}" ]; then
-  DOCKER_RUN_ENV+=" -e PIPELINE=${PIPELINE} "
-fi
-if [ -n "${COMMON_IDENTITY_OAUTH_SERVICE_URL}" ]; then
-  DOCKER_RUN_ENV+=" -e COMMON_IDENTITY_OAUTH_SERVICE_URL=${COMMON_IDENTITY_OAUTH_SERVICE_URL} "
-fi
-if [ -n "${SKIP_FLAKY_TESTS}" ]; then
-  DOCKER_RUN_ENV+=" -e SKIP_FLAKY_TESTS=${SKIP_FLAKY_TESTS} "
-fi
-if [ -n "${SAUCE_IS_DOWN}" ]; then
-  DOCKER_RUN_ENV+=" -e SAUCE_IS_DOWN=${SAUCE_IS_DOWN} "
-fi
-if [ -n "${SDK_BUILD_DEBUG}" ]; then
-  DOCKER_RUN_ENV+=" -e SDK_BUILD_DEBUG=${SDK_BUILD_DEBUG} "
-fi
+
+rm -f docker-env
+DOCKER_ENV_KEYS=""
+DOCKER_ENV_KEYS+="ATLAS_SERVICE_URL "
+DOCKER_ENV_KEYS+="BUILD_NUMBER "
+DOCKER_ENV_KEYS+="CONVERSATION_SERVICE "
+DOCKER_ENV_KEYS+="COMMON_IDENTITY_OAUTH_SERVICE_URL "
+DOCKER_ENV_KEYS+="DEVICE_REGISTRATION_URL "
+DOCKER_ENV_KEYS+="ENABLE_NETWORK_LOGGING "
+DOCKER_ENV_KEYS+="ENABLE_VERBOSE_NETWORK_LOGGING "
+DOCKER_ENV_KEYS+="HYDRA_SERVICE_URL "
+DOCKER_ENV_KEYS+="PIPELINE "
+DOCKER_ENV_KEYS+="SAUCE_IS_DOWN "
+DOCKER_ENV_KEYS+="SDK_BUILD_DEBUG "
+DOCKER_ENV_KEYS+="SKIP_FLAKY_TESTS "
+DOCKER_ENV_KEYS+="WDM_SERVICE_URL "
+for KEY in $DOCKER_ENV_KEYS; do
+  env | grep "${KEY}" >> docker-env
+done
 
 export DOCKER_CONTAINER_NAME="${JOB_NAME}-${BUILD_NUMBER}-builder"
 
@@ -123,35 +104,38 @@ WORKDIR ${WORKDIR}
 USER $(id -u)
 EOT
 
-set +e
-docker build -t ${DOCKER_CONTAINER_NAME} ./docker/builder
-EXIT_CODE=$?
-if [ "${EXIT_CODE}" -ne "0" ]; then
-  echo "Docker build failed, making attempt 2"
+for DOCKER_BUILD_ITERATION in $(seq 1 3); do
+  echo "Docker build ${DOCKER_BUILD_ITERATION}: building"
+
+  set +e
   docker build -t ${DOCKER_CONTAINER_NAME} ./docker/builder
   EXIT_CODE=$?
-  if [ "${EXIT_CODE}" -ne "0" ]; then
-    echo "Docker build failed, making attempt 3"
-    docker build -t ${DOCKER_CONTAINER_NAME} ./docker/builder
-    EXIT_CODE=$?
+  if [ "${EXIT_CODE}" == "0" ]; then
+    echo "Docker build ${DOCKER_BUILD_ITERATION}: succeeded"
+    break
   fi
+  echo "Docker build ${DOCKER_BUILD_ITERATION}: failed"
+done
+
+if [ "${EXIT_CODE}" -ne "0" ]; then
+  exit 30
 fi
-set -e
 
 # Reset the Dockerfile to make sure we don't accidentally commit it later
 git checkout ./docker/builder/Dockerfile
 
-export DOCKER_RUN_OPTS="${DOCKER_RUN_ENV}"
+DOCKER_RUN_OPTS="--env-file docker-env"
 # Cleanup the container when done
-export DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --rm"
+DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --rm"
 # Make sure the npm cache stays inside the workspace
-export DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} -e NPM_CONFIG_CACHE=${WORKDIR}/.npm"
+DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} -e NPM_CONFIG_CACHE=${WORKDIR}/.npm"
 # Mount the workspace from the Jenkins slave volume
-export DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --volumes-from ${HOSTNAME}"
+DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --volumes-from ${HOSTNAME}"
 # Run commands as Jenkins user
-export DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --user=$(id -u):$(id -g)"
+DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} --user=$(id -u):$(id -g)"
 # Use the computed container name
-export DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} ${DOCKER_CONTAINER_NAME}"
+DOCKER_RUN_OPTS="${DOCKER_RUN_OPTS} ${DOCKER_CONTAINER_NAME}"
+export DOCKER_RUN_OPTS
 
 #
 # MAKE SECRETS AVAILABLE TO AUX CONTAINERS
@@ -184,4 +168,4 @@ trap "docker rmi ${DOCKER_CONTAINER_NAME}" EXIT
 #
 # RUN THE COMMAND THAT WAS PASSED TO THIS SCRIPT
 #
-eval $@
+eval "$@"
