0cab496cb60524839282025ad015fd12a3a037f8
Adam Weeks <adweeks@cisco.com>
2016-12-02 10:33:10 -0500

    test(widget-chat): expand ListSeparator test coverage

--- packages/widget-chat/src/components/list-separator/index.test.js
+++ packages/widget-chat/src/components/list-separator/index.test.js
@@ -4,13 +4,23 @@ import renderer from 'react-test-renderer';
 import ListSeparator from '.';
 
 describe(`ListSeparator component`, () => {
-  const component = renderer.create(
-    <ListSeparator
-      primaryText={`Testing 123`}
-    />
-  );
-
   it(`renders properly`, () => {
+    const component = renderer.create(
+      <ListSeparator
+        primaryText={`Testing 123`}
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it(`renders properly with classes`, () => {
+    const classNames = [`bold`, `blue`];
+    const component = renderer.create(
+      <ListSeparator
+        classNames={classNames}
+        primaryText={`Testing 123`}
+      />
+    );
     expect(component).toMatchSnapshot();
   });
 });
