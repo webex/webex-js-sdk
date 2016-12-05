66e77558689ecef41de7e8107aea95ea6eded9dc
saurjai3 <saurjai3@cisco.com>
2016-12-05 11:02:46 -0800

    refactor(helper-image): modified test cases as per review feedback

--- packages/helper-image/test/unit/spec/image-helper.browser.js
+++ packages/helper-image/test/unit/spec/image-helper.browser.js
@@ -4,7 +4,7 @@
 */
 
 import {assert} from '@ciscospark/test-helper-chai';
-import {drawImage, fixImageOrientation, readExifData, orient} from '../..';
+import {readExifData, orient} from '../..';
 import file from '@ciscospark/test-helper-file';
 import sinon from '@ciscospark/test-helper-sinon';
 describe(`helper-image`, function() {
@@ -36,55 +36,65 @@ describe(`helper-image`, function() {
   });
 
   describe(`orient()`, () => {
-    it(`rotates/flips the image on the canvas as per exif information`, () => {
-      const options = {
-        img: `Portrait_7.jpg`,
-        x: 0,
-        y: 0,
-        width: 362,
-        height: 300,
-        ctx: {
-          save: sinon.stub().returns(() => true),
-          translate: sinon.stub().returns(() => true),
-          rotate: sinon.stub().returns(() => true),
-          scale: sinon.stub().returns(() => true),
-          drawImage: sinon.stub().returns(() => true),
-          restore: sinon.stub().returns(() => true)
-        }
-      };
-      [3, 4, 5, 6, 7, 8].forEach((index) => {
-        options.orientation = index;
-        orient(options);
-        assert.isTrue(options.ctx.save.called);
-        assert.isTrue(options.ctx.translate.calledWith(options.x + options.width / 2, options.y + options.height / 2));
-        assert.isTrue(options.ctx.drawImage.calledWith(options.img, -options.width / 2, -options.height / 2, options.width, options.height));
-        assert.isTrue(options.ctx.restore.called);
-        switch (index) {
-        case 3:
-          assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 180 * Math.PI / 180));
-          break;
-        case 4:
-          assert.isTrue(options.ctx.rotate.calledWith(180 * Math.PI / 180));
-          assert.isTrue(options.ctx.scale.calledWith(-1, 1));
-          break;
-        case 5:
-          assert.isTrue(options.ctx.rotate.calledWith(270 * Math.PI / 180));
-          assert.isTrue(options.ctx.scale.calledWith(-1, 1));
-          break;
-        case 6:
-          assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 270 * Math.PI / 180));
-          break;
-        case 7:
-          assert.isTrue(options.ctx.rotate.calledWith(90 * Math.PI / 180));
-          assert.isTrue(options.ctx.scale.calledWith(-1, 1));
-          break;
-        case 8:
-          assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 90 * Math.PI / 180));
-          break;
-        default:
-          break;
-        }
-      });
+    const options = {
+      img: `Portrait_7.jpg`,
+      x: 0,
+      y: 0,
+      width: 362,
+      height: 300,
+      ctx: {
+        save: sinon.stub().returns(() => true),
+        translate: sinon.stub().returns(() => true),
+        rotate: sinon.stub().returns(() => true),
+        scale: sinon.stub().returns(() => true),
+        drawImage: sinon.stub().returns(() => true),
+        restore: sinon.stub().returns(() => true)
+      }
+    };
+    [1, 2, 3, 4, 5, 6, 7, 8].forEach(/* eslint complexity: ["error", 9] */ (orientation) => {
+      let msg;
+      options.orientation = orientation;
+      orient(options);
+      assert.isTrue(options.ctx.save.called);
+      assert.isTrue(options.ctx.translate.calledWith(options.x + options.width / 2, options.y + options.height / 2));
+      assert.isTrue(options.ctx.drawImage.calledWith(options.img, -options.width / 2, -options.height / 2, options.width, options.height));
+      assert.isTrue(options.ctx.restore.called);
+      switch (orientation) {
+      case 2:
+        msg = `flipImage`;
+        break;
+      case 3:
+        msg = `rotateImage180`;
+        assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 180 * Math.PI / 180));
+        break;
+      case 4:
+        msg = `rotate180AndFlipImage`;
+        assert.isTrue(options.ctx.rotate.calledWith(180 * Math.PI / 180));
+        assert.isTrue(options.ctx.scale.calledWith(-1, 1));
+        break;
+      case 5:
+        msg = `rotate270AndFlipImage`;
+        assert.isTrue(options.ctx.rotate.calledWith(270 * Math.PI / 180));
+        assert.isTrue(options.ctx.scale.calledWith(-1, 1));
+        break;
+      case 6:
+        msg = `rotateImage270`;
+        assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 270 * Math.PI / 180));
+        break;
+      case 7:
+        msg = `rotateNeg90AndFlipImage`;
+        assert.isTrue(options.ctx.rotate.calledWith(90 * Math.PI / 180));
+        assert.isTrue(options.ctx.scale.calledWith(-1, 1));
+        break;
+      case 8:
+        msg = `rotateNeg90`;
+        assert.isTrue(options.ctx.rotate.calledWith(2 * Math.PI - 90 * Math.PI / 180));
+        break;
+      default:
+        msg = `do nothing`;
+        break;
+      }
+      it(`${msg} on the canvas if image orientation is ${orientation}`, /* eslint no-empty-function: 0 */ () => {});
     });
   });
 
--- test/unit/spec/util/image-helper.js
+++ test/unit/spec/util/image-helper.js
@@ -34,59 +34,69 @@ describe('readExifData()', function() {
 });
 
 describe('orient()', function() {
-  it('rotates/flips the image on the canvas as per exif information', /* eslint complexity: ["error", 9] */ function() {
-    ImageHelper.drawImage = sinon.spy();
-    var sampleImageOptions = {
-      img: 'SampleImage.jpg',
+  ImageHelper.drawImage = sinon.spy();
+  var sampleImageOptions = {
+    img: 'SampleImage.jpg',
+    x: 0,
+    y: 0,
+    width: 362,
+    height: 300,
+    ctx: {}
+  };
+  [1, 2, 3, 4, 5, 6, 7, ].forEach(function(orientation) {
+    var image = {
+      img: sampleImageOptions.img,
       x: 0,
       y: 0,
-      width: 362,
-      height: 300,
-      ctx: {}
+      width: sampleImageOptions.width,
+      height: sampleImageOptions.height,
+      deg: 0,
+      flip: true,
+      ctx: sampleImageOptions.ctx
     };
-
-    for (var index=3; index<=8; index++) {
-      // just changing the orientation to make sure that the function is correctly processing the inputs
-      sampleImageOptions.orientation = index;
-      ImageHelper.orient(sampleImageOptions);
-      var image = {
-        img: sampleImageOptions.img,
-        x: 0,
-        y: 0,
-        width: sampleImageOptions.width,
-        height: sampleImageOptions.height,
-        deg: 0,
-        flip: true,
-        ctx: sampleImageOptions.ctx
-      };
-
-      switch(index) {
-        case 3:
-          image.deg = 180;
-          image.flip = false;
-          break;
-        case 4:
-          image.deg = 180;
-          image.flip = true;
-          break;
-        case 5:
-          image.deg = 270;
-          image.flip = true;
-          break;
-        case 6:
-          image.deg = 270;
-          image.flip = false;
-          break;
-        case 7:
-          image.deg = 90;
-          image.flip = true;
-          break;
-        case 8:
-          image.deg = 90;
-          image.flip = false;
-          break;
-      }
-      assert(ImageHelper.drawImage.calledWith(image));
+    var msg;
+    // just changing the orientation to make sure that the function is correctly processing the inputs
+    sampleImageOptions.orientation = orientation;
+    ImageHelper.orient(sampleImageOptions);
+    switch(orientation) {
+      case 2:
+        msg = 'flipImage';
+        break;
+      case 3:
+        msg = 'rotateImage180';
+        image.deg = 180;
+        image.flip = false;
+        break;
+      case 4:
+        msg = 'rotate180AndFlipImage';
+        image.deg = 180;
+        image.flip = true;
+        break;
+      case 5:
+        msg = 'rotate270AndFlipImage';
+        image.deg = 270;
+        image.flip = true;
+        break;
+      case 6:
+        msg = 'rotateImage270';
+        image.deg = 270;
+        image.flip = false;
+        break;
+      case 7:
+        msg = 'rotateNeg90AndFlipImage';
+        image.deg = 90;
+        image.flip = true;
+        break;
+      case 8:
+        msg = 'rotateNeg90';
+        image.deg = 90;
+        image.flip = false;
+        break;
+      default:
+        msg = 'do nothing';
+        break;
     }
+    assert(ImageHelper.drawImage.calledWith(image));
+    it(msg + ' on the canvas if image orientation is ' + orientation, /* eslint complexity: ["error", 9] */ function() {});
   });
 });
