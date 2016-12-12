97ea12a203c2c9f0ee5094508c0f718be72c1217
Bernie Zang <nzang@cisco.com>
2016-12-12 12:59:53 -0500

    fix(widget-chat): Remove injection of ENV VARS into bundles

--- packages/widget-message-meet/src/app.js
+++ packages/widget-message-meet/src/app.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
 
 import Root from './root';
 
 import './styles/main.css';
 
-const defaultConfig = {
-  accessToken: process.env.CISCOSPARK_ACCESS_TOKEN,
-  toPersonEmail: process.env.TO_PERSON_EMAIL,
-  toPersonId: process.env.TO_PERSON_ID,
-  env: process.env.ENV_VARIABLE
-};
-
 export function initMessageMeetWidget(element, config) {
-  config = _.merge({}, defaultConfig, config);
-
   ReactDOM.render(
     <Root accessToken={config.accessToken} toPersonEmail={config.toPersonEmail} toPersonId={config.toPersonId} />,
     element
   );
-
   return element;
 }
-
 
 function loadAllWidgets() {
   const widgets = document.querySelectorAll(`[data-toggle="spark-message-meet"]`);
