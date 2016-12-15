891156ca8b2fea5b39e068527937267c44bed809
Adam Weeks <adweeks@cisco.com>
2016-12-14 22:16:25 -0500

    feat(widget-message-meet): add oauth ability to input screen

--- packages/widget-message-meet/src/components/widget-data-input/index.js
+++ packages/widget-message-meet/src/components/widget-data-input/index.js
@@ -1,6 +1,7 @@
-/* eslint-disable react/no-set-state, global-require */
+/* eslint-disable react/no-set-state, global-require, camelcase */
 import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
+import Spark from '@ciscospark/spark-core';
 
 import styles from './styles.css';
 
@@ -9,6 +10,23 @@ import SparkLogo from '../spark-logo';
 class WidgetDataInput extends Component {
   constructor(props) {
     super(props);
+    const l = window.location;
+    const redirectUri = `${l.protocol}//${l.host}${l.pathname}`.replace(/\/$/, ``);
+    this.spark = new Spark({
+      config: {
+        credentials: {
+          oauth: {
+            client_id: `C6acec3fac30d32ed481f3592d4b96ea9a55214f91cc285486ab0d1fe26d180ad`,
+            client_secret: `eed978411377926971722de5c4d7b11b625df01137eec9a51c6a92d62acedfc3`,
+            scope: `spark:rooms_read spark:rooms_write spark:memberships_read spark:memberships_write spark:messages_read spark:messages_write`,
+            redirect_uri: redirectUri
+          }
+        }
+      }
+    });
+    this.spark.listenToAndRun(this.spark, `change:isAuthenticated`, () => {
+      this.checkForOauthToken();
+    });
     const {accessToken, toPersonEmail} = props;
     const error = false;
     this.state = {
@@ -17,8 +35,13 @@ class WidgetDataInput extends Component {
       toPersonEmail
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAuth = (e) => {
+      e.preventDefault();
+      this.spark.authenticate();
+    };
     this.handleAccessTokenChange = (e) => this.setState({accessToken: e.target.value});
     this.handleEmailChange = (e) => this.setState({toPersonEmail: e.target.value});
+    this.checkForOauthToken();
   }
 
   shouldComponentUpdate() {
@@ -38,6 +61,13 @@ class WidgetDataInput extends Component {
 
   validateForm(state) {
     return state.accessToken && state.toPersonEmail;
+  }
+
+  checkForOauthToken() {
+    if (this.spark.credentials.authorization && this.spark.credentials.authorization.access_token) {
+      const token = this.spark.credentials.authorization.access_token;
+      this.setState({accessToken: token});
+    }
   }
 
   render() {
@@ -63,9 +93,12 @@ class WidgetDataInput extends Component {
               type="text"
               value={this.state ? this.state.accessToken : ``}
             />
+            <button className={classNames(`props-submit`, styles.propsSubmit)} onClick={this.handleAuth}>{`OAuth`}</button>
           </div>
-          <button className={classNames(`props-submit`, styles.propsSubmit)} onClick={this.handleSubmit}>{`Chat`}</button>
-          {error}
+          <div className={classNames(`field-wrapper`, styles.fieldWrapper)}>
+            <button className={classNames(`props-submit`, styles.propsSubmit)} onClick={this.handleSubmit}>{`Go Meet!`}</button>
+            {error}
+          </div>
         </form>
       </div>
     );
