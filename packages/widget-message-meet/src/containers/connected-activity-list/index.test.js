5539afe345ba112b6b054732d2da0e3a98c1e0f5
Adam Weeks <adweeks@cisco.com>
2017-01-09 17:31:00 -0500

    test(widget-message-meet): implement mock store for selectors

--- packages/widget-message-meet/src/containers/connected-activity-list/index.test.js
+++ packages/widget-message-meet/src/containers/connected-activity-list/index.test.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
-
-import {initialState as conversation} from '../../reducers/conversation';
-import {initialState as flags} from '../../reducers/flags';
-import {initialState as user} from '../../reducers/user';
+import createComponentWithIntl from '../../utils/createComponentWithIntl';
 
 import ConnectedActivityList from '.';
 
-const middlewares = [];
-const mockStore = configureStore(middlewares);
-
-const store = mockStore({
-  conversation,
-  flags,
-  user
-});
+import store from '../../selectors/fixtures/mock-store';
 
 describe(`ConnectedActivityList`, () => {
   it(`renders properly`, () => {
-    const component = renderer.create(
+    const component = createComponentWithIntl(
       <Provider store={store}>
         <ConnectedActivityList />
       </Provider>
--- packages/widget-message-meet/src/containers/scrolling-activity/index.test.js
+++ packages/widget-message-meet/src/containers/scrolling-activity/index.test.js
@@ -2,48 +2,12 @@ import React from 'react';
 import createComponentWithIntl from '../../utils/createComponentWithIntl';
 import {Provider} from 'react-redux';
 
-import store from '../../store';
+import store from '../../selectors/fixtures/mock-store';
 
 import ScrollingActivity from '.';
 
-const flags = [];
-const activities = [{
-  id: `test-123-123-123-123`,
-  actor: {
-    id: 1,
-    displayName: `Test User 1`
-  },
-  object: {
-    displayName: `Test Activity Content 1`
-  },
-  published: `2016-09-20T19:52:57.186Z`,
-  verb: `post`
-}, {
-  id: `test-456-123-456-123`,
-  actor: {
-    id: 2,
-    displayName: `Test User 2`
-  },
-  object: {
-    displayName: `Test Activity Content 2`
-  },
-  published: `2016-09-20T19:53:57.186Z`,
-  verb: `post`
-}, {
-  id: `test-789-123-789-123`,
-  actor: {
-    id: 3,
-    displayName: `Test User 3`
-  },
-  object: {
-    displayName: `Test Activity Content 3`
-  },
-  published: `2016-09-20T19:54:57.186Z`,
-  verb: `post`
-}];
-const avatars = {
-  'test-123-123-123-123': `http://googledoodle.jpg`
-};
+store.indicators = {typing: []};
+
 const onActivityDelete = jest.fn();
 const onActivityFlag = jest.fn();
 
@@ -52,9 +16,18 @@ describe(`ScrollingActivity container`, () => {
     const component = createComponentWithIntl(
       <Provider store={store}>
         <ScrollingActivity
-          activities={activities}
-          avatars={avatars}
-          flags={flags}
+          onActivityDelete={onActivityDelete}
+          onActivityFlag={onActivityFlag}
+        />
+      </Provider>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it(`renders properly loading history`, () => {
+    const component = createComponentWithIntl(
+      <Provider store={store}>
+        <ScrollingActivity
+          isLoadingHistoryUp
           onActivityDelete={onActivityDelete}
           onActivityFlag={onActivityFlag}
         />
