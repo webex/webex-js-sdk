383df68a0e162247bf6ad00ae4b0dc0cccaa5d5e
saurjai3 <saurjai3@cisco.com>
2017-01-11 11:58:50 -0800

    refactor(plugin-credentials): Fixed login issues

--- packages/plugin-credentials/src/credentials/browser.js
+++ packages/plugin-credentials/src/credentials/browser.js
@@ -101,7 +101,6 @@ const Credentials = SparkPlugin.extend(Object.assign({}, common, {
     }
 
     this.logger.info(`credentials(shim): initiating authorization code grant flow`);
-
     window.location = this.buildOAuthUrl(Object.assign({response_type: `code`}, options));
     return new Promise(noop);
   },
@@ -112,17 +111,18 @@ const Credentials = SparkPlugin.extend(Object.assign({}, common, {
     // depends on this.config needs to run after SparkCore#initialize executes,
     // so, we'll use process.nextTick to run the following block on the next
     // execution cycle.
+    const location = url.parse(window.location.href, true);
+    let query = clone(location.query);
+
+    if (query.code) {
+      this.isAuthenticating = true;
+      Reflect.deleteProperty(location.query, `code`);
+      Reflect.deleteProperty(location.query, `state`);
+      this._updateLocation(location);
+    }
+
     process.nextTick(() => {
-      const location = url.parse(window.location.href, true);
-
-      let query = clone(location.query);
-
       if (query.code) {
-        Reflect.deleteProperty(location.query, `code`);
-        Reflect.deleteProperty(location.query, `state`);
-
-        this._updateLocation(location);
-
         // Though initialize is a synchronous call, it should be safe to
         // call authenticate() because it'll get called again later but end
         // up cached via oneFlight.
