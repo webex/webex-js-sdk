63ec1efdeb690f826d74a2b6eb2e05d3188414c6
Carter Foxgrover <cfoxgrov@cisco.com>
2017-01-17 13:27:27 -0800

    feat(plugin-metrics): add support for pre-login and aliasing endpoints for metrics

--- packages/plugin-metrics/src/metrics.js
+++ packages/plugin-metrics/src/metrics.js
@@ -26,7 +26,7 @@ const Metrics = SparkPlugin.extend({
     return this.batcher.request(Object.assign({key}, value));
   },
 
-  submitClientMetrics(eventName, props) {
+  submitClientMetrics(eventName, props, preLoginId) {
     const payload = {metricName: eventName};
     if (props.tags) {
       payload.tags = props.tags;
@@ -34,8 +34,57 @@ const Metrics = SparkPlugin.extend({
     if (props.fields) {
       payload.fields = props.fields;
     }
-    return this.clientMetricsBatcher.request(payload);
+    if (props.type) {
+      payload.type = props.type;
+    }
+    if (preLoginId) {
+      const _payload = {
+        metrics: [
+          payload
+        ]
+      };
+      // Do not batch these because pre-login events occur during onboarding, so we will be partially blind
+      // to users' progress through the reg flow if we wait to persist pre-login metrics for people who drop off because
+      // their metrics will not post from a queue flush in time
+      this.postPreLoginMetric(_payload, preLoginId);
+    } else {
+      return this.clientMetricsBatcher.request(payload);
+    }
+  },
+
+
+  /**
+   * Issue request to alias a user's pre-login ID with their CI UUID
+   * @param {string} preLoginId
+   */
+  alias: function(preLoginId) {
+    var req = this.request({
+      method: `POST`,
+      api: `metrics`,
+      resource: `clientmetrics`,
+      headers: {
+        "X-Prelogin-UserId": preLoginId
+      },
+      body: {}
+    });
+    req.setParameter("alias", true);
+    return req;
+  },
+
+  postPreLoginMetric: function(payload, preLoginId) {
+    return this.request({
+      method: `POST`,
+      url: 'https://metrics-a.wbx2.com/metrics/api/v1/clientmetrics-prelogin',
+      headers: {
+        "X-Prelogin-UserId": preLoginId,
+        "content-type": "application/json",
+        "charset": "utf-8"
+      },
+      body: payload
+    });
   }
+
+
 });
 
 export default Metrics;
