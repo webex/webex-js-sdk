8a8468d64ba7a39441e1affe67deac90f6d8bfaa
Adam Weeks <adweeks@cisco.com>
2017-02-02 11:22:52 -0500

    test(example-phone): add describe blocks for test suite names

--- packages/example-phone/test/automation/spec/auth.js
+++ packages/example-phone/test/automation/spec/auth.js
@@ -7,67 +7,69 @@ import createBrowser from '../lib/create-browser';
 import testUsers from '@ciscospark/test-helper-test-users';
 import pkg from '../../../package';
 
-describe(`example-phone auth`, () => {
-  let browser, user;
+describe(`example-phone`, () => {
+  describe(`auth`, () => {
+    let browser, user;
 
-  before(`create user`, () => testUsers.create({count: 1})
-    .then((users) => {user = users[0];}));
+    before(`create user`, () => testUsers.create({count: 1})
+      .then((users) => {user = users[0];}));
 
-  beforeEach(`create browser`, () => createBrowser(pkg, {name: `auth`})
-    .then((b) => {browser = b;}));
+    beforeEach(`create browser`, () => createBrowser(pkg, {name: `auth`})
+      .then((b) => {browser = b;}));
 
-  afterEach(`quit browser`, () => browser && browser.quit()
-    .catch((reason) => {console.warn(reason);}));
+    afterEach(`quit browser`, () => browser && browser.quit()
+      .catch((reason) => {console.warn(reason);}));
 
-  describe(`An unauthenticated user`, () => {
-    it.skip(`can authenticate by entering an email address`, () => browser
-      .getMainPage()
-      .waitForElementByCssSelector(`[href="/auth"]`)
-        .click()
-      .waitForElementByCssSelector(`[title="Enter email"]`)
-        .sendKeys(user.email)
-      .waitForElementByCssSelector(`[title="Login by email address"]`)
-        .click()
-      .login(user)
-      .waitForElementByClassName(`ready`)
-      .title()
-        .should.eventually.become(`Example: Phone`)
-      .waitForElementById(`authstate`)
-        .text()
-          .should.eventually.become(`Authenticated`));
+    describe(`An unauthenticated user`, () => {
+      it.skip(`can authenticate by entering an email address`, () => browser
+        .getMainPage()
+        .waitForElementByCssSelector(`[href="/auth"]`)
+          .click()
+        .waitForElementByCssSelector(`[title="Enter email"]`)
+          .sendKeys(user.email)
+        .waitForElementByCssSelector(`[title="Login by email address"]`)
+          .click()
+        .login(user)
+        .waitForElementByClassName(`ready`)
+        .title()
+          .should.eventually.become(`Example: Phone`)
+        .waitForElementById(`authstate`)
+          .text()
+            .should.eventually.become(`Authenticated`));
 
-    it.skip(`can authenticate by starting the CI login flow`, () => browser
-      .getMainPage()
-      .loginWithUI(user)
-      .title()
-        .should.eventually.become(`Example: Phone`)
-      .assertAuthenticationStatus(`authenticated`));
+      it.skip(`can authenticate by starting the CI login flow`, () => browser
+        .getMainPage()
+        .loginWithUI(user)
+        .title()
+          .should.eventually.become(`Example: Phone`)
+        .assertAuthenticationStatus(`authenticated`));
 
-    it.skip(`can authorize by entering an access token`, () => browser
-      .getMainPage()
-      .waitForElementByCssSelector(`[href="/auth"]`)
-        .click()
-      .waitForElementByCssSelector(`[title="Enter access token"]`)
-        .sendKeys(user.token.access_token)
-      .waitForElementByCssSelector(`[title="Set access token"]`)
-        .click()
-      .waitForElementById(`authstate`)
-        .text()
-          .should.eventually.become(`Authenticated`));
-  });
+      it.skip(`can authorize by entering an access token`, () => browser
+        .getMainPage()
+        .waitForElementByCssSelector(`[href="/auth"]`)
+          .click()
+        .waitForElementByCssSelector(`[title="Enter access token"]`)
+          .sendKeys(user.token.access_token)
+        .waitForElementByCssSelector(`[title="Set access token"]`)
+          .click()
+        .waitForElementById(`authstate`)
+          .text()
+            .should.eventually.become(`Authenticated`));
+    });
 
-  describe(`An authenticated user`, () => {
-    beforeEach(`login`, () => browser
-      .getMainPage()
-      .waitForElementByCssSelector(`[href="/auth"]`)
-        .click()
-      .waitForElementByCssSelector(`[title="Enter access token"]`)
-        .click(user.token.access_token)
-      .waitForElementByCssSelector(`[title="Set access token"]`)
-        .click()
-      .hasElementByClassName(`.authenticating`)
-        .should.eventually.become(false));
+    describe(`An authenticated user`, () => {
+      beforeEach(`login`, () => browser
+        .getMainPage()
+        .waitForElementByCssSelector(`[href="/auth"]`)
+          .click()
+        .waitForElementByCssSelector(`[title="Enter access token"]`)
+          .click(user.token.access_token)
+        .waitForElementByCssSelector(`[title="Set access token"]`)
+          .click()
+        .hasElementByClassName(`.authenticating`)
+          .should.eventually.become(false));
 
-    it(`can logout`);
+      it(`can logout`);
+    });
   });
 });
--- packages/example-phone/test/automation/spec/incoming-call.js
+++ packages/example-phone/test/automation/spec/incoming-call.js
@@ -2,56 +2,58 @@ import createBrowser from '../lib/create-browser';
 import testUsers from '@ciscospark/test-helper-test-users';
 import pkg from '../../../package';
 
-describe.skip(`example-phone incoming call`, () => {
-  let browser, callee, caller;
+describe.skip(`example-phone`, () => {
+  describe(`incoming-call`, () => {
+    let browser, callee, caller;
 
-  beforeEach(`create users`, () => testUsers.create({count: 2})
-    .then(([u1, u2]) => {
-      caller = u1;
-      callee = u2;
-    }));
+    beforeEach(`create users`, () => testUsers.create({count: 2})
+      .then(([u1, u2]) => {
+        caller = u1;
+        callee = u2;
+      }));
 
-  beforeEach(`create browser`, () => createBrowser(pkg, {
-    name: `caller`
-  })
-    .then((b) => {browser = b;})
-    .then(() => browser
-      .loginWithLocalStorage(callee)
-      .bdInit(caller)
-      .clickOnTitle(`Link to Call Page`)));
+    beforeEach(`create browser`, () => createBrowser(pkg, {
+      name: `caller`
+    })
+      .then((b) => {browser = b;})
+      .then(() => browser
+        .loginWithLocalStorage(callee)
+        .bdInit(caller)
+        .clickOnTitle(`Link to Call Page`)));
 
-  afterEach(`quit browser`, () => browser && browser.quit()
-    .catch((reason) => {console.warn(reason);}));
+    afterEach(`quit browser`, () => browser && browser.quit()
+      .catch((reason) => {console.warn(reason);}));
 
-  describe(`As an authenticated user`, () => {
-    describe(`when there is an incoming call`, () => {
-      it(`answers the call`, () => browser
-        .bdPlaceCall(callee.email)
-        .waitForElementByClassName(`alert`)
-        .answerCall()
-        .assertIsInCallWith(caller));
+    describe(`As an authenticated user`, () => {
+      describe(`when there is an incoming call`, () => {
+        it(`answers the call`, () => browser
+          .bdPlaceCall(callee.email)
+          .waitForElementByClassName(`alert`)
+          .answerCall()
+          .assertIsInCallWith(caller));
 
-      it(`answers the call with audio only`, () => browser
-        .bdPlaceCall(callee.email)
-        .clickOnTitle(`Answer Call with Audio`)
-        .assertIsInCallWith(caller)
-        .assertLocalAudioDirection(`sendrecv`)
-        .assertLocalVideoDirection(`inactive`)
-      );
+        it(`answers the call with audio only`, () => browser
+          .bdPlaceCall(callee.email)
+          .clickOnTitle(`Answer Call with Audio`)
+          .assertIsInCallWith(caller)
+          .assertLocalAudioDirection(`sendrecv`)
+          .assertLocalVideoDirection(`inactive`)
+        );
 
-      // FIXME seems broken
-      it.skip(`answers the call with video only`, () => browser
-        .bdPlaceCall(callee.email)
-        .clickOnTitle(`Answer Call with Video`)
-        .assertIsInCallWith(caller)
-        .assertLocalAudioDirection(`inactive`)
-        .assertLocalVideoDirection(`sendrecv`)
-      );
+        // FIXME seems broken
+        it.skip(`answers the call with video only`, () => browser
+          .bdPlaceCall(callee.email)
+          .clickOnTitle(`Answer Call with Video`)
+          .assertIsInCallWith(caller)
+          .assertLocalAudioDirection(`inactive`)
+          .assertLocalVideoDirection(`sendrecv`)
+        );
 
-      it(`declines the call`);
+        it(`declines the call`);
 
-      describe(`when the page is reloaded`, () => {
-        it(`answers the incoming call`);
+        describe(`when the page is reloaded`, () => {
+          it(`answers the incoming call`);
+        });
       });
     });
   });
