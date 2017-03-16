ccfb5ad1c6dd84768d34a227228e766fcf27a371
Ian W. Remmel <iremmel@cisco.com>
2017-03-16 12:16:51 -0700

    test(plugin-phone): add max-wait on afterall hangup

--- packages/plugin-phone/test/integration/spec/_setup.js
+++ packages/plugin-phone/test/integration/spec/_setup.js
@@ -5,6 +5,7 @@
 
 import {Call} from '../..';
 import sinon from '@ciscospark/test-helper-sinon';
+import {maxWaitForPromise} from '@ciscospark/test-helper-mocha';
 
 beforeEach(() => {
   sinon.spy(Call, `make`);
@@ -12,7 +13,7 @@ beforeEach(() => {
 
 afterEach(`end all calls`, function() {
   this.timeout(30000);
-  const promises = Call.make.returnValues.map((c) => c.hangup()
+  const promises = Call.make.returnValues.map((c) => maxWaitForPromise(2000, c.hangup())
     .catch((reason) => console.warn(reason)));
 
   Call.make.restore();
