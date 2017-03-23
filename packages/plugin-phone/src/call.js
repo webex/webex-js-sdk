b54dfe5052226ad5c353208b039b06adf4da51f5
Bernie Zang <nzang@cisco.com>
2017-03-23 17:15:38 -0400

    fix(plugin-phone): Fix options setting for _join

--- packages/plugin-phone/src/call.js
+++ packages/plugin-phone/src/call.js
@@ -832,11 +832,14 @@ const Call = SparkPlugin.extend({
         offerToReceiveAudio: recvOnly || !!options.constraints.audio,
         offerToReceiveVideo: recvOnly || !!options.constraints.video
       });
+
+      this.media.set({
+        audio: options.constraints.audio,
+        video: options.constraints.video
+      });
     }
 
     this.media.set({
-      audio: options.constraints.audio,
-      video: options.constraints.video,
       offerToReceiveAudio: options.offerOptions.offerToReceiveAudio,
       offerToReceiveVideo: options.offerOptions.offerToReceiveVideo
     });
