c57e047e48a4c7eaa9fafe2b67a1cb4fa7c6d066
Ian W. Remmel <design@ianwremmel.com>
2017-03-24 11:40:54 -0700

    fix(ciscospark): ensure hydra calls are still authorized after registering with WDM

--- packages/ciscospark/test/integration/spec/request.js
+++ packages/ciscospark/test/integration/spec/request.js
@@ -4,18 +4,67 @@
  */
 
 import {assert} from '@ciscospark/test-helper-chai';
+import sinon from '@ciscospark/test-helper-sinon';
 import spark from '../..';
 
 describe(`ciscospark`, function() {
   this.timeout(60000);
   describe(`#request`, () => {
-    it(`can make requests`, () => {
-      return spark.request({
-        uri: `${spark.config.hydraServiceUrl}/ping`
+    it(`can make requests`, () => spark.request({
+      service: `hydra`,
+      resource: `/ping`
+    })
+      .then((res) => {
+        assert.statusCode(res, 200);
+        assert.property(res.options, `headers`);
+        assert.property(res.options.headers, `authorization`);
+        assert.isDefined(res.options.headers.authorization);
+        assert.isNotNull(res.options.headers.authorization);
+      }));
+
+    describe(`after registering against wdm`, () => {
+      before(() => spark.device.register());
+      it(`can make authorized requests`, () => spark.request({
+        service: `hydra`,
+        resource: `/ping`
       })
         .then((res) => {
           assert.statusCode(res, 200);
+          assert.property(res.options, `headers`);
+          assert.property(res.options.headers, `authorization`);
+          assert.isDefined(res.options.headers.authorization);
+          assert.isNotNull(res.options.headers.authorization);
+        }));
+
+      it(`handles pagination`, () => {
+        const spy = sinon.spy();
+        return Promise.all([
+          spark.rooms.create({title: `1`}),
+          spark.rooms.create({title: `2`})
+        ])
+        .then(() => spark.rooms.list({max: 1}))
+        .then((rooms) => {
+          assert.lengthOf(rooms, 1);
+          return (function f(page) {
+            for (const room of page) {
+              spy(room.id);
+            }
+
+            // by the time this test runs, we're going to have way more than two
+            // rooms. This test is really just here to make sure we've addressed
+            // the fact that, once registered with wdm, we'll be able to fetch
+            // page 1 from hydra-a.wbx2.com and page 2 from api.ciscospark.com.
+            if (page.hasNext() && spy.callCount < 2) {
+              return page.next().then(f);
+            }
+
+            return Promise.resolve();
+          }(rooms));
+        })
+        .then(() => {
+          assert.isAbove(spy.callCount, 1);
         });
+      });
     });
   });
 });
