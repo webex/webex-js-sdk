fba2079ce818baadb59120e866b18b294a91e3c3
Ian W. Remmel <iremmel@cisco.com>
2017-04-28 23:32:35 -0700

    feat(tooling): force NODE_ENV to test

--- tooling/test.js
+++ tooling/test.js
@@ -6,6 +6,7 @@
 const dotenv = require(`dotenv`);
 dotenv.config({path: `.env.default`});
 dotenv.config();
+process.env.NODE_ENV = `test`;
 
 const g = require(`glob`);
 const debug = require(`debug`)(`monorepo:test`);
