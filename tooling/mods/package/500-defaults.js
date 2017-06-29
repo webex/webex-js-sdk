8dc6522c34ff4d9dcf6a54263199f3a3e11a7fe8
Ian W. Remmel <iremmel@cisco.com>
2017-06-29 16:51:33 -0700

    feat(tooling): add packagemod to add standard values

---
+++ tooling/mods/package/500-defaults.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function addDefaults(pkg) {
+  Object.assign(pkg, {
+    engines: {
+      node: `>=6`
+    },
+    license: `MIT`,
+    repository: `https://github.com/ciscospark/spark-js-sdk`
+  });
+
+  return Promise.resolve(pkg);
+};
