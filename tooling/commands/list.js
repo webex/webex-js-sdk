5cb838ce31a698a7a9ea354a5b7e488b474be02a
Ian W. Remmel <iremmel@cisco.com>
2017-07-26 10:01:20 -0700

    fix(tooling): do not run legacy tests during validated merge
    Also, correctly run legacy tests in pipeline

--- tooling/commands/list.js
+++ tooling/commands/list.js
@@ -17,20 +17,25 @@ module.exports = {
       type: `boolean`
     }
   },
+  // eslint-disable-next-line complexity
   handler: wrapHandler(async ({fortests, forpipeline}) => {
     let packages;
     if (fortests) {
       const changed = await updated({});
       if (changed.includes(`tooling`)) {
         packages = await list();
-        packages.push(`legacy`);
       }
       else {
         packages = await updated({dependents: true});
       }
+      // We don't want to run legacy tests in validated merge
+      packages = packages.filter((p) => p !== `legacy`);
     }
     else {
       packages = await list();
+      if (forpipeline) {
+        packages.push(`legacy`);
+      }
     }
 
     if (forpipeline || fortests) {
