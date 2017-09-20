886305cbf7377d31981e3bc567600937f3c3fa7c
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-09-20 14:36:25 -0700

    chore(tooling): remove all of grunt that we can

--- Gruntfile.concurrent.js
+++ Gruntfile.concurrent.js
@@ -12,60 +12,7 @@ module.exports = function gruntConfig(grunt) {
   require(`time-grunt`)(grunt);
   grunt.loadTasks(`tasks`);
 
-  const PACKAGES = grunt.file.expand({
-    cwd: `packages/node_modules`,
-    filter: `isDirectory`
-  }, [
-    `*`,
-    `!*/*`,
-    `@ciscospark/*`,
-    `!@ciscospark`,
-    `!@ciscospark/example*`,
-    `!@ciscospark/test-helper*`,
-    `!@ciscospark/bin*`,
-    `!@ciscospark/xunit-with-logs`,
-    `@ciscospark/test-helper-mock-web-socket`,
-    `@ciscospark/test-helper-mock-socket`
-  ]);
-
   const config = {
-    concurrent: {
-      test: {
-        tasks: [],
-        options: {
-          // Run sequentially for now - port management is hard
-          limit: 1,
-          logConcurrentOutput: true
-        }
-      }
-    },
-
-    clean: {
-      reports: {
-        src: [
-          `./reports`
-        ]
-      }
-    },
-
-    documentation: {
-      options: {
-        private: false
-      },
-      html: {
-        src: [
-          `./packages/node_modules/ciscospark/src/index.js`,
-          `./packages/node_modules/@ciscospark/plugin-phone/src/index.js`
-        ],
-        options: {
-          destination: `./docs/api/`,
-          format: `html`,
-          github: true,
-          theme: `./docs/_theme`
-        }
-      }
-    },
-
     env: {
       default: {
         src: `.env.default`
@@ -73,13 +20,6 @@ module.exports = function gruntConfig(grunt) {
       secrets: {
         src: `.env`
       }
-    },
-
-    fileExists: {
-      checkstylexml: [
-        `./reports/style/eslint-concurrent.xml`,
-        `./reports/style/eslint-legacy.xml`
-      ]
     },
 
     'gh-pages': {
@@ -100,89 +40,8 @@ module.exports = function gruntConfig(grunt) {
           }
         }
       }
-    },
-
-    makeReport2: {
-      all: {
-        files: [{
-          cwd: `./reports/coverage-final/`,
-          expand: true,
-          src: `**/*.json`
-        }],
-        options: {
-          reporters: {
-            cobertura: {
-              file: `./reports/cobertura-coverage.xml`
-            },
-            json: {
-              file: `./reports/coverage.json`
-            },
-            html: {},
-            'text-summary': {}
-          }
-        }
-      }
-    },
-
-    'package-json': {
-      package: {
-        options: {
-          overrides(packageName) {
-            return {
-              license: `MIT`,
-              repository: `https://github.com/ciscospark/spark-js-sdk/tree/master/packages/node_modules${packageName}`,
-              engines: {
-                node: `>=4`
-              }
-            };
-          }
-        }
-      }
-    },
-
-    shell: {}
+    }
   };
-
-  PACKAGES.forEach((packageName) => {
-    const shellTestKey = `test_${packageName}`;
-    const testKey = `test:${packageName}`;
-
-    config.shell[shellTestKey] = {
-      command: `PACKAGE=${packageName} grunt --gruntfile Gruntfile.package.js --no-color test`
-    };
-    grunt.registerTask(testKey, `shell:${shellTestKey}`);
-    config.concurrent.test.tasks.push(testKey);
-  });
-
-  grunt.registerTask(`test`, [
-    `clean:reports`,
-    `concurrent:test`,
-    p(process.env.COVERAGE) && `makeReport2`
-  ].filter((key) => typeof key === `string`));
-
-  /**
-   * Helper function which converts environment strings into
-   * undefined/true/false
-   * @param {string} env
-   * @returns {boolean|undefined}
-   * @private
-   */
-  function p(env) {
-    if (typeof env === `undefined` || env === `undefined` || env === ``) {
-      return undefined;
-    }
-    if (env.toLowerCase() === `true`) {
-      return true;
-    }
-    if (env.toLowerCase() === `false`) {
-      return false;
-    }
-    throw new Error(`p(): \`env\`"${env}" is not a recognized string`);
-  }
-
-  grunt.registerTask(`build:docs`, [
-    `documentation`
-  ]);
 
   grunt.registerTask(`publish:docs`, [
     `gh-pages:ghc`
