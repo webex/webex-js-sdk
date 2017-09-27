990c1e2b2d7f19f33a1ed1b45fb802ac786b2bbe
mmaines-cisco <mmaines@cisco.com>
2017-09-27 14:21:52 -0400

    refactor(lint-package-names): Use existing libs to get package names

--- tooling/lint-package-names.js
+++ tooling/lint-package-names.js
@@ -1,53 +1,12 @@
-// const shelljs = require(`shelljs`);
-const fs = require(`fs`);
+const fs = require('fs');
+const package_lib = require('./lib/package');
 
-const modules = [];
-
-
-// JS equivelent of "set -e" bash command
-// for (var envVariable in process.env ) {
-//     shelljs.exec(`echo ${envVariable}`);
-// }
-
-// Grab the module names of each package in the packages/node_modules 
-// and package/node_modules/@ciscospark directories containing the ciscospark substring
-// Using a Promise to excecute async functions in a sync manner to pass linting tests
-new Promise((resolve, reject) => {
-
-  fs.readdir(`./packages/node_modules/`, (error, files) => {
-    if (error) {
-      reject();
-    }
-
-    files.forEach((module) => {
-      const regex = new RegExp(`ciscospark`);
-      if (regex.test(module) && module !== `@ciscospark`) {
-        modules.push(module);
-      }
+package_lib.list(function(packages) {
+    packages.forEach((package_name) => {
+        JSON.parse(fs.readFile(`./packages/node_modules/${package_name}/package.json`, `utf8`), (error, packageJson) => {
+            if (module !== packageJson.name) {
+                throw new Error(`package.json for '${package_name}' contains unexpected package name '${packageJson.name}'`);
+            }
+        });
     });
-
-  });
-
-  resolve();
-}).then(fs.readdir(`./packages/node_modules/@ciscospark/`, (error, files) => {
-  if (error) {
-    throw new Error(error);
-  }
-
-  files.forEach((module) => {
-    modules.push(module);
-  });
-
-})).then(modules.forEach((module) => {
-
-  JSON.parse(fs.readFile(`./packages/node_modules/${module}/package.json`, `utf8`), (error, packageJson) => {
-    if (module !== packageJson.name) {
-      throw new Error(`package.json for '${module}' contains unexpected package name '${packageJson.name}'`);
-    }
-  });
-})).catch((error) => {
-
-  throw new Error(error);
-});
-
-
+})
