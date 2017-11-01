1c55ee71db3f025e2f58ffb6fc60c7345505d39e
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-11-01 10:06:16 -0700

    chore(tooling): exclude samples folder from package.json lint

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$')
+PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$')
 for PACKAGE in $PACKAGES; do
   NAME_IN_PACKAGE=$(cat "./packages/node_modules/${PACKAGE}/package.json" | jq -r .name);
   if [ "${NAME_IN_PACKAGE}" != "${PACKAGE}" ]; then
