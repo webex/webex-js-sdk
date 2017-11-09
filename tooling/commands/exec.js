474389c5a373240f20e96e05cab7f171a685ea37
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-11-09 15:23:12 -0500

    chore(tooling): add log statements

--- tooling/commands/exec.js
+++ tooling/commands/exec.js
@@ -2,6 +2,7 @@
  * Copyright (c) 2015-2017 Cisco Systems, Inc. See LICENSE file.
  */
 
+const debug = require(`debug`)(`tooling:command:exec`);
 const wrapHandler = require(`../lib/wrap-handler`);
 const {list, spawn} = require(`../util/package`);
 
@@ -11,6 +12,7 @@ module.exports = {
   builder: {},
   handler: wrapHandler(async ({cmd, args}) => {
     for (const packageName of await list()) {
+      debug(`running ${cmd} ${args.join(` `)} in ${packageName}`);
       await spawn(packageName, cmd, args);
     }
   })
