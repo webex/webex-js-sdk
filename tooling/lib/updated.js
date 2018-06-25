754ee0a6aa316da29eb3bfd7824096e83c5c6cff
Bernie Zang <nzang@cisco.com>
2018-06-25 16:24:43 -0400

    fix(tooling): correctly ignore doc changes

--- tooling/lib/updated.js
+++ tooling/lib/updated.js
@@ -55,7 +55,7 @@ function fileToPackage(d) {
     return d[0];
   }
 
-  if (d.startsWith('docs') || d.startsWith('documentation' || d.startsWith('.github') || d.endsWith('.md'))) {
+  if (d.startsWith('docs') || d.startsWith('documentation') || d.startsWith('.github') || d.endsWith('.md')) {
     return 'docs';
   }
 
