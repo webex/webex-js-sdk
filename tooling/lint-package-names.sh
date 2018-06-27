ff366af5e878a49c4a0fb6d0f85ed4c07dec640f
Matt Norris <matt@mattnorris.me>
2018-06-27 14:05:07 -0400

    chore(lint-packages): exclude deprecated packages

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 set -e
+shopt -s extglob
+
+DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
+  @ciscospark/sparkd"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
-  NAME_IN_PACKAGE=$(cat "./packages/node_modules/${PACKAGE}/package.json" | jq -r .name);
-  if [ "${NAME_IN_PACKAGE}" != "${PACKAGE}" ]; then
-   echo "package.json for '${PACKAGE}' contains unexpected package name '${NAME_IN_PACKAGE}'"
-   exit 1
-  fi
+  # Check only packages that are not deprecated.
+  case $DEPRECATED_PACKAGES in
+    !(*"$PACKAGE"*))
+    NAME_IN_PACKAGE=$(cat "./packages/node_modules/${PACKAGE}/package.json" | jq -r .name);
+    if [ "${NAME_IN_PACKAGE}" != "${PACKAGE}" ]; then
+      echo "package.json for '${PACKAGE}' contains unexpected package name '${NAME_IN_PACKAGE}'"
+      exit 1
+    fi
+    ;;
+  esac
 done
