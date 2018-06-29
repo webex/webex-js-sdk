074b7caec0f222a6645221bf82f21640f8b951d5
Matt Norris <matt@mattnorris.me>
2018-06-29 10:19:35 -0400

    chore(test-helper-automation): deprecate old package

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -4,6 +4,7 @@ set -e
 shopt -s extglob
 
 DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
+  @ciscospark/test-helper-automation \
   @ciscospark/sparkd"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
