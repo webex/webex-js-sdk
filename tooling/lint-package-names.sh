85dd95d03ee841ca7f254b881d821f8a8e330725
Matt Norris <matt@mattnorris.me>
2018-07-19 16:24:07 -0400

    chore(recipe-private-web-client): reinstate package to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -5,7 +5,8 @@ shopt -s extglob
 
 DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/test-helper-automation \
-  @ciscospark/sparkd"
+  @ciscospark/sparkd \
+  @ciscospark/recipe-private-web-client"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
