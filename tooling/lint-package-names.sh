155e195540e9e2e98dfc5db11ae7329a1fd79758
Matt Norris <matt@mattnorris.me>
2018-09-10 15:22:36 -0400

    chore(storage-adapter-local-forage): deprecate package in linter

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -8,7 +8,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/sparkd \
   @ciscospark/recipe-private-web-client \
   @ciscospark/test-helper-mock-socket \
-  @ciscospark/test-helper-server"
+  @ciscospark/test-helper-server \
+  @ciscospark/storage-adapter-local-forage"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
