180fa29af3af311a0fb3cd0c3f3684bef301be87
Matthew G. Norris <mattnorris@users.noreply.github.com>
2018-10-23 12:41:35 -0400

    Create "how to" for plugin-meetings (#997)
    docs(plugin-meetings): create docs on how to get started with plugin-meetings

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -9,9 +9,11 @@ const devConfig = require('./webpack.config.dev.js');
 dotenv.config();
 
 module.exports = (env) => {
-  if (env && env.package) {
+  if (process.env.PACKAGE || env && env.package) {
     if (process.env.NODE_ENV !== 'production') {
-      const context = path.resolve(__dirname, '..', 'packages', 'node_modules', env.package);
+      // TODO: Passing --env.package is not working for some reason.
+      const packageName = process.env.PACKAGE || env.package;
+      const context = path.resolve(__dirname, '..', 'packages', 'node_modules', packageName);
       return merge(baseConfig, devConfig, {
         mode: process.env.NODE_ENV || 'development',
         context,
