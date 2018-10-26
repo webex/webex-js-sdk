c10861f48968df45e3ad0168c0a0f4b04713ec98
Matt Norris <matt@mattnorris.me>
2018-10-26 12:00:59 -0400

    chore: run prod and int tests and output to CSV

--- test.py
+++ test.py
@@ -18,43 +18,7 @@ packages = ciscospark_packages + webex_packages
 
 print('Testing %d packages...' % len(packages))
 
-# Test the packages.
-
-# PROD_ENV_VARS = '''IDBROKER_BASE_URL="https://idbroker.webex.com" \\
-# IDENTITY_BASE_URL="https://identity.webex.com" \\
-# WDM_SERVICE_URL="https://wdm-a.wbx2.com/wdm/api/v1" \\
-# CONVERSATION_SERVICE_URL="https://conv-a.wbx2.com/conversation/api/v1" \\
-# ENCRYPTION_SERVICE_URL="https://encryption-a.wbx2.com" \\
-# ATLAS_SERVICE_URL="https://atlas-a.wbx2.com/admin/api/v1" \\
-# '''
-
-# INT_ENV_VARS = '''ACL_SERVICE_URL="https://acl-intb.ciscospark.com/acl/api/v1" \\
-# IDBROKER_BASE_URL="https://idbrokerbts.webex.com" \\
-# IDENTITY_BASE_URL="https://identitybts.webex.com" \\
-# WDM_SERVICE_URL="https://wdm-intb.ciscospark.com/wdm/api/v1" \\
-# CONVERSATION_SERVICE_URL="https://conversation-intb.ciscospark.com/conversation/api/v1" \\
-# ENCRYPTION_SERVICE_URL="https://encryption-intb.ciscospark.com/encryption/api/v1" \\
-# ATLAS_SERVICE_URL="https://atlas-intb.ciscospark.com/admin/api/v1" \\
-# '''
-
-# PROD_ENV_VARS = [
-#   'ATLAS_SERVICE_URL="https://atlas-a.wbx2.com/admin/api/v1"',
-#   'CONVERSATION_SERVICE_URL="https://conv-a.wbx2.com/conversation/api/v1"',
-#   'ENCRYPTION_SERVICE_URL="https://encryption-a.wbx2.com"',
-#   'IDBROKER_BASE_URL="https://idbroker.webex.com"',
-#   'IDENTITY_BASE_URL="https://identity.webex.com"',
-#   'WDM_SERVICE_URL="https://wdm-a.wbx2.com/wdm/api/v1"',
-# ]
-
-# INT_ENV_VARS = [
-#   'ACL_SERVICE_URL="https://acl-intb.ciscospark.com/acl/api/v1"',
-#   'ATLAS_SERVICE_URL="https://atlas-intb.ciscospark.com/admin/api/v1"',
-#   'CONVERSATION_SERVICE_URL="https://conversation-intb.ciscospark.com/conversation/api/v1"',
-#   'ENCRYPTION_SERVICE_URL="https://encryption-intb.ciscospark.com/encryption/api/v1"',
-#   'IDBROKER_BASE_URL="https://idbrokerbts.webex.com"',
-#   'IDENTITY_BASE_URL="https://identitybts.webex.com"',
-#   'WDM_SERVICE_URL="https://wdm-intb.ciscospark.com/wdm/api/v1"'
-# ]
+# Test the packages & write the results to a CSV file.
 
 PROD_ENV_VARS = {
   'ATLAS_SERVICE_URL': "https://atlas-a.wbx2.com/admin/api/v1",
@@ -77,35 +41,8 @@ INT_ENV_VARS = {
 
 TEST_COMMAND = 'npm test -- --package %s'
 
-import subprocess
-
-for package in packages[0:1]:
-  print('Testing `%s` on production...' % package)
-
-  env = os.environ.copy()
-  env.update(PROD_ENV_VARS)
-
-  bash_command = TEST_COMMAND % package
-  process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, env=env)
-
-  output, error = process.communicate()
-
-  print('output: ' + str(output))
-  print('error: ' + str(error))
-  print(process.returncode)
-
-  exit(1)
-
-  print('Testing `%s` on integration...' % package)
-  print(TEST_COMMAND % (INT_ENV_VARS, package))
-
-# bashCommand = "cwm --rdf test.rdf --ntriples > test.nt"
-# process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-# output, error = process.communicate()
-
-# Write the results to a CSV file.
-
 import csv
+import subprocess
 
 try:
   os.mkdir('output')
@@ -114,11 +51,30 @@ except OSError:
 
 OUTPUT_FILE_PATH = './output/test-comparison.csv'
 
+def run_subprocess(bash_command, env_vars):
+  env = os.environ.copy()
+  env.update(env_vars)
+  process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, env=env)
+
+  output, error = process.communicate()
+  return process.returncode # , output, error
+
 with open(OUTPUT_FILE_PATH, 'wb') as csvfile:
   writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-  writer.writerow(['package'])
-  for package in packages:
-    writer.writerow([package])
+  writer.writerow(['Package', 'Production exit code', 'Integration exit code'])
+  # for package in packages:
+  #   writer.writerow([package])
+
+  for package in packages[0:1]:
+    bash_command = TEST_COMMAND % package
+
+    print('Testing `%s` on production...' % package)
+    prod_return_code = run_subprocess(bash_command, PROD_ENV_VARS)
+
+    print('Testing `%s` on integration...' % package)
+    int_return_code = run_subprocess(bash_command, PROD_ENV_VARS)
+
+    writer.writerow([package, prod_return_code, int_return_code])
 
 print('Wrote output to: %s' % OUTPUT_FILE_PATH)
 print('Done.')
