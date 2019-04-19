3b0e7ae6b134f321e0b036be0d21a6861ae70441
Taymoor Khan <taykhan@cisco.com>
2019-04-18 17:01:22 -0700

    refactor(internal-plugin-user): move to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -30,7 +30,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/internal-plugin-presence \
   @ciscospark/internal-plugin-search \
   @ciscospark/internal-plugin-support \
-  @ciscospark/internal-plugin-team"
+  @ciscospark/internal-plugin-team \
+  @ciscospark/internal-plugin-user"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
