4b12129d48029590c38248c72e22adfb06f31cd1
Taymoor Khan <taykhan@cisco.com>
2019-04-19 12:37:27 -0700

    refactor(plugin-authorization): move to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -33,7 +33,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/internal-plugin-team \
   @ciscospark/internal-plugin-user \
   @ciscospark/internal-plugin-wdm \
-  @ciscospark/media-engine-webrtc"
+  @ciscospark/media-engine-webrtc \
+  @ciscospark/plugin-authorization"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
