67bf407a2159e3df770bd3c2f637a1359bec5a81
Taymoor Khan <taykhan@cisco.com>
2019-04-19 13:21:11 -0700

    refactor(plugin-authorization-browser-first-party): move to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -35,7 +35,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/internal-plugin-wdm \
   @ciscospark/media-engine-webrtc \
   @ciscospark/plugin-authorization \
-  @ciscospark/plugin-authorization-browser"
+  @ciscospark/plugin-authorization-browser \
+  @ciscospark/plugin-authorization-browser-first-party"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
