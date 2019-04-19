ca323f9028d854a3148673da4f56b0578ad63e0f
Taymoor Khan <taykhan@cisco.com>
2019-04-19 15:43:26 -0700

    refactor(plugin-rooms): move to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -42,7 +42,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/plugin-memberships \
   @ciscospark/plugin-messages \
   @ciscospark/plugin-people \
-  @ciscospark/plugin-phone"
+  @ciscospark/plugin-phone \
+  @ciscospark/plugin-rooms"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
