88acbd13655b391f55c5368badb7b822a4797848
Taymoor Khan <taykhan@cisco.com>
2019-04-19 17:38:03 -0700

    refactor(test-helper-file): move to webex org

--- tooling/lint-package-names.sh
+++ tooling/lint-package-names.sh
@@ -51,7 +51,8 @@ DEPRECATED_PACKAGES="@ciscospark/storage-adapter-session-storage \
   @ciscospark/storage-adapter-local-storage \
   @ciscospark/storage-adapter-spec \
   @ciscospark/test-helper-appid \
-  @ciscospark/test-helper-chai"
+  @ciscospark/test-helper-chai \
+  @ciscospark/test-helper-file"
 
 PACKAGES=$(echo packages/node_modules/{*,@ciscospark/*,@webex/*} | xargs -n 1 | sed 's/packages\/node_modules\///' | xargs -n 1 | grep -v '^@ciscospark$' | grep -v '^samples$' | grep -v '^@webex$')
 for PACKAGE in $PACKAGES; do
