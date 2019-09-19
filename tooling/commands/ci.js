cef67bbfbc57bac7c58cb07fbb03d29ef23e2b0f
Taymoor Khan <taymoork2@users.noreply.github.com>
2019-09-19 10:20:15 -0700

    Revert "Pipeline v3 - Merge Pipeline"

--- tooling/commands/ci.js
+++ tooling/commands/ci.js
@@ -62,7 +62,7 @@ module.exports = {
     const [cmd, ...args] = `npm run test --silent -- --packages ${packages.join(' ')} ${
       argv.github ?
         '--unit --os mac --browsers chrome firefox' :
-        '--browsers defaults'
+        '--integration --browsers defaults'
     }`.split(' ');
 
     await spawn(cmd, args);
