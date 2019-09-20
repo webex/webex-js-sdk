f68d85642a1b2170288705ec6cc0164d7855a60e
Taymoor Khan <taykhan@cisco.com>
2019-09-20 14:15:35 -0700

    chore(tooling): add a way to grab current version of SDK

---
+++ tooling/commands/version/current.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (c) 2015-2019 Cisco Systems, Inc. See LICENSE file.
+ */
+
+const wrapHandler = require('../../lib/wrap-handler');
+const {current} = require('../../lib/version');
+
+module.exports = {
+  command: 'current',
+  desc: 'Go to npm to read the current published version of any package in the repo',
+  builder: {},
+  handler: wrapHandler(async () => {
+    console.log(await current());
+  })
+};
