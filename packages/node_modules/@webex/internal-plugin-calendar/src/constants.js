9cb9feb41e882ee077dcd8f5f9506e13014b661b
Webex Publisher <webex-jenkins.gen@cisco.com>
2020-02-12 20:48:42 +0000

    feat(calendar): conversation sync api

---
+++ packages/node_modules/@webex/internal-plugin-calendar/src/constants.js
@@ -0,0 +1,6 @@
+export const CALENDAR_REGISTERED = 'calendar:registered';
+export const CALENDAR_UNREGISTERED = 'calendar:unregistered';
+export const CALENDAR_UPDATED = 'calendar:update';
+export const CALENDAR_DELETE = 'calendar:delete';
+export const CALENDAR_CREATE = 'calendar:create';
+export const CALENDAR = 'CALENDAR';
