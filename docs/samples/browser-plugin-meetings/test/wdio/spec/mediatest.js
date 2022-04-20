6bd256768b4f1c0bab7f09ec4bf50778125308b5
arungane <arungane@cisco.com>
2022-04-19 23:51:31 -0400

    test: upgrade wdio to latest

--- docs/samples/browser-plugin-meetings/test/wdio/spec/mediatest.js
+++ docs/samples/browser-plugin-meetings/test/wdio/spec/mediatest.js
@@ -1,41 +1,42 @@
 /* globals browser */
-import {expect} from 'chai';
 import {flaky} from '@webex/test-helper-mocha';
 import testUsers from '@webex/test-helper-test-users';
 
 describe('Meetings Plugin', () => {
-  let mccoy, spock;
+let mccoy, spock;
 
   before('create test users', () => testUsers.create({count: 2})
     .then((users) => {
       [mccoy, spock] = users;
 
       // Adding pause to let test users propagate through integration
-      browser.pause(2500);
     }));
 
-  before('reload browser', () => {
-    browser.refresh();
+  before('reload browser', async () => {
+    await browser.refresh();
   });
 
-  it('loads the app', () => {
-    browser.url('/samples/browser-plugin-meetings');
-    browser.$('#access-token').waitForExist({timeout: 1500});
+  it('loads the app', async () => {
+    await browser.url('/samples/browser-plugin-meetings');
+    await browser.$('#access-token').waitForExist({timeout: 1500});
   });
 
-  it('connects mccoys\'s browser', () => {
-    expect(browserChrome.getTitle()).to.equal('Webex JavaScript SDK Sample: Meetings Plugin');
-    browserChrome.execute((token) => {
+  it('connects mccoys\'s browser', async () => {
+    await expect(browserChrome).toHaveTitle('Webex JavaScript SDK Sample: Meetings Plugin')
+   
+    await browserChrome.execute((token) => {
       // eslint-disable-next-line no-undef
       document.getElementById('access-token').value += token;
     }, mccoy.token.access_token);
-    browserChrome.$('#access-token-save').click();
+    const element = await browserChrome.$('#access-token-save');
+    await element.scrollIntoView();
+    await element.click();
 
     // registeration
-    browserChrome.$('#registration-register').click();
-    browserChrome.waitUntil(() => {
+    await (await browserChrome.$('#registration-register')).click();
+    await browserChrome.waitUntil(async () => {
       // Test runs too quickly sometimes and tries to click before call is fully established
-      if (browserChrome.$('#access-token-status').getText() !== 'Saved access token!') {
+      if ((await (await browserChrome.$('#access-token-status')).getText()) !== 'Saved access token!') {
         return false;
       }
 
@@ -46,20 +47,22 @@ describe('Meetings Plugin', () => {
     });
   });
 
-  it('connects spock\'s browser', () => {
-    expect(browserFirefox.getTitle()).to.equal('Webex JavaScript SDK Sample: Meetings Plugin');
-    browserFirefox.execute((token) => {
+  it('connects spock\'s browser', async () => {
+    await expect(browserFirefox).toHaveTitle('Webex JavaScript SDK Sample: Meetings Plugin')
+    await browserFirefox.execute((token) => {
       // eslint-disable-next-line no-undef
       document.getElementById('access-token').value += token;
     }, spock.token.access_token);
-    browserFirefox.$('#access-token-save').click();
+    const element = await browserFirefox.$('#access-token-save');
+    await element.scrollIntoView();
+    await element.click();
 
     // registeration
-    browserFirefox.$('#registration-register').click();
+    await (await browserFirefox.$('#registration-register')).click();
 
-    browserFirefox.waitUntil(() => {
+    await browserFirefox.waitUntil(async () => {
       // Test runs too quickly sometimes and tries to click before call is fully established
-      if (browserFirefox.$('#access-token-status').getText() !== 'Saved access token!') {
+      if ((await (await browserFirefox.$('#access-token-status')).getText()) !== 'Saved access token!') {
         return false;
       }
 
@@ -70,23 +73,25 @@ describe('Meetings Plugin', () => {
     });
   });
 
-  it('places call from spock to mccoy', () => {
-    browserFirefox.$('#create-meeting-destination').setValue(mccoy.emailAddress);
-    browserFirefox.$('#create-meeting-action').click();
-    browserFirefox.pause(5000);
-    browserFirefox.$('#meetings-list label').click();
-    browserFirefox.$('#btn-join').click();
-    browserFirefox.pause(5000);
+  it('places call from spock to mccoy', async () => {
+    const createMeetingButton = await browserFirefox.$('#create-meeting-destination')
+    await createMeetingButton.scrollIntoView()
+    await createMeetingButton.setValue(mccoy.emailAddress);
+    await (await browserFirefox.$('#create-meeting-action')).click();
+    await browserFirefox.pause(5000);
+    await (await browserFirefox.$('#meetings-list label')).click();
+    await (await browserFirefox.$('#btn-join')).click();
+    await browserFirefox.pause(5000);
   });
 
-  flaky(it, process.env.SKIP_FLAKY_TESTS)('mccoy answers the call', () => {
-    browserChrome.pause(5000);
-    browserChrome.$('#answer').waitForDisplayed({timeout: 20000});
-    browserChrome.$('#answer').click();
-    browserChrome.pause(5000);
+  flaky(it, process.env.SKIP_FLAKY_TESTS)('mccoy answers the call', async () => {
+    await browserChrome.pause(5000);
+    await (await browserChrome.$('#answer')).waitForDisplayed({timeout: 20000});
+    await (await browserChrome.$('#answer')).click();
+    await browserChrome.pause(5000);
   }, 4);
 
-  it('ends the call', () => {
-    browser.$('#meetings-leave').click();
+  it('ends the call', async () => {
+    await (await browser.$('#meetings-leave')).click();
   });
 });
