3f308cdf630a3f6fc80ce2e72f03f4cde10ca2d8
Kesava Krishnan Madavan <kmadavan@cisco.com>
2022-08-10 17:00:13 +0530

    fix: remove internal-media-core from excludes
    Internal media core converts to cjs
    No excludes required at user end

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,6 @@ module.exports = (env = {NODE_ENV: process.env.NODE_ENV || 'production'}) => ({
         test: /\.js$/,
         include: [
           /packages\/node_modules/,
-          /node_modules\/@webex\/internal-media-core/
         ],
         use: {
           loader: 'babel-loader'
