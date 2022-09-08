cb56b8396d37e7c4e09f9d60aceb703b85e543e1
Rajesh Kumar <rarajes2@cisco.com>
2022-09-08 19:52:24 +0530

    test(plugin-meetings): fix media stream test

--- packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
+++ packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
@@ -671,7 +671,7 @@ describe('plugin-meetings', () => {
           const audioVideoSettings = {};
 
           sinon.stub(meeting.mediaProperties, 'videoDeviceId').value(videoDevice);
-
+          sinon.stub(meeting.mediaProperties, 'localQualityLevel').value('MEDIUM');
           await meeting.getMediaStreams(mediaDirection, audioVideoSettings);
 
           assert.calledWith(Media.getUserMedia, {
@@ -680,6 +680,8 @@ describe('plugin-meetings', () => {
           },
           {
             video: {
+              width: {max: 640, ideal: 640},
+              height: {max: 480, ideal: 480},
               deviceId: videoDevice
             }
           });
