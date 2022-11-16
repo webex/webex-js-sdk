9460a607be92ad78253a3ff6fcb05a9406fa289d
Srinivas Jayaram <104304483+srinijay98@users.noreply.github.com>
2022-11-16 22:14:35 +0530

    fix(voices): add deregister events (#2500)
    Co-authored-by: Srinivas <srinijay@cisco.com>

--- packages/@webex/internal-plugin-voicea/test/unit/spec/voicea.js
+++ packages/@webex/internal-plugin-voicea/test/unit/spec/voicea.js
@@ -71,6 +71,35 @@ describe('plugin-voicea', () => {
       });
     });
 
+    describe('#deregisterEvents', () => {
+      beforeEach(async () => {
+        const mockWebSocket = new MockWebSocket();
+
+        voiceaService.webex.internal.llm.socket = mockWebSocket;
+      });
+
+      it('deregisters voicea service', async () => {
+        voiceaService.listenToEvents();
+        await voiceaService.toggleTranscribing(true);
+
+        // eslint-disable-next-line no-underscore-dangle
+        voiceaService.webex.internal.llm._emit('event:relay.event', {
+          headers: {from: 'ws'},
+          data: {relayType: 'voicea.annc', voiceaPayload: {}},
+        });
+
+        assert.equal(voiceaService.hasVoiceaJoined, true);
+        assert.equal(voiceaService.areCaptionsEnabled, true);
+        assert.equal(voiceaService.isTranscribingEnabled, true);
+        assert.equal(voiceaService.vmcDeviceId, 'ws');
+
+        voiceaService.deregisterEvents();
+        assert.equal(voiceaService.hasVoiceaJoined, false);
+        assert.equal(voiceaService.areCaptionsEnabled, false);
+        assert.equal(voiceaService.isTranscribingEnabled, false);
+        assert.equal(voiceaService.vmcDeviceId, undefined);
+      });
+    });
     describe('#processAnnouncementMessage', () => {
       it('works on non-empty payload', async () => {
         const voiceaPayload = {
