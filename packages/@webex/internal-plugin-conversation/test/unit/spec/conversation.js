2d1cfa59bc1bc85617686728119886452c0409ec
Timothy Scheuering <InteractiveTimmy@users.noreply.github.com>
2023-04-14 12:51:23 -0400

    fix(automation): pass in env variables for the pull request (#2736)

--- packages/@webex/internal-plugin-conversation/test/unit/spec/conversation.js
+++ packages/@webex/internal-plugin-conversation/test/unit/spec/conversation.js
@@ -138,7 +138,7 @@ describe('plugin-conversation', () => {
       it('should convert a "us" cluster to WEBEX_CONVERSATION_DEFAULT_CLUSTER cluster', async () => {
         await webex.internal.conversation.getUrlFromClusterId({cluster: 'us'});
 
-        sinon.assert.calledWith(webex.internal.services.getServiceFromClusterId, {clusterId: 'urn:TEAM:us-east-2_a:identityLookup'});
+        sinon.assert.calledWith(webex.internal.services.getServiceFromClusterId, {clusterId: 'urn:TEAM:us-east-1_int13:identityLookup'});
       });
 
       it('should add the cluster service when missing', async () => {
