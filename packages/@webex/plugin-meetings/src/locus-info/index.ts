c458c7a642f89cd8ca0ef0017e4c6b75cba13f91
Mickel Ren (mickelr) <mickelr@cisco.com>
2023-04-17 17:35:13 +0800

    fix(meetings): need parse same meeting info when roles changed

--- packages/@webex/plugin-meetings/src/locus-info/index.ts
+++ packages/@webex/plugin-meetings/src/locus-info/index.ts
@@ -59,6 +59,7 @@ export default class LocusInfo extends EventsScope {
   fullState: any;
   host: any;
   info: any;
+  roles: any;
   mediaShares: any;
   replace: any;
   url: any;
@@ -946,8 +947,11 @@ export default class LocusInfo extends EventsScope {
    * @memberof LocusInfo
    */
   updateMeetingInfo(info: object, self?: object) {
-    if (info && !isEqual(this.info, info)) {
-      const roles = self ? SelfUtils.getRoles(self) : this.parsedLocus.self?.roles || [];
+    const roles = self ? SelfUtils.getRoles(self) : this.parsedLocus.self?.roles || [];
+    if (
+      (info && !isEqual(this.info, info)) ||
+      (roles.length && !isEqual(this.roles, roles) && info)
+    ) {
       const isJoined = SelfUtils.isJoined(self || this.parsedLocus.self);
       const parsedInfo = InfoUtils.getInfos(this.parsedLocus.info, info, roles, isJoined);
 
@@ -986,6 +990,7 @@ export default class LocusInfo extends EventsScope {
       // Parses the info and adds necessary values
       this.updateMeeting(parsedInfo.current);
     }
+    this.roles = roles;
   }
 
   /**
--- packages/@webex/plugin-meetings/test/unit/spec/locus-info/index.js
+++ packages/@webex/plugin-meetings/test/unit/spec/locus-info/index.js
@@ -18,7 +18,7 @@ import {
   DISPLAY_HINTS,
 } from '../../../../src/constants';
 
-import {self, selfWithInactivity} from './selfConstant';
+import { self, selfWithInactivity } from "./selfConstant";
 
 describe('plugin-meetings', () => {
   describe('LocusInfo index', () => {
@@ -1131,7 +1131,7 @@ describe('plugin-meetings', () => {
             function: 'updateMeetingInfo',
           },
           LOCUSINFO.EVENTS.MEETING_INFO_UPDATED,
-          {info: locusInfo.parsedLocus.info, self},
+         {info: locusInfo.parsedLocus.info, self},
         ];
 
         if (expected) {
@@ -1141,6 +1141,25 @@ describe('plugin-meetings', () => {
         }
         locusInfo.emitScoped.resetHistory();
       };
+
+      const checkMeetingInfoUpdatedCalledForRoles = (expected) => {
+        const expectedArgs = [
+          locusInfo.emitScoped,
+          {
+            file: 'locus-info',
+            function: 'updateMeetingInfo',
+          },
+          LOCUSINFO.EVENTS.MEETING_INFO_UPDATED,
+        ];
+
+        if (expected) {
+          assert.calledWith(...expectedArgs);
+        } else {
+          assert.neverCalledWith(...expectedArgs);
+        }
+        locusInfo.emitScoped.resetHistory();
+      };
+
 
       it('emits MEETING_INFO_UPDATED if the info changes', () => {
         const initialInfo = cloneDeep(meetingInfo);
@@ -1167,6 +1186,16 @@ describe('plugin-meetings', () => {
 
         // since the info is the same it should not call trigger the event
         checkMeetingInfoUpdatedCalled(false);
+
+        // update it with the same info, but roles changed
+        const updateSelf = cloneDeep(self);
+        updateSelf?.controls?.role?.roles.push({
+          type: 'COHOST',
+          hasRole: true,
+        });
+        locusInfo.updateMeetingInfo(newInfo, updateSelf);
+        // since the info is the same but roles changed, it should call trigger the event
+        checkMeetingInfoUpdatedCalledForRoles(true);
       });
 
       it('gets roles from self if available', () => {
