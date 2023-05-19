8587f003cae06ada3ba2cdc94dcc79a9b4ed3f5a
Othella Shao (weijshao) <weijshao@cisco.com>
2023-05-19 17:18:03 +0800

    feat(breakout): add unlockEdit parameter when update breakouts

--- packages/@webex/plugin-meetings/test/unit/spec/breakouts/index.ts
+++ packages/@webex/plugin-meetings/test/unit/spec/breakouts/index.ts
@@ -723,6 +723,24 @@ describe('plugin-meetings', () => {
           },
         });
       });
+      it('makes the request as expected when unlockEdit is true', async () => {
+        breakouts.editLock = {
+          token: 'token1',
+        };
+        const params = {
+          id: 'groupId',
+          sessions: [{name: 'Session 1'}],
+        };
+        const result = await breakouts.update(params, true);
+        assert.calledOnceWithExactly(webex.request, {
+          method: 'PUT',
+          uri: 'url',
+          body: {
+            editlock: {token: 'token1', refresh: false},
+            groups: [params],
+          },
+        });
+      });
       it('throw error if missing id in params', async () => {
         const params = {
           sessions: [{name: 'Session 1'}],
