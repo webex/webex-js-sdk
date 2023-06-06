60e5f936303d6cc052721b6fb17624bb771406b8
perfectt <perfectt@cisco.com>
2023-06-06 13:43:42 +0800

    feat(Live-Annotation): add url and shareInstanceId

---
+++ packages/@webex/plugin-meetings/test/unit/spec/locus-info/  mediaSharesUtils.ts
@@ -0,0 +1,22 @@
+import MediaSharesUtils from '@webex/plugin-meetings/src/locus-info/mediaSharesUtils';
+import {assert} from "chai";
+import Sinon from "sinon";
+
+describe('getShareInstanceId', () => {
+  it('getShareInstanceId return shareInstanceId value', () => {
+    const stub = Sinon.stub(MediaSharesUtils, 'extractContent').returns({ floor:{shareInstanceId:'shareInstanceId'}});
+    const shareInstanceId = MediaSharesUtils.getShareInstanceId();
+    assert.equal(shareInstanceId,'shareInstanceId');
+    stub.restore();
+  });
+});
+
+describe('getContentUrl', () => {
+  it('getContentUrl return url value', () => {
+    const stub = Sinon.stub(MediaSharesUtils, 'extractContent').returns({url:'url'});
+    const url = MediaSharesUtils.getContentUrl();
+    assert.equal(url,'url');
+    stub.restore();
+  });
+});
+
