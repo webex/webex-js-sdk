2175be940be2905a77b7234aa3f7623c5396efaf
Lisa Smith <82507773+lismith2-cisco@users.noreply.github.com>
2023-06-14 15:40:39 +0100

    feat(bulk dss): add batcher to dss (#2678)

--- packages/@webex/internal-plugin-dss/src/dss.ts
+++ packages/@webex/internal-plugin-dss/src/dss.ts
@@ -22,7 +22,13 @@ import {
   DSS_SERVICE_NAME,
   DSS_SEARCH_MERCURY_EVENT,
   DSS_RESULT,
+  LOOKUP_DATA_PATH,
+  LOOKUP_FOUND_PATH,
+  LOOKUP_NOT_FOUND_PATH,
+  LOOKUP_REQUEST_KEY,
+  SEARCH_DATA_PATH,
 } from './constants';
+import DssBatcher from './dss-batcher';
 
 const DSS = WebexPlugin.extend({
   namespace: 'DSS',
@@ -34,6 +40,18 @@ const DSS = WebexPlugin.extend({
    * @memberof DSS
    */
   registered: false,
+
+  /**
+   * Initializer
+   * @private
+   * @param {Object} attrs
+   * @param {Object} options
+   * @returns {undefined}
+   */
+  initialize(...args) {
+    Reflect.apply(WebexPlugin.prototype.initialize, this, args);
+    this.batchers = {};
+  },
 
   /**
    * Explicitly sets up the DSS plugin by connecting to mercury, and listening for DSS events.
@@ -114,6 +132,7 @@ const DSS = WebexPlugin.extend({
   },
 
   /**
+   * constructs the event name based on request id
    * @param {UUID} requestId the id of the request
    * @returns {string}
    */
@@ -122,6 +141,7 @@ const DSS = WebexPlugin.extend({
   },
 
   /**
+   * Takes incoming data and triggers correct events
    * @param {Object} data the event data
    * @returns {undefined}
    */
@@ -135,39 +155,67 @@ const DSS = WebexPlugin.extend({
    * @param {Object} options
    * @param {string} options.resource the URL to query
    * @param {string} options.params additional params for the body of the request
-   * @param {string} options.dataPath to path to get the data in the result object
-   * @returns {Promise} Resolves with an array of entities found
+   * @param {string} options.dataPath the path to get the data in the result object
+   * @param {string} options.foundPath the path to get the lookups of the found data (optional)
+   * @param {string} options.notFoundPath the path to get the lookups of the not found data (optional)
+   * @returns {Promise<Object>} result Resolves with an object
+   * @returns {Array} result.resultArray an array of entities found
+   * @returns {Array} result.foundArray an array of the lookups of the found entities (if foundPath provided)
+   * @returns {Array} result.notFoundArray an array of the lookups of the not found entities (if notFoundPath provided)
    */
   _request(options) {
-    const {resource, params, dataPath} = options;
+    const {resource, params, dataPath, foundPath, notFoundPath} = options;
 
     const requestId = uuid.v4();
     const eventName = this._getResultEventName(requestId);
     const result = {};
     let expectedSeqNums;
+    let notFoundArray;
 
     return new Promise((resolve) => {
       this.listenTo(this, eventName, (data) => {
-        const resultData = get(data, dataPath);
+        const resultData = get(data, dataPath, []);
+        let found;
 
-        result[data.sequence] = resultData;
+        if (foundPath) {
+          found = get(data, foundPath, []);
+        }
+        result[data.sequence] = foundPath ? {resultData, found} : {resultData};
 
         if (data.finished) {
           expectedSeqNums = range(data.sequence + 1).map(String);
+          if (notFoundPath) {
+            notFoundArray = get(data, notFoundPath, []);
+          }
         }
 
         const done = isEqual(expectedSeqNums, Object.keys(result));
 
         if (done) {
-          const resultArray = [];
+          const resultArray: any[] = [];
+          const foundArray: any[] = [];
+
           expectedSeqNums.forEach((index) => {
             const seqResult = result[index];
+
             if (seqResult) {
-              resultArray.push(...seqResult);
+              resultArray.push(...seqResult.resultData);
+              if (foundPath) {
+                foundArray.push(...seqResult.found);
+              }
             }
           });
+          const resolveValue: {resultArray: any[]; foundArray?: any[]; notFoundArray?: any[]} = {
+            resultArray,
+          };
 
-          resolve(resultArray);
+          if (foundPath) {
+            resolveValue.foundArray = foundArray;
+          }
+          if (notFoundPath) {
+            resolveValue.notFoundArray = notFoundArray;
+          }
+          resolve(resolveValue);
           this.stopListening(this, eventName);
         }
       });
@@ -182,58 +230,119 @@ const DSS = WebexPlugin.extend({
   },
 
   /**
+   * Uses a batcher to make the request to the directory service
+   * @param {Object} options
+   * @param {string} options.resource the URL to query
+   * @param {string} options.value the id or email to lookup
+   * @returns {Promise} Resolves with an array of entities found
+   */
+  _batchedLookup(options) {
+    const {resource, lookupValue} = options;
+    const dataPath = LOOKUP_DATA_PATH;
+    const entitiesFoundPath = LOOKUP_FOUND_PATH;
+    const entitiesNotFoundPath = LOOKUP_NOT_FOUND_PATH;
+    const requestKey = LOOKUP_REQUEST_KEY;
+
+    this.batchers[resource] =
+      this.batchers[resource] ||
+      new DssBatcher({
+        resource,
+        dataPath,
+        entitiesFoundPath,
+        entitiesNotFoundPath,
+        requestKey,
+        parent: this,
+      });
+
+    return this.batchers[resource].request(lookupValue);
+  },
+
+  /**
    * Retrieves detailed information about an entity
    * @param {Object} options
    * @param {UUID} options.id the id of the entity to lookup
-   * @returns {Promise} Resolves with an array of entities found
+   * @returns {Promise} Resolves with the entity found or null if not found
    */
   lookupDetail(options: LookupDetailOptions) {
     const {id} = options;
 
+    const resource = `/lookup/orgid/${this.webex.internal.device.orgId}/identity/${id}/detail`;
+
     return this._request({
-      dataPath: 'lookupResult.entities',
-      resource: `/lookup/orgid/${this.webex.internal.device.orgId}/identity/${id}/detail`,
+      dataPath: LOOKUP_DATA_PATH,
+      foundPath: LOOKUP_FOUND_PATH,
+      resource,
+    }).then(({resultArray, foundArray}) => {
+      // TODO: find out what is actually returned!
+      if (foundArray[0] === id) {
+        return resultArray[0];
+      }
+
+      return null;
     });
   },
 
   /**
-   * Retrieves basic information about a list entities within an organization
+   * Retrieves basic information about an entity within an organization
    * @param {Object} options
-   * @param {UUID} options.ids the id of the entity to lookup
+   * @param {UUID} options.id the id of the entity to lookup
    * @param {UUID} options.entityProviderType the provider to query (optional)
-   * @returns {Promise} Resolves with an array of entities found
+   * @param {Boolean} options.shouldBatch whether to batch the query, set to false for single immediate result (defaults to true)
+   * @returns {Promise} Resolves with the entity found or null if not found
    */
   lookup(options: LookupOptions) {
-    const {ids, entityProviderType} = options;
+    const {id, entityProviderType, shouldBatch = true} = options;
 
     const resource = entityProviderType
       ? `/lookup/orgid/${this.webex.internal.device.orgId}/entityprovidertype/${entityProviderType}`
       : `/lookup/orgid/${this.webex.internal.device.orgId}/identities`;
 
+    if (shouldBatch) {
+      return this._batchedLookup({
+        resource,
+        lookupValue: id,
+      });
+    }
+
     return this._request({
-      dataPath: 'lookupResult.entities',
+      dataPath: LOOKUP_DATA_PATH,
+      foundPath: LOOKUP_FOUND_PATH,
       resource,
       params: {
-        lookupValues: ids,
+        [LOOKUP_REQUEST_KEY]: [id],
       },
+    }).then(({resultArray, foundArray}) => {
+      if (foundArray[0] === id) {
+        return resultArray[0];
+      }
+
+      return null;
     });
   },
 
   /**
-   * Retrieves basic information about a list entities within an organization
+   * Retrieves basic information about an enitity within an organization
    * @param {Object} options
-   * @param {UUID} options.emails the emails of the entities to lookup
-   * @returns {Promise} Resolves with an array of entities found
+   * @param {UUID} options.email the email of the entity to lookup
+   * @returns {Promise} Resolves with the entity found or rejects if not found
    */
   lookupByEmail(options: LookupByEmailOptions) {
-    const {emails} = options;
+    const {email} = options;
+    const resource = `/lookup/orgid/${this.webex.internal.device.orgId}/emails`;
 
     return this._request({
-      dataPath: 'lookupResult.entities',
-      resource: `/lookup/orgid/${this.webex.internal.device.orgId}/emails`,
+      dataPath: LOOKUP_DATA_PATH,
+      foundPath: LOOKUP_FOUND_PATH,
+      resource,
       params: {
-        lookupValues: emails,
+        [LOOKUP_REQUEST_KEY]: [email],
       },
+    }).then(({resultArray, foundArray}) => {
+      if (foundArray[0] === email) {
+        return resultArray[0];
+      }
+
+      return null;
     });
   },
 
@@ -249,14 +358,14 @@ const DSS = WebexPlugin.extend({
     const {requestedTypes, resultSize, queryString} = options;
 
     return this._request({
-      dataPath: 'directoryEntities',
+      dataPath: SEARCH_DATA_PATH,
       resource: `/search/orgid/${this.webex.internal.device.orgId}/entities`,
       params: {
         queryString,
         resultSize,
         requestedTypes,
       },
-    });
+    }).then(({resultArray}) => resultArray);
   },
 });
 
