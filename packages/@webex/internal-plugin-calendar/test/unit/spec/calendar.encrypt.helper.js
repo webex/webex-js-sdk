718da73b7160b5dcb4d8bf23774f1101c743a933
Yilong Zheng (yilzheng) <yilzheng@cisco.com>
2023-07-12 13:38:28 +0800

    fix(plugin-calendar): enhance getFreeBusy function

--- packages/@webex/internal-plugin-calendar/test/unit/spec/calendar.encrypt.helper.js
+++ packages/@webex/internal-plugin-calendar/test/unit/spec/calendar.encrypt.helper.js
@@ -1,12 +1,12 @@
 import sinon from 'sinon';
 import {expect} from '@webex/test-helper-chai';
-import EncryptHelper from "@webex/internal-plugin-calendar/src/calendar.encrypt.helper"
+import EncryptHelper from '@webex/internal-plugin-calendar/src/calendar.encrypt.helper';
 describe('internal-plugin-calendar', () => {
   describe('encryptHelper', () => {
     let ctx;
     beforeEach(() => {
       ctx = {
-        encryptionKeyUrl: "http://example.com/encryption-key",
+        encryptionKeyUrl: 'http://example.com/encryption-key',
         webex: {
           internal: {
             encryption: {
@@ -23,30 +23,28 @@ describe('internal-plugin-calendar', () => {
 
     it('#encryptFreebusyRequestData with emails should ', async () => {
       const freeBusyRequest = {
-        start:"20230712T10:20:00Z",
-        end:"20230712T20:20:00Z",
-        emails: [
-          "test@webex.com"
-        ]
+        start: '20230712T10:20:00Z',
+        end: '20230712T20:20:00Z',
+        emails: ['test@webex.com'],
       };
-        const expectedCiphertext = 'some encrpty data';
-        ctx.webex.internal.encryption.encryptText.callsFake((key, ciphertext) =>
-          Promise.resolve(expectedCiphertext)
-        );
+      const expectedCiphertext = 'some encrpty data';
+      ctx.webex.internal.encryption.encryptText.callsFake((key, ciphertext) =>
+        Promise.resolve(expectedCiphertext)
+      );
       await EncryptHelper.encryptFreeBusyRequest(ctx, freeBusyRequest);
       expect(freeBusyRequest.emails[0]).to.be.equal(expectedCiphertext);
     });
 
     it('#encryptFreebusyRequestData not include emails, but include ids- should b', async () => {
       const freeBusyRequest = {
-        start: "20230712T10:20:00Z",
-        end: "20230712T20:20:00Z",
-        userIds: ["91aee1231"],
+        start: '20230712T10:20:00Z',
+        end: '20230712T20:20:00Z',
+        userIds: ['91aee1231'],
       };
-       const expectedCiphertext = "91aee1231";
-       ctx.webex.internal.encryption.encryptText.callsFake((key, ciphertext) =>
-         Promise.resolve(expectedCiphertext)
-       );
+      const expectedCiphertext = '91aee1231';
+      ctx.webex.internal.encryption.encryptText.callsFake((key, ciphertext) =>
+        Promise.resolve(expectedCiphertext)
+      );
       await EncryptHelper.encryptFreeBusyRequest(ctx, freeBusyRequest);
       expect(freeBusyRequest.userIds[0]).to.equal(expectedCiphertext);
     });
