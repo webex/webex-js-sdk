ed87deb8637144728ae1db5ed71fb4e442d99e5c
Sreekanth Narayanan <sreenara@cisco.com>
2023-08-04 14:20:25 +0530

    fix(test): test commit to try out increment and pipeline

--- packages/@webex/internal-plugin-flag/src/flag.js
+++ packages/@webex/internal-plugin-flag/src/flag.js
@@ -171,6 +171,15 @@ const Flag = WebexCore.WebexPlugin.extend({
 
     return this.webex.request(params).then((res) => res.body);
   },
+
+  /**
+   * Test function
+   * @param {Object} options
+   * @returns {Promise<Object>} Resolves with the flag removal
+   */
+  testFlag(options) {
+    return this.webex.request(options);
+  },
 });
 
 export default Flag;
