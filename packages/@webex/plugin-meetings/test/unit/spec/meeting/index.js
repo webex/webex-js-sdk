3e5e80061be835c55e18216b01079c8b4ecd860c
perfect <perfectt@cisco.com>
2023-09-07 14:29:53 +0800

    fix(Live-Annotation): remove the presenter side annotation info (#3048)

--- packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
+++ packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
@@ -4041,14 +4041,6 @@ describe('plugin-meetings', () => {
             checkScreenShareAudioPublished(audioShareTrack);
           });
         });
-        it('creates instance and publishes with annotation info', async () => {
-          const annotationInfo = {
-            version: '1',
-            policy: ANNOTATION_POLICY.APPROVAL,
-          };
-          await meeting.publishTracks({annotationInfo});
-          assert.equal(meeting.annotationInfo, annotationInfo);
-        });
 
         describe('unpublishTracks', () => {
           beforeEach(async () => {
