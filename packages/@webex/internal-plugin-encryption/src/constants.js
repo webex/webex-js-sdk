80527cb266869d201b3dc1074b729d4838bc1d6f
shivani1211 <shivaniyadav1211@gmail.com>
2023-09-15 10:27:05 -0700

    feat(): byok revoked error handling (#3083)

---
+++ packages/@webex/internal-plugin-encryption/src/constants.js
@@ -0,0 +1,3 @@
+export const KMS_KEY_REVOKE_FAILURE = 'event:kms:key:revoke:encryption:failure';
+export const KMS_KEY_REVOKE_ERROR_STATUS = 405;
+export const KMS_KEY_REVOKE_ERROR_CODES = [405005, 405006];
