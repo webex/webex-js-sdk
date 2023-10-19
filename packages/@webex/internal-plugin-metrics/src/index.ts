0558a6bc83bf507ad957ab9d8221a9fc8a700cfb
Catalin <ctorge@cisco.com>
2023-10-19 18:40:52 +0100

    feat(ca): add temporary flag for buildType

--- packages/@webex/internal-plugin-metrics/src/index.ts
+++ packages/@webex/internal-plugin-metrics/src/index.ts
@@ -20,6 +20,7 @@ import {
   SubmitMQE,
 } from './metrics.types';
 import * as CALL_DIAGNOSTIC_CONFIG from './call-diagnostic/config';
+import * as CallDiagnosticUtils from './call-diagnostic/call-diagnostic-metrics.util';
 
 registerInternalPlugin('metrics', Metrics, {
   config,
@@ -30,7 +31,7 @@ registerInternalPlugin('newMetrics', NewMetrics, {
 });
 
 export {default, getOSNameInternal} from './metrics';
-export {config, CALL_DIAGNOSTIC_CONFIG, NewMetrics, Utils};
+export {config, CALL_DIAGNOSTIC_CONFIG, NewMetrics, Utils, CallDiagnosticUtils};
 export type {
   ClientEvent,
   ClientEventLeaveReason,
