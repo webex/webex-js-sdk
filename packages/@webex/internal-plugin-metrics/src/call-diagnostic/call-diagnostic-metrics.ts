a427035503951b2e1e8fa7962fe4dcc62b2fdba5
Colin Read <coread@cisco.com>
2023-12-01 15:45:37 +0000

    feat(plugin-metrics): classify network errors

--- packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics.ts
+++ packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics.ts
@@ -16,6 +16,7 @@ import {
   extractVersionMetadata,
   isMeetingInfoServiceError,
   isBrowserMediaErrorName,
+  isNetworkError,
 } from './call-diagnostic-metrics.util';
 import {CLIENT_NAME} from '../config';
 import {
@@ -46,6 +47,7 @@ import {
   BROWSER_MEDIA_ERROR_NAME_TO_CLIENT_ERROR_CODES_MAP,
   MEETING_INFO_LOOKUP_ERROR_CLIENT_CODE,
   CALL_DIAGNOSTIC_LOG_IDENTIFIER,
+  NETWORK_ERROR,
 } from './config';
 import {generateCommonErrorMetadata} from '../utils';
 
@@ -439,6 +441,13 @@ export default class CallDiagnosticMetrics extends StatelessWebexPlugin {
     if (isMeetingInfoServiceError(rawError)) {
       return this.getErrorPayloadForClientErrorCode({
         clientErrorCode: MEETING_INFO_LOOKUP_ERROR_CLIENT_CODE,
+        serviceErrorCode,
+      });
+    }
+
+    if (isNetworkError(rawError)) {
+      return this.getErrorPayloadForClientErrorCode({
+        clientErrorCode: NETWORK_ERROR,
         serviceErrorCode,
       });
     }
