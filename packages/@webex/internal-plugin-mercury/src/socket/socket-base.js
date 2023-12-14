d66e6248acdd5b8c38acdfaa77ce3c43f85dd608
Laszlo Vadasz <lvadasz@cisco.com>
2023-12-14 16:07:12 +0000

    fix: log in socket-base

--- packages/@webex/internal-plugin-mercury/src/socket/socket-base.js
+++ packages/@webex/internal-plugin-mercury/src/socket/socket-base.js
@@ -30,7 +30,7 @@ export default class Socket extends EventEmitter {
    */
   constructor() {
     super();
-    this.domain = 'unknown-domain';
+    this._domain = 'unknown-domain';
     this.onmessage = this.onmessage.bind(this);
     this.onclose = this.onclose.bind(this);
   }
@@ -112,10 +112,10 @@ export default class Socket extends EventEmitter {
         return;
       }
       // logger is defined once open is called
-      this.logger.info(`socket,${this.domain}: closing`);
+      this.logger.info(`socket,${this._domain}: closing`);
 
       if (socket.readyState === 2 || socket.readyState === 3) {
-        this.logger.info(`socket,${this.domain}: already closed`);
+        this.logger.info(`socket,${this._domain}: already closed`);
         resolve();
 
         return;
@@ -135,7 +135,7 @@ export default class Socket extends EventEmitter {
 
       const closeTimer = safeSetTimeout(() => {
         try {
-          this.logger.info(`socket,${this.domain}: no close event received, forcing closure`);
+          this.logger.info(`socket,${this._domain}: no close event received, forcing closure`);
           resolve(
             this.onclose({
               code: 1000,
@@ -143,12 +143,12 @@ export default class Socket extends EventEmitter {
             })
           );
         } catch (error) {
-          this.logger.warn(`socket,${this.domain}: force-close failed`, error);
+          this.logger.warn(`socket,${this._domain}: force-close failed`, error);
         }
       }, this.forceCloseDelay);
 
       socket.onclose = (event) => {
-        this.logger.info(`socket,${this.domain}: close event fired`, event.code, event.reason);
+        this.logger.info(`socket,${this._domain}: close event fired`, event.code, event.reason);
         clearTimeout(closeTimer);
         this.onclose(event);
         resolve(event);
@@ -174,7 +174,7 @@ export default class Socket extends EventEmitter {
    */
   open(url, options) {
     try {
-      this.domain = new URL(url).hostname;
+      this._domain = new URL(url).hostname;
     } catch {
       /* Ignore error */
     }
@@ -209,7 +209,7 @@ export default class Socket extends EventEmitter {
 
       const WebSocket = Socket.getWebSocketConstructor();
 
-      this.logger.info(`socket,${this.domain}: creating WebSocket`);
+      this.logger.info(`socket,${this._domain}: creating WebSocket`);
       const socket = new WebSocket(url, [], options);
 
       socket.binaryType = 'arraybuffer';
@@ -217,7 +217,7 @@ export default class Socket extends EventEmitter {
 
       socket.onclose = (event) => {
         event = this._fixCloseCode(event);
-        this.logger.info(`socket,${this.domain}: closed before open`, event.code, event.reason);
+        this.logger.info(`socket,${this._domain}: closed before open`, event.code, event.reason);
         switch (event.code) {
           case 1005:
             // IE 11 doesn't seem to allow 4XXX codes, so if we get a 1005, assume
@@ -239,10 +239,10 @@ export default class Socket extends EventEmitter {
       };
 
       socket.onopen = () => {
-        this.logger.info(`socket,${this.domain}: connected`);
+        this.logger.info(`socket,${this._domain}: connected`);
         this._authorize()
           .then(() => {
-            this.logger.info(`socket,${this.domain}: authorized`);
+            this.logger.info(`socket,${this._domain}: authorized`);
             socket.onclose = this.onclose;
             resolve();
           })
@@ -250,11 +250,11 @@ export default class Socket extends EventEmitter {
       };
 
       socket.onerror = (event) => {
-        this.logger.warn(`socket,${this.domain}: error event fired`, event);
+        this.logger.warn(`socket,${this._domain}: error event fired`, event);
       };
 
       sockets.set(this, socket);
-      this.logger.info(`socket,${this.domain}: waiting for server`);
+      this.logger.info(`socket,${this._domain}: waiting for server`);
     });
   }
 
@@ -264,7 +264,7 @@ export default class Socket extends EventEmitter {
    * @returns {undefined}
    */
   onclose(event) {
-    this.logger.info(`socket,${this.domain}: closed`, event.code, event.reason);
+    this.logger.info(`socket,${this._domain}: closed`, event.code, event.reason);
     clearTimeout(this.pongTimer);
     clearTimeout(this.pingTimer);
 
@@ -286,10 +286,10 @@ export default class Socket extends EventEmitter {
       const data = JSON.parse(event.data);
       const sequenceNumber = parseInt(data.sequenceNumber, 10);
 
-      this.logger.debug(`socket,${this.domain}: sequence number: `, sequenceNumber);
+      this.logger.debug(`socket,${this._domain}: sequence number: `, sequenceNumber);
       if (this.expectedSequenceNumber && sequenceNumber !== this.expectedSequenceNumber) {
         this.logger.debug(
-          `socket,${this.domain}: sequence number mismatch indicates lost mercury message. expected: ${this.expectedSequenceNumber}, actual: ${sequenceNumber}`
+          `socket,${this._domain}: sequence number mismatch indicates lost mercury message. expected: ${this.expectedSequenceNumber}, actual: ${sequenceNumber}`
         );
         this.emit('sequence-mismatch', sequenceNumber, this.expectedSequenceNumber);
       }
@@ -311,7 +311,7 @@ export default class Socket extends EventEmitter {
       // message from Mercury. At this time, the only action we have is to
       // ignore it and move on.
       /* istanbul ignore next */
-      this.logger.warn(`socket,${this.domain}: error while receiving WebSocket message`, error);
+      this.logger.warn(`socket,${this._domain}: error while receiving WebSocket message`, error);
     }
   }
 
@@ -365,7 +365,7 @@ export default class Socket extends EventEmitter {
    */
   _authorize() {
     return new Promise((resolve) => {
-      this.logger.info(`socket,${this.domain}: authorizing`);
+      this.logger.info(`socket,${this._domain}: authorizing`);
       this.send({
         id: uuid.v4(),
         type: 'authorization',
@@ -404,7 +404,7 @@ export default class Socket extends EventEmitter {
       switch (event.reason.toLowerCase()) {
         case 'replaced':
           this.logger.info(
-            `socket,${this.domain}: fixing CloseEvent code for reason: `,
+            `socket,${this._domain}: fixing CloseEvent code for reason: `,
             event.reason
           );
           event.code = 4000;
@@ -412,7 +412,7 @@ export default class Socket extends EventEmitter {
         case 'authentication failed':
         case 'authentication did not happen within the timeout window of 30000 seconds.':
           this.logger.info(
-            `socket,${this.domain}: fixing CloseEvent code for reason: `,
+            `socket,${this._domain}: fixing CloseEvent code for reason: `,
             event.reason
           );
           event.code = 1008;
@@ -434,12 +434,12 @@ export default class Socket extends EventEmitter {
   _ping(id) {
     const confirmPongId = (event) => {
       try {
-        this.logger.debug(`socket,${this.domain}: pong`, event.data.id);
+        this.logger.debug(`socket,${this._domain}: pong`, event.data.id);
         if (event.data && event.data.id !== id) {
           this.logger.info(
-            `socket,${this.domain}: received pong for wrong ping id, closing socket`
+            `socket,${this._domain}: received pong for wrong ping id, closing socket`
           );
-          this.logger.debug(`socket,${this.domain}: expected`, id, 'received', event.data.id);
+          this.logger.debug(`socket,${this._domain}: expected`, id, 'received', event.data.id);
           this.close({
             code: 1000,
             reason: 'Pong mismatch',
@@ -449,21 +449,21 @@ export default class Socket extends EventEmitter {
         // This try/catch block was added as a debugging step; to the best of my
         // knowledge, the above can never throw.
         /* istanbul ignore next */
-        this.logger.error(`socket,${this.domain}: error occurred in confirmPongId`, error);
+        this.logger.error(`socket,${this._domain}: error occurred in confirmPongId`, error);
       }
     };
 
     const onPongNotReceived = () => {
       try {
         this.logger.info(
-          `socket,${this.domain}: pong not receive in expected period, closing socket`
+          `socket,${this._domain}: pong not receive in expected period, closing socket`
         );
         this.close({
           code: 1000,
           reason: 'Pong not received',
         }).catch((reason) => {
           this.logger.warn(
-            `socket,${this.domain}: failed to close socket after missed pong`,
+            `socket,${this._domain}: failed to close socket after missed pong`,
             reason
           );
         });
@@ -471,7 +471,7 @@ export default class Socket extends EventEmitter {
         // This try/catch block was added as a debugging step; to the best of my
         // knowledge, the above can never throw.
         /* istanbul ignore next */
-        this.logger.error(`socket,${this.domain}: error occurred in onPongNotReceived`, error);
+        this.logger.error(`socket,${this._domain}: error occurred in onPongNotReceived`, error);
       }
     };
 
@@ -484,7 +484,7 @@ export default class Socket extends EventEmitter {
         // knowledge, the above can never throw.
         /* istanbul ignore next */
         this.logger.error(
-          `socket,${this.domain}: error occurred in scheduleNextPingAndCancelPongTimer`,
+          `socket,${this._domain}: error occurred in scheduleNextPingAndCancelPongTimer`,
           error
         );
       }
@@ -495,7 +495,7 @@ export default class Socket extends EventEmitter {
     this.once('pong', scheduleNextPingAndCancelPongTimer);
     this.once('pong', confirmPongId);
 
-    this.logger.debug(`socket,${this.domain}: ping ${id}`);
+    this.logger.debug(`socket,${this._domain}: ping ${id}`);
 
     return this.send({
       id,
