399e695826661a7760a50ec1fb1188a60d511c4b
Sreekanth Narayanan <sreenara@cisco.com>
2024-01-10 19:51:24 +0530

    fix(ut): add tests for the new reclaim host param

--- packages/@webex/plugin-meetings/test/unit/spec/members/utils.js
+++ packages/@webex/plugin-meetings/test/unit/spec/members/utils.js
@@ -38,5 +38,58 @@ describe('plugin-meetings', () => {
         );
       });
     });
+
+    describe('#getRoleAssignmentMemberRequestParams', () => {
+      it('returns the correct request params', () => {
+        const format = {
+          locusUrl: 'locusUrl',
+          memberId: 'test',
+          roles: [
+            {type: 'PRESENTER', hasRole: true},
+            {type: 'MODERATOR', hasRole: false},
+            {type: 'COHOST', hasRole: true},
+          ],
+        };
+        assert.deepEqual(MembersUtil.getRoleAssignmentMemberRequestParams(format), {
+          method: 'PATCH',
+          uri: `locusUrl/${PARTICIPANT}/test/${CONTROLS}`,
+          body: {
+            role: {
+              roles: [
+                {type: 'PRESENTER', hasRole: true},
+                {type: 'MODERATOR', hasRole: false},
+                {type: 'COHOST', hasRole: true},
+              ],
+            },
+          },
+        });
+      });
+
+      it('returns the correct request params with a hostKey', () => {
+        const format = {
+          locusUrl: 'locusUrl',
+          memberId: 'test',
+          roles: [
+            {type: 'PRESENTER', hasRole: true, hostKey: '123456'},
+            {type: 'MODERATOR', hasRole: false, hostKey: '123456'},
+            {type: 'COHOST', hasRole: true, hostKey: '123456'},
+          ],
+        };
+
+        assert.deepEqual(MembersUtil.getRoleAssignmentMemberRequestParams(format), {
+          method: 'PATCH',
+          uri: `locusUrl/${PARTICIPANT}/test/${CONTROLS}`,
+          body: {
+            role: {
+              roles: [
+                {type: 'PRESENTER', hasRole: true, hostKey: '123456'},
+                {type: 'MODERATOR', hasRole: false, hostKey: '123456'},
+                {type: 'COHOST', hasRole: true, hostKey: '123456'},
+              ],
+            },
+          },
+        });
+      });
+    });
   });
 });
