7b22b1492a757434b1380a2c62bb1dbf2a1aabae
johnsoter13 <john.soter@outlook.com>
2024-02-20 16:16:35 -0500

    fix: code update

--- packages/@webex/plugin-meetings/src/meeting/util.ts
+++ packages/@webex/plugin-meetings/src/meeting/util.ts
@@ -763,7 +763,7 @@ const MeetingUtil = {
         {
           ...errorPayload,
           fatal: true,
-          category: 'expected',
+          category: 'signaling',
           errorCode: 4066,
         },
       ];
