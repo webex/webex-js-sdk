ce17dee6f18f98395ae01d589c04ff9e80673e6e
John Soter <john.soter@outlook.com>
2024-03-11 13:35:54 -0400

    fix(plugin-meetings): shareInstanceId to requestScreenShareFloor request (#3433)

--- packages/@webex/plugin-meetings/src/meeting/index.ts
+++ packages/@webex/plugin-meetings/src/meeting/index.ts
@@ -7224,6 +7224,7 @@ export default class Meeting extends StatelessWebexPlugin {
             deviceUrl: this.deviceUrl,
             uri: content.url,
             resourceUrl: this.resourceUrl,
+            shareInstanceId: this.localShareInstanceId,
           })
           .then(() => {
             this.screenShareFloorState = ScreenShareFloorStatus.GRANTED;
@@ -7326,6 +7327,7 @@ export default class Meeting extends StatelessWebexPlugin {
           deviceUrl: this.deviceUrl,
           uri: content.url,
           resourceUrl: this.resourceUrl,
+          shareInstanceId: this.localShareInstanceId,
         })
         .catch((error) => {
           LoggerProxy.logger.error('Meeting:index#releaseScreenShareFloor --> Error ', error);
