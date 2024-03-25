e946789b05aa44388928df9de665e8c135a911fc
arungane <arungane@cisco.com>
2024-03-24 22:39:52 -0400

    fix: broken test waiting for local fixtures

--- packages/legacy/tools/src/utils/index.ts
+++ packages/legacy/tools/src/utils/index.ts
@@ -1,9 +1,15 @@
 import Jest from './jest';
 import Karma from './karma';
+import {
+  startProxies,
+  stopProxies,
+} from './karma/proxies';
 import Mocha from './mocha';
 
 export {
   Jest,
   Karma,
   Mocha,
+  startProxies,
+  stopProxies,
 };
