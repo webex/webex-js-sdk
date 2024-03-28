90b8e6cdfce6e09ffddd1d9c9a78603a71372d62
Coread <coread@cisco.com>
2024-03-28 11:32:07 +0000

    feat(internal-plugin-metrics): allow payload modifications for unknow… (#3492)

--- packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics.ts
+++ packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics.ts
@@ -607,6 +607,7 @@ export default class CallDiagnosticMetrics extends StatelessWebexPlugin {
     return this.getErrorPayloadForClientErrorCode({
       clientErrorCode: UNKNOWN_ERROR,
       serviceErrorCode: UNKNOWN_ERROR,
+      payloadOverrides: rawError.payloadOverrides,
       rawErrorMessage,
       httpStatusCode,
     });
