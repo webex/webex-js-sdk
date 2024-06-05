7c4819997282e6552939b287beca50661157e7d0
Shreyas Sharma <shreysh2@cisco.com>
2024-06-05 23:35:31 +0530

    fix(plugin-meetings): export-meetinginfoutil-as-class

--- packages/@webex/plugin-meetings/src/index.ts
+++ packages/@webex/plugin-meetings/src/index.ts
@@ -15,6 +15,8 @@ import {
   ReclaimHostNotSupportedError,
 } from './common/errors/reclaim-host-role-errors';
 import Meeting from './meeting';
+import MeetingInfoUtil from './meeting-info/utilv2';
+import JoinMeetingError from './common/errors/join-meeting';
 
 registerPlugin('meetings', Meetings, {
   config,
@@ -51,13 +53,13 @@ export default Meetings;
 export * as CONSTANTS from './constants';
 export * as REACTIONS from './reactions/reactions';
 export * as sdkAnnotationTypes from './annotation/annotation.types';
-export * as MeetingInfoUtil from './meeting-info/utilv2';
 export * as MeetingInfoV2 from './meeting-info/meeting-info-v2';
 export {type Reaction} from './reactions/reactions.type';
 
 export {
   CaptchaError,
   IntentToJoinError,
+  JoinMeetingError,
   PasswordError,
   PermissionError,
   ReclaimHostIsHostAlreadyError,
@@ -65,6 +67,7 @@ export {
   ReclaimHostNotSupportedError,
   ReclaimHostEmptyWrongKeyError,
   Meeting,
+  MeetingInfoUtil,
 };
 
 export {RemoteMedia} from './multistream/remoteMedia';
