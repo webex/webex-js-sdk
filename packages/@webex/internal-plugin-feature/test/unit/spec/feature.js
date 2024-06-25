34340d71ce38227676d37ba081d76e57bc3ddff7
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2024-06-25 09:40:51 +0530

    fix: added mistakenly removed tests

--- packages/@webex/internal-plugin-feature/test/unit/spec/feature.js
+++ packages/@webex/internal-plugin-feature/test/unit/spec/feature.js
@@ -65,6 +65,39 @@ describe('plugin-feature', () => {
       });
     });
 
+    describe('#setFeature()', () => {
+      beforeEach(() => {
+        webex.request = sinon.stub().returns(
+          Promise.resolve({
+            body: {},
+            statusCode: 200,
+          })
+        );
+      });
+      afterEach(() => {
+        webex.request.resetHistory();
+      });
+      it('does not allow entitlement keyType to be set', () =>
+        assert.isRejected(
+          webex.internal.feature.setFeature('entitlement', 'featureName', true),
+          /Only `developer` and `user` feature toggles can be set./
+        ));
+
+      ['developer', 'user'].forEach((keyType) => {
+        it(`sets a value for a ${keyType} feature toggle`, () => {
+          sinon.spy(webex.internal.device.features[keyType], 'add');
+
+          return webex.internal.feature.setFeature(keyType, 'featureName', true).then(() => {
+            assert.called(webex.internal.device.features[keyType].add);
+            assert.equal(
+              webex.request.getCall(0).args[0].resource,
+              `features/users/${webex.internal.device.userId}/${keyType}`
+            );
+          });
+        });
+      });
+    });
+
     describe('#updateFeature', () => {
       it('updates the feature toggle', () => {
         const key = 'featureName';
