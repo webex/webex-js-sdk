6440b3d3047516daea2ea0e7bd8ef374e4e0cc33
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2024-07-23 08:54:40 +0530

    fix(CallingClient): add cline at session level for answer sdp

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -2375,6 +2375,24 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
 
   /* istanbul ignore next */
   /**
+   * Copy SDP's c-line to session level from media level.
+   * SPARK-522437
+   */
+  private addSessionConnection(sdp: string): string {
+    const lines: string[] = sdp.split(/\r\n|\r|\n/);
+    const mIndex: number = lines.findIndex((line) => line.startsWith('m='));
+    const tIndex: number = lines.findIndex((line) => line.startsWith('t='));
+
+    if (mIndex !== -1 && mIndex < lines.length - 1 && lines[mIndex + 1].startsWith('c=')) {
+      const cLine: string = lines[mIndex + 1];
+      lines.splice(tIndex, 0, cLine);
+    }
+
+    return lines.join('\r\n');
+  }
+
+  /* istanbul ignore next */
+  /**
    * Setup a listener for roap events emitted by the media sdk.
    */
   private mediaRoapEventsListener() {
@@ -2416,10 +2434,13 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
             break;
           }
 
-          case RoapScenario.ANSWER:
+          case RoapScenario.ANSWER: {
+            const sdp = this.addSessionConnection(event.roapMessage.sdp);
+            event.roapMessage.sdp = sdp;
             this.localRoapMessage = event.roapMessage;
             this.sendMediaStateMachineEvt({type: 'E_SEND_ROAP_ANSWER', data: event.roapMessage});
             break;
+          }
 
           case RoapScenario.ERROR:
             this.sendMediaStateMachineEvt({type: 'E_ROAP_ERROR', data: event.roapMessage});
