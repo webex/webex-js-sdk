6e10581e1fbf17b3026fba85b3fcb82e510df857
Bharath Balan <62698609+bhabalan@users.noreply.github.com>
2024-09-05 10:25:05 +0530

    feat(byods): create @webex/byods package

---
+++ packages/byods/src/BYODS.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import fetch from 'node-fetch';
+import {LocalStream} from '@webex/media-helpers'; // Just to show that you can import other packages from the workspace
+
+interface SDKConfig {
+  clientId: string;
+  clientSecret: string;
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: Date;
+}
+
+class BYODS {
+  private clientId: string;
+  private clientSecret: string;
+  private tokenHost: string;
+  private accessToken: string;
+  private refreshToken: string;
+  private expiresAt: Date;
+
+  constructor({clientId, clientSecret, accessToken, refreshToken, expiresAt}: SDKConfig) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+    this.expiresAt = expiresAt;
+    this.tokenHost = 'https://webexapis.com/v1/access_token';
+  }
+
+  public async makeAuthenticatedRequest(endpoint: string): Promise<any> {
+    if (new Date() >= new Date(this.expiresAt as Date)) {
+      throw new Error('Token has expired');
+    }
+
+    // Use this.token.access_token to make authenticated requests
+    const response = await fetch(endpoint, {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    });
+
+    return response.json();
+  }
+}
+
+export default BYODS;
+export {SDKConfig, LocalStream};
