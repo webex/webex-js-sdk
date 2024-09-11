2855f31ecbd0bc8c79215364173415060ed7481b
Priya <pkesari@cisco.com>
2024-09-11 11:18:24 +0530

    feat(byods): switched back to babel

--- packages/byods/jest.config.js
+++ packages/byods/jest.config.js
@@ -1,8 +1,7 @@
 export default {
-  preset: 'ts-jest',
   rootDir: './',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transformIgnorePatterns: ['/node_modules/(?!node-fetch)|data-uri-to-buffer'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testResultsProcessor: 'jest-junit',
