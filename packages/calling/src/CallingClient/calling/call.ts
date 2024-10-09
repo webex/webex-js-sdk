9724e3a09820b9a4aa474eb6f88810781526890a
Ravindra Gupta <ravingup@cisco.com>
2024-10-09 16:52:09 +0530

    adding istio-url hard coded

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -222,7 +222,7 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
     this.callerInfo = {} as DisplayInformation;
     this.localRoapMessage = {} as RoapMessage;
 
-    this.mobiusUrl = activeUrl;
+    this.mobiusUrl = "https://mobius.us-vaawxc1.i1.int.infra.webex.com/api/v1/calling/web/";
     this.receivedRoapOKSeq = 0;
     this.mediaNegotiationCompleted = false;
 
