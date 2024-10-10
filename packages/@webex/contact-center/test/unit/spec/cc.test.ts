5c1cc7281e095da686e7d085829f525bcc1bbcff
Priya <pkesari@cisco.com>
2024-10-10 16:06:36 +0530

    feat(contact-center): fixed failing UT

--- packages/@webex/contact-center/test/unit/spec/cc.test.ts
+++ packages/@webex/contact-center/test/unit/spec/cc.test.ts
@@ -4,24 +4,27 @@ import MockWebex from '@webex/test-helper-mock-webex';
 import {Services} from '@webex/webex-core';
 
 describe('CC Tests', () => {
-  const webex = new MockWebex({
+  let webex;
+
+  beforeEach(() => {
+    webex = new MockWebex({
       children: {
         cc: ContactCenter,
         mercury: Mercury,
         services: Services,
       },
-  });
+    });
+  })
 
   it('Success: invoke the register function, resolved promise', async () => {
-    console.log('mock webex object in test file, ', webex);
     webex.cc.register(true).then((data) => {
         expect(data).toEqual('Success: Dummy data returned');
-    })  
+    })
   });
 
   it('Failed: invoke the register function, rejected promise', async () => {
     webex.cc.register(false).catch((error) => {
-        expect(error).toBe('Simulated error');
-    });    
-  });  
+        expect(error).toEqual('Simulated error');
+    });
+  });
 });
