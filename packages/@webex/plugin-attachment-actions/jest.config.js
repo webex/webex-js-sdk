2614b4fadfffc62591df1263e67484621a453bf7
parv_gour <parv_gour@PAGOUR-M-D8B2>
2024-10-14 15:19:56 +0530

    feat(cc): added back jest config

--- packages/@webex/plugin-attachment-actions/jest.config.js
+++ packages/@webex/plugin-attachment-actions/jest.config.js
@@ -1,44 +1,4 @@
-import config from '@webex/jest-config-legacy';
+const config = require('@webex/jest-config-legacy');	import config from '@webex/jest-config-legacy';
 
-const jestConfig = {
-  rootDir: './',
-  transformIgnorePatterns: [],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  testResultsProcessor: 'jest-junit',
-  // Clear mocks in between tests by default
-  clearMocks: true,
-  // TODO: Set this to true once we have the source code and their corresponding test files added
-  collectCoverage: false,
-  coverageThreshold: {
-    global: {
-      lines: 85,
-      functions: 85,
-      branches: 85,
-      statements: 85,
-    },
-  },
-  coverageDirectory: 'coverage',
-  coverageReporters: ['clover', 'json', 'lcov'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'coverage/junit',
-        outputName: 'coverage-junit.xml',
-        classNameTemplate: '{classname}',
-        titleTemplate: '{title}',
-      },
-    ],
-    [
-      'jest-html-reporters',
-      {
-        publicPath: './coverage',
-        filename: 'jest-report.html',
-        openReport: false,
-      },
-    ],
-  ],
-};
 
-export default {...config, ...jestConfig};
+module.exports = config;
