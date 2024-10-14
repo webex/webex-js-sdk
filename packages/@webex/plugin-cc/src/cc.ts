b9bd6cb47b9d09be280ed9756d16b26a53faefa3
parv_gour <parv_gour@PAGOUR-M-D8B2>
2024-10-14 15:44:48 +0530

    feat(cc): removed eslint disable no console flags

--- packages/@webex/plugin-cc/src/cc.ts
+++ packages/@webex/plugin-cc/src/cc.ts
@@ -22,7 +22,6 @@ export default class ContactCenter extends WebexPlugin implements IContactCenter
   async register(success: boolean): Promise<string> {
     this.wccApiUrl = this.$webex.internal.services.get(WCC_API_GATEWAY); // Added this change for Ravi's PR, he will move this under different function if needed.
     // TODO: Mercury Subsciption code should be added as part of this function
-    // TODO: Mercury Subsciption code should be added as part of this function
     // Establishing Mercury Connection here to get CI Id, which will be used by getAgentProfile method
     // to get Agent Profile by passing CI Id as a parameter.
     const ciUserId = 'dummy_ciUserId';
