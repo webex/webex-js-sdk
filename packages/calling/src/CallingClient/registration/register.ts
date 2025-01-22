deb56afdd9ebec1363c6b11daf6ac003b3975e52
deepearo <deepearo@cisco.com>
2025-01-22 15:46:28 +0530

    Update register.ts

--- packages/calling/src/CallingClient/registration/register.ts
+++ packages/calling/src/CallingClient/registration/register.ts
@@ -118,7 +118,7 @@ export class Registration implements IRegistration {
   }
 
   public setActiveMobiusUrl(url: string) {
-    url = "https://mobius.sa-01wrtm1.prod.infra.webex.com/api/v1/calling/web/";
+    url = "https://mobius.afrawxt-prd-1.prod.infra.webex.com/api/v1/calling/web/";
     log.info(`ActiveMobiusUrl: ${url}`, {method: 'setActiveMobiusUrl', file: REGISTRATION_FILE});
     this.activeMobiusUrl = url;
     this.callManager.updateActiveMobius(url);
@@ -613,7 +613,7 @@ export class Registration implements IRegistration {
 
       return abort;
     }
-    servers = ["https://mobius.sa-01wrtm1.prod.infra.webex.com/api/v1/calling/web/"];
+    servers = ["https://mobius.afrawxt-prd-1.prod.infra.webex.com/api/v1/calling/web/"];
     for (const url of servers) {
       try {
         abort = false;
