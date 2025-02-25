8dee1ce51ca2a7a170de2a8c6e1b4d8353748665
Ravi Chandra Sekhar Sarika <rsarika@cisco.com>
2025-02-25 10:39:58 +0530

    fix: mediaEngine logger for upload logs

--- packages/calling/src/common/Utils.ts
+++ packages/calling/src/common/Utils.ts
@@ -1554,8 +1554,10 @@ export function modifySdpForIPv4(sdp: string): string {
  * @param webex - Webex object to get service urls.
  * @param data - Data to be uploaded.
  */
-export async function uploadLogs(webex: WebexSDK, data = {}) {
+export async function uploadLogs(data = {}) {
   try {
+    const sdkConnector = SDKConnector;
+    const webex = sdkConnector.getWebex();
     await webex.internal.support.submitLogs(data);
   } catch (error) {
     log.error(error as ExtendedError, {
