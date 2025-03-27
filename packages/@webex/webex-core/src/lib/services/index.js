1523a9bb97658ed112efc8bd9b38807c0dce902f
Colin Read <coread@cisco.com>
2025-03-27 11:45:56 +0000

    feat(webex-core): stop registering core plugins on import

--- packages/@webex/webex-core/src/lib/services/index.js
+++ packages/@webex/webex-core/src/lib/services/index.js
@@ -1,19 +1,8 @@
 /*!
  * Copyright (c) 2015-2020 Cisco Systems, Inc. See LICENSE file.
  */
-import {registerInternalPlugin} from '../../webex-core';
 
 import * as constants from './constants';
-import Services from './services';
-import ServerErrorInterceptor from './interceptors/server-error';
-import ServiceInterceptor from './interceptors/service';
-
-registerInternalPlugin('services', Services, {
-  interceptors: {
-    ServiceInterceptor: ServiceInterceptor.create,
-    ServerErrorInterceptor: ServerErrorInterceptor.create,
-  },
-});
 
 export {constants};
 export {default as ServiceInterceptor} from './interceptors/service';
