4a12029f86f6edad61a93a0619fde97cacf930f1
Priya <pkesari@cisco.com>
2025-03-27 21:33:20 +0530

    fix(plugin-cc): fixed desktop login mode with contact-center.min.js

--- packages/webex/src/contact-center.js
+++ packages/webex/src/contact-center.js
@@ -5,6 +5,7 @@ import config from './config';
 
 require('@webex/plugin-authorization');
 require('@webex/plugin-cc');
+require('@webex/internal-plugin-mercury');
 
 const Webex = WebexCore.extend({
   webex: true,
