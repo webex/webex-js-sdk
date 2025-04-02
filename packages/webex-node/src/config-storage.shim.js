deaa358b87b6ac9b0167949f2ef45b3a201305bc
Sreekanth Narayanan <sreenara@cisco.com>
2025-04-02 11:21:59 +0530

    feat(webex-node): add a new package webex-node

---
+++ packages/webex-node/src/config-storage.shim.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2015-2020 Cisco Systems, Inc. See LICENSE file.
+ */
+
+import {MemoryStoreAdapter} from '@webex/webex-core';
+import LocalStorageStoreAdapter from '@webex/storage-adapter-local-storage';
+
+export default {
+  boundedAdapter: new LocalStorageStoreAdapter('webex'),
+  unboundedAdapter: MemoryStoreAdapter,
+};
