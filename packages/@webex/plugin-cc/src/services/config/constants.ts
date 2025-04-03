6ab9d103bf3aca3956102b53aca334473bc0221d
Rajesh Kumar <rarajes2@cisco.com>
2025-04-03 20:21:34 +0530

    feat(plugin-cc): add getQueues() method

--- packages/@webex/plugin-cc/src/services/config/constants.ts
+++ packages/@webex/plugin-cc/src/services/config/constants.ts
@@ -43,4 +43,5 @@ export const endPointMap = {
   tenantData: (orgId: string) => `organization/${orgId}/v2/tenant-configuration?agentView=true`,
   urlMapping: (orgId: string) => `organization/${orgId}/v2/org-url-mapping?sort=name,ASC`,
   dialPlan: (orgId: string) => `organization/${orgId}/dial-plan?agentView=true`,
+  queueList: (orgId: string) => `organization/${orgId}/v2/contact-service-queue`,
 };
