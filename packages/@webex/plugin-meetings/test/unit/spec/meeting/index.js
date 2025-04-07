5de401fc3a0ff1058fce5760d41d09952995091e
k-wasniowski <kacp.was@gmail.com>
2025-04-07 18:06:40 +0200

    fix: add test

--- packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
+++ packages/@webex/plugin-meetings/test/unit/spec/meeting/index.js
@@ -8614,11 +8614,17 @@ describe('plugin-meetings', () => {
           const fakeErrorMessage = 'test error';
           const fakeRootCauseName = 'root cause name';
           const fakeErrorName = 'test error name';
+          let clock;
 
           beforeEach(() => {
+            clock = sinon.useFakeTimers();
             meeting.setupMediaConnectionListeners();
             webex.internal.newMetrics.submitClientEvent.resetHistory();
             Metrics.sendBehavioralMetric.resetHistory();
+          });
+
+          afterEach(() => {
+            clock.restore();
           });
 
           const checkMetricSent = (event, error) => {
@@ -8689,6 +8695,11 @@ describe('plugin-meetings', () => {
           });
 
           it('should send metrics for SdpAnswerHandlingError error', () => {
+            // Add check that timer was closed
+            meeting.sdpResponseTimer = '1234';
+            
+            const clearTimeoutSpy = sinon.spy(clock, 'clearTimeout');
+
             const fakeError = new Errors.SdpAnswerHandlingError(fakeErrorMessage, {
               name: fakeErrorName,
               cause: {name: fakeRootCauseName},
@@ -8703,6 +8714,7 @@ describe('plugin-meetings', () => {
               fakeErrorMessage,
               fakeRootCauseName
             );
+            assert.calledOnce(clearTimeoutSpy);
           });
 
           it('should send metrics for SdpError error', () => {
