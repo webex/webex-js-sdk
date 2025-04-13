bcf301c6f23856015a7c270ec122874750b373fd
parv_gour <parv_gour@PAGOUR-M-D8B2>
2025-04-13 16:34:38 +0530

    fix(samples): fix agent state timer issue

--- docs/samples/contact-center/app.js
+++ docs/samples/contact-center/app.js
@@ -923,9 +923,17 @@ function logoutAgent() {
     console.log('Agent logged out successfully', response);
     loginAgentElm.disabled = false;
 
+     // Clear the timer when the agent logs out
+     if (stateTimer) {
+      clearInterval(stateTimer);
+      stateTimer = null; // Reset the timer variable
+    }
+
+    // Reset UI elements.
     setTimeout(() => {
       logoutAgentElm.classList.add('hidden');
       agentLogin.selectedIndex = 0;
+      timerElm.innerHTML = '00:00:00'; // Reset the timer display
     }, 1000);
   }
   ).catch((error) => {
