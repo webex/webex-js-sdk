d2e3d03f967dc63dddaad72986b007f44649d012
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2025-04-16 17:42:52 +0530

    fix(plugin-cc): update login option to webCallingService on login

--- packages/@webex/plugin-cc/src/cc.ts
+++ packages/@webex/plugin-cc/src/cc.ts
@@ -298,6 +298,7 @@ export default class ContactCenter extends WebexPlugin implements IContactCenter
 
       if (this.agentConfig.webRtcEnabled && data.loginOption === LoginOption.BROWSER) {
         await this.webCallingService.registerWebCallingLine();
+        this.webCallingService.setLoginOption(data.loginOption);
       }
 
       const resp = await loginResponse;
