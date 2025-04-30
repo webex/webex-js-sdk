4cf37fcd69f011f4e9219dcb935cc0c80b6ac001
adhmenon <adhmenon@cisco.com>
2025-04-30 21:30:43 +0530

    fix(plugin-meetings): added-comment

--- packages/@webex/plugin-meetings/src/meeting/util.ts
+++ packages/@webex/plugin-meetings/src/meeting/util.ts
@@ -575,7 +575,7 @@ const MeetingUtil = {
 
   canUserRenameOthers: (displayHints) => displayHints.includes(DISPLAY_HINTS.CAN_RENAME_OTHERS),
 
-  // Default empty value for policies in case we get an undefined value (ie permissionToken is not availableo)
+  // Default empty value for policies if we get an undefined value (ie permissionToken is not available)
   canShareWhiteBoard: (displayHints, policies = {}) =>
     displayHints.includes(DISPLAY_HINTS.SHARE_WHITEBOARD) &&
     !!policies[SELF_POLICY.SUPPORT_WHITEBOARD],
