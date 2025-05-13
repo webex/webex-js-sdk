dc09825049f678458bc7cc24c376df6d67f44f05
Priya <pkesari@cisco.com>
2025-05-13 22:28:11 +0530

    fix(plugin-cc): jest tests fixes

--- packages/@webex/plugin-cc/babel.config.js
+++ packages/@webex/plugin-cc/babel.config.js
@@ -1,3 +1,14 @@
-const babelConfigLegacy = require('@webex/babel-config-legacy');
-
-module.exports = babelConfigLegacy;
+module.exports = {
+  plugins: ['@webex/babel-config-legacy/inject-package-version'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+};
