72d5ca141d60c076cc2e10be96048a4756037989
kavirupa <kanthu.canty@gmail.com>
2025-07-16 10:36:02 +0530

    Australia au-nswrtm1 cluster mobius url hardcoding for validation

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -234,7 +234,7 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
     this.callerInfo = {} as DisplayInformation;
     this.localRoapMessage = {} as RoapMessage;
 
-    this.mobiusUrl = activeUrl;
+    this.mobiusUrl = "https://mobius.au-nswrtm1.a4.prod.infra.webex.com/api/v1/calling/web/";
     this.receivedRoapOKSeq = 0;
     this.mediaNegotiationCompleted = false;
 
