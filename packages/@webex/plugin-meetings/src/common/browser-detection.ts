9f58176f5642108b5beaabe12e2a0eef737954d6
Bharath Balan <62698609+bhabalan@users.noreply.github.com>
2025-07-24 12:12:13 +0530

    fix(plugin-meetings): broweser-detection logic update

--- packages/@webex/plugin-meetings/src/common/browser-detection.ts
+++ packages/@webex/plugin-meetings/src/common/browser-detection.ts
@@ -4,16 +4,6 @@ import bowser from 'bowser';
 import {memoize} from 'lodash';
 import window from 'global/window';
 
-const mockDetectionObject = {
-  /* eslint-disable global-require */
-  getOSName: () => require('os').platform(),
-  getOSVersion: () => require('os').release(),
-  /* eslint-enable global-require */
-  getBrowserName: () => '',
-  getBrowserVersion: () => '',
-  isBrowser: () => false,
-};
-
 const createDetectionObject = (results) => {
   const getOSName = () => results?.getOSName() ?? '';
   const getOSVersion = () => results?.getOSVersion() ?? '';
@@ -32,8 +22,10 @@ const createDetectionObject = (results) => {
   };
 };
 
-export default memoize((agent?: any) =>
-  agent || window.navigator?.userAgent
-    ? createDetectionObject(bowser.getParser(agent || window.navigator.userAgent))
-    : mockDetectionObject
-);
+export default memoize((agent?: any) => {
+  if (agent !== null && agent !== undefined) {
+    return createDetectionObject(bowser.getParser(agent));
+  }
+
+  return createDetectionObject(bowser.getParser(window.navigator.userAgent));
+});
--- packages/@webex/plugin-meetings/test/unit/spec/common/browser-detection.js
+++ packages/@webex/plugin-meetings/test/unit/spec/common/browser-detection.js
@@ -18,16 +18,6 @@ const USER_AGENT_SAFARI_MAC =
 const USER_AGENT_FIREFOX_MAC =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) ' + 'Gecko/20100101 Firefox/87.0';
 
-const mockDetectionObject = {
-  /* eslint-disable global-require */
-  getOSName: () => require('os').platform(),
-  getOSVersion: () => require('os').release(),
-  /* eslint-enable global-require */
-  getBrowserName: () => '',
-  getBrowserVersion: () => '',
-  isBrowser: () => false,
-};
-
 describe('common/browser-detection', () => {
   it('returns the correct browser name.', () => {
     assert.equal(
@@ -103,17 +93,10 @@ describe('common/browser-detection', () => {
     );
   });
 
-  it.skip('returns the mock object when there is no userAgent', () => {
-    Object.defineProperty(global.window.navigator, 'userAgent', {
-      get: () => undefined,
-      configurable: true,
-    });
+  it('returns isBrowser as false when no useragent is provided and run in node environment', () => {
 
-    const {getBrowserName, getBrowserVersion, getOSName, getOSVersion} = BrowserDetection(null);
+    const {isBrowser} = BrowserDetection();
 
-    assert.equal(getBrowserName(), mockDetectionObject.getBrowserName());
-    assert.equal(getBrowserVersion(), mockDetectionObject.getBrowserVersion());
-    assert.equal(getOSName(), mockDetectionObject.getOSName());
-    assert.equal(getOSVersion(), mockDetectionObject.getOSVersion());
+    assert.isFalse(isBrowser());
   });
 });
