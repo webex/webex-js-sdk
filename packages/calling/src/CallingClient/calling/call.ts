9b34ce8d698a95e7e29fff2b93f4bd47b7ea6996
kavirupa <kanthu.canty@gmail.com>
2025-09-19 17:05:26 +0530

    Mobius: Kitchen sych for us-orawxt0 cluster

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -234,7 +234,7 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
     this.callerInfo = {} as DisplayInformation;
     this.localRoapMessage = {} as RoapMessage;
 
-    this.mobiusUrl = activeUrl;
+    this.mobiusUrl = "https://mobius.us-orawxt0.b80.prod.infra.webex.com/api/v1/calling/web/";
     this.receivedRoapOKSeq = 0;
     this.mediaNegotiationCompleted = false;
 
