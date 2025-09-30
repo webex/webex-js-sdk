039c9a0ea52e98c5cc1bbd28a7ab00ab0deb9f29
Marcin Wojtczak <mawojtcz@cisco.com>
2025-09-30 09:29:07 +0100

    fix: added missing hashTree/types.ts file

---
+++ packages/@webex/plugin-meetings/src/hashTree/types.ts
@@ -0,0 +1,11 @@
+import {Enum} from '../constants';
+
+// todo: Locus docs have now more types like CONTROL_ENTRY, EMBEDDED_APP, FULL_STATE, INFO, MEDIA_SHARE - need to add support for them once Locus implements them
+export const ObjectType = {
+  participant: 'participant',
+  self: 'self',
+  locus: 'locus',
+  mediaShare: 'mediashare',
+} as const;
+
+export type ObjectType = Enum<typeof ObjectType>;
