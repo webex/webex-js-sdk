8f45ed45d6a1fc247731d02df99295ed75b6e9a0
mickelr <121160648+mickelr@users.noreply.github.com>
2025-10-14 15:09:36 +0800

    feat(multipleLLM): force publish (#4536)

--- packages/@webex/webex-core/src/webex-core.js
+++ packages/@webex/webex-core/src/webex-core.js
@@ -330,7 +330,7 @@ const WebexCore = AmpState.extend({
    * @returns {WebexCore}
    */
   initialize(attrs = {}) {
-    console.log('multiple LLM special build');
+    console.log('multiple llm special build');
     this.config = merge({}, config, attrs.config);
 
     // There's some unfortunateness with the way {@link AmpersandState#children}
