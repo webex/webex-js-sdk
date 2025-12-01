c62e01e692761058b7c0d767f0ccd7e3e3e7997a
jiayinzh <jiayinzh@cisco.com>
2025-12-01 14:32:32 +0800

    test: raise a test pr for test integration

--- packages/@webex/webex-core/test/integration/spec/services-v2/services-v2.js
+++ packages/@webex/webex-core/test/integration/spec/services-v2/services-v2.js
@@ -27,7 +27,7 @@ import {
 } from '../../../fixtures/host-catalog-v2';
 
 // /* eslint-disable no-underscore-dangle */
-
+// test
 describe('webex-core', () => {
   describe('ServicesV2', () => {
     let webexUser;
