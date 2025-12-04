5c45468d78c7d5745d708cf144c12b3bd492d496
kavirupa <147145098+kavirupa@users.noreply.github.com>
2025-12-04 14:34:49 +0530

    Update Mobius URL for Webex Calling

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -235,7 +235,7 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
     this.localRoapMessage = {} as RoapMessage;
 
     // Hardcoded url
-    this.mobiusUrl = "https://mobius.aorewxt-prd-2.prod.infra.webex.com/api/v1/calling/web/";
+    this.mobiusUrl = "https://mobius.wamswxc-p-2.prod.infra.webex.com/api/v1/calling/web/";
     this.receivedRoapOKSeq = 0;
     this.mediaNegotiationCompleted = false;
 
