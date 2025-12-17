d3e485deb648fbfa901eaf75e4f54f4ce2285a23
kavirupa <147145098+kavirupa@users.noreply.github.com>
2025-12-17 22:00:37 +0530

    Update call.ts

--- packages/calling/src/CallingClient/calling/call.ts
+++ packages/calling/src/CallingClient/calling/call.ts
@@ -234,7 +234,7 @@ export class Call extends Eventing<CallEventTypes> implements ICall {
     this.callerInfo = {} as DisplayInformation;
     this.localRoapMessage = {} as RoapMessage;
 
-    this.mobiusUrl = "https://mobius.us-orawxt0.b80.prod.infra.webex.com/api/v1/calling/web/";
+    this.mobiusUrl = "https://mobius.aload-calling2.ciscospark.com/api/v1/calling/web/";
     this.receivedRoapOKSeq = 0;
     this.mediaNegotiationCompleted = false;
 
