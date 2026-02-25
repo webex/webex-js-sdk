e81a969c8c54e905e0d4718a945c8585f6bae39e
Jordan Rowan <jorowan@cisco.com>
2026-02-25 22:34:31 +0000

    fix(ca): send stayLobbyTime with lobby exit event

--- packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics-latencies.ts
+++ packages/@webex/internal-plugin-metrics/src/call-diagnostic/call-diagnostic-metrics-latencies.ts
@@ -303,10 +303,14 @@ export default class CallDiagnosticLatencies extends WebexPlugin {
    * @returns - latency
    */
   public getStayLobbyTime() {
-    return this.getDiffBetweenTimestamps(
-      'client.locus.join.response',
-      'internal.host.meeting.participant.admitted'
-    );
+    // stayLobbyTime is sent with client.lobby.exited event so we may not have time timestamp yet
+    if (!this.latencyTimestamps.get('client.lobby.exited')) {
+      this.saveTimestamp({
+        key: 'client.lobby.exited',
+      });
+    }
+
+    return this.getDiffBetweenTimestamps('client.locus.join.response', 'client.lobby.exited');
   }
 
   /**
