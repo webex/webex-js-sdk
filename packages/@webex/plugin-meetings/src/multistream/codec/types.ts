7534daf867f000a29dbfce789952bcf6a366ef85
Filip Nowakowski <fnowakow@cisco.com>
2026-03-18 07:40:42 +0100

    refactor(meetings): remove unused constants and streamline codec types

--- packages/@webex/plugin-meetings/src/multistream/codec/types.ts
+++ packages/@webex/plugin-meetings/src/multistream/codec/types.ts
@@ -1,22 +1,7 @@
-import {
-  CodecInfo as WcmeCodecInfo,
-  SupportedResolution,
-  H264EncodingParams,
-} from '@webex/internal-media-core';
-import type {MediaRequest} from '../types';
+import {H264EncodingParams} from '@webex/internal-media-core';
 
 export type H264CodecInfo = H264EncodingParams & {
   codec: 'h264';
 };
 
 export type CodecInfo = H264CodecInfo;
-
-export interface MediaCodecHelper {
-  getCodecInfo(options: {
-    getMaxFs?: () => number;
-    getMaxPicSize?: () => number;
-  }): CodecInfo | undefined;
-  getWCMECodecInfos(mediaRequest: MediaRequest): WcmeCodecInfo[];
-  degradeMediaRequest(mediaRequest: MediaRequest, resolution: SupportedResolution): number;
-  getMaxPayloadBitsPerSecond(mediaRequest: MediaRequest): number;
-}
