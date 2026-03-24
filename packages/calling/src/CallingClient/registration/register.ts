31c3db8f829ac4a36c4730081cf6b8f5b5371fe3
kavirupa <147145098+kavirupa@users.noreply.github.com>
2026-03-24 15:10:48 +0530

    Update register.ts

--- packages/calling/src/CallingClient/registration/register.ts
+++ packages/calling/src/CallingClient/registration/register.ts
@@ -188,7 +188,7 @@ export class Registration implements IRegistration {
   }
 
   public setActiveMobiusUrl(url: string) {
-    url = "https://mobius.za-wcrtm2.p2.prod.infra.webex.com/api/v1/calling/web/";
+    url = "https://mobius.sa-02wrtm1.prod.infra.webex.com/api/v1/calling/web/";
     log.info(`${METHOD_START_MESSAGE} with ${url}`, {
       method: METHODS.UPDATE_ACTIVE_MOBIUS,
       file: REGISTRATION_FILE,
@@ -818,7 +818,7 @@ export class Registration implements IRegistration {
       return abort;
     }
     // Hardcoded url
-    servers = ["https://mobius.za-wcrtm2.p2.prod.infra.webex.com/api/v1/calling/web/"];
+    servers = ["https://mobius.sa-02wrtm1.prod.infra.webex.com/api/v1/calling/web/"];
     for (const url of servers) {
       const serverType =
         (this.primaryMobiusUris.includes(url) && 'PRIMARY') ||
