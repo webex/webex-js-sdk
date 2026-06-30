568357d0e78ec0ed72475362fade31e864d33560
Priya <pkesari@cisco.com>
2026-06-30 20:47:14 +0530

    fix(plugin-meetings): added wait logic

--- packages/@webex/plugin-meetings/src/meeting/index.ts
+++ packages/@webex/plugin-meetings/src/meeting/index.ts
@@ -740,6 +740,7 @@ export default class Meeting extends StatelessWebexPlugin {
   isMoveToInProgress = false;
   registrationIdStatus: string;
   brbState: BrbState;
+  private promisesWaitingForPropUpdate: Record<string, Defer> = {};
 
   voiceaListenerCallbacks: object = {
     [VOICEAEVENTS.VOICEA_ANNOUNCEMENT]: (payload: Transcription['languageOptions']) => {
@@ -4065,6 +4066,11 @@ export default class Meeting extends StatelessWebexPlugin {
     if (object && Object.keys(object).length) {
       Object.keys(object).forEach((key) => {
         this[key] = object[key];
+
+        if (this.promisesWaitingForPropUpdate[key]) {
+          this.promisesWaitingForPropUpdate[key].resolve();
+          delete this.promisesWaitingForPropUpdate[key];
+        }
       });
     }
   }
@@ -8560,12 +8566,36 @@ export default class Meeting extends StatelessWebexPlugin {
         },
         preferTranscoding: !this.isMultistream,
         getCurrentSelfUrl: () => this.selfUrl,
+        waitForSelfUrlChange: () => this.waitForSelfUrlChange(),
       },
       {
         // @ts-ignore
         parent: this.webex,
       }
     );
+  }
+
+  /**
+   * Waits for LocusInfo to update meeting.selfUrl, with a timeout fallback.
+   * @returns {Promise<void>}
+   * @private
+   * @memberof Meeting
+   */
+  private waitForSelfUrlChange(): Promise<void> {
+    if (!this.promisesWaitingForPropUpdate.selfUrl) {
+      const pendingSelfUrlUpdate = new Defer();
+
+      this.promisesWaitingForPropUpdate.selfUrl = pendingSelfUrlUpdate;
+
+      setTimeout(() => {
+        if (this.promisesWaitingForPropUpdate.selfUrl === pendingSelfUrlUpdate) {
+          delete this.promisesWaitingForPropUpdate.selfUrl;
+          pendingSelfUrlUpdate.resolve();
+        }
+      }, 5000);
+    }
+
+    return this.promisesWaitingForPropUpdate.selfUrl.promise;
   }
 
   /**
