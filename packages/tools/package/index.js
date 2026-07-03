e99d03d7271f77a9cc41eead3bdbb31de4933c4d
Shreyas Sharma <shreyassharma9912@gmail.com>
2026-07-03 10:45:12 +0530

    fix(tooling): remove testing code

--- packages/tools/package/index.js
+++ packages/tools/package/index.js
@@ -4,7 +4,6 @@ const PackageTools = require('./dist/module');
 const main = () => {
   const commands = new CLITools.Commands();
 
-  // Temporary CI validation touchpoint for the package-tools entrypoint.
   commands.mount(PackageTools.increment);
   commands.mount(PackageTools.list);
   commands.mount(PackageTools.scripts);
