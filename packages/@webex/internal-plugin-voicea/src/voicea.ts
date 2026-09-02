cef78834d2938720dfea3f09552faeee54ee2d6c
chrisadubois <chdubois@cisco.com>
2026-09-02 05:54:28 -0700

    feat(voicea): emit tagged speaker name updates (#5202)

--- packages/@webex/internal-plugin-voicea/src/voicea.ts
+++ packages/@webex/internal-plugin-voicea/src/voicea.ts
@@ -17,6 +17,7 @@ import {
 import {
   AnnouncementPayload,
   CaptionLanguageResponse,
+  SpeakerNameUpdatePayload,
   TranscriptionResponse,
   IVoiceaChannel,
 } from './voicea.types';
@@ -70,6 +71,9 @@ export class VoiceaChannel extends WebexPlugin implements IVoiceaChannel {
         break;
       case AIBRIDGE_RELAY_TYPES.VOICEA.TRANSCRIPTION:
         this.processTranscription(e.data.voiceaPayload);
+        break;
+      case AIBRIDGE_RELAY_TYPES.VOICEA.SPEAKER_NAME_UPDATE:
+        this.processSpeakerNameUpdate(e.data.voiceaPayload);
         break;
       case AIBRIDGE_RELAY_TYPES.MANUAL.TRANSCRIPTION:
       case AIBRIDGE_RELAY_TYPES.MANUAL.CAPTIONER:
@@ -154,6 +158,16 @@ export class VoiceaChannel extends WebexPlugin implements IVoiceaChannel {
         source: transcriptPayload.data_source,
       });
     }
+  };
+
+  /**
+   * Process speaker name update and send alert
+   * @param {SpeakerNameUpdatePayload} voiceaPayload
+   * @returns {void}
+   */
+  private processSpeakerNameUpdate = (voiceaPayload: SpeakerNameUpdatePayload): void => {
+    // @ts-ignore
+    this.trigger(EVENT_TRIGGERS.SPEAKER_NAME_UPDATED, voiceaPayload);
   };
 
   /**
